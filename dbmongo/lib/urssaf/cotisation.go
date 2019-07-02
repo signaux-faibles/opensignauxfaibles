b608d6695ed09474c2fb65bbe2807c26e06a876c
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2019-07-02 16:49:16 +0200

    Adaptation nouveaux formats URSSAF

--- dbmongo/lib/urssaf/cotisation.go
+++ dbmongo/lib/urssaf/cotisation.go
@@ -1,125 +1,115 @@
 package urssaf
 
 import (
-	"bufio"
-	"dbmongo/lib/engine"
-	"encoding/csv"
-	"io"
-	"os"
-	"strconv"
-	"strings"
+  "bufio"
+  "dbmongo/lib/engine"
+  "encoding/csv"
+  "io"
+  "os"
+  "strconv"
+  "strings"
   "time"
 
-	"github.com/chrnin/gournal"
-	"github.com/spf13/viper"
+  "github.com/chrnin/gournal"
+  "github.com/spf13/viper"
 )
 
 // Cotisation Objet cotisation
 type Cotisation struct {
-	key          string  `hash:"-"`
-	NumeroCompte string  `json:"numero_compte" bson:"numero_compte"`
-	PeriodeDebit string  `json:"periode_debit" bson:"periode_debit"`
-	Periode      Periode `json:"period" bson:"periode"`
-	Recouvrement float64 `json:"recouvrement" bson:"recouvrement"`
-	Encaisse     float64 `json:"encaisse" bson:"encaisse"`
-	Du           float64 `json:"du" bson:"du"`
-	Ecriture     string  `json:"ecriture" bson:"ecriture"`
+  key          string  `hash:"-"`
+  NumeroCompte string  `json:"numero_compte" bson:"numero_compte"`
+  Periode      Periode `json:"period" bson:"periode"`
+  Encaisse     float64 `json:"encaisse" bson:"encaisse"`
+  Du           float64 `json:"du" bson:"du"`
 }
 
 // Key _id de l'objet
 func (cotisation Cotisation) Key() string {
-	return cotisation.key
+  return cotisation.key
 }
 
 // Scope de l'objet
 func (cotisation Cotisation) Scope() string {
-	return "etablissement"
+  return "etablissement"
 }
 
 // Type de l'objet
 func (cotisation Cotisation) Type() string {
-	return "cotisation"
+  return "cotisation"
 }
 
 // ParseCotisation transforme les fichiers en données à intégrer
 func parseCotisation(batch engine.AdminBatch, mapping Comptes) (chan engine.Tuple, chan engine.Event) {
-	outputChannel := make(chan engine.Tuple)
-	eventChannel := make(chan engine.Event)
+  outputChannel := make(chan engine.Tuple)
+  eventChannel := make(chan engine.Event)
 
-	field := map[string]int{
-		"NumeroCompte": 0,
-		"PeriodeDebit": 1,
-		"Periode":      4,
-		"Recouvrement": 2,
-		"Encaisse":     3,
-		"Du":           5,
-		"Ecriture":     6,
-	}
+  field := map[string]int{
+    "NumeroCompte": 2,
+    "Periode":      3,
+    "Encaisse":     5,
+    "Du":           6,
+  }
 
-	go func() {
-		event := engine.Event{
-			Code:    "cotisationParser",
-			Channel: eventChannel,
-		}
+  go func() {
+    event := engine.Event{
+      Code:    "cotisationParser",
+      Channel: eventChannel,
+    }
 
-		for _, path := range batch.Files["cotisation"] {
-			tracker := gournal.NewTracker(
-				map[string]string{"path": path},
-				engine.TrackerReports)
+    for _, path := range batch.Files["cotisation"] {
+      tracker := gournal.NewTracker(
+        map[string]string{"path": path},
+        engine.TrackerReports)
 
-			file, err := os.Open(viper.GetString("APP_DATA") + path)
-			defer file.Close()
+      file, err := os.Open(viper.GetString("APP_DATA") + path)
+      defer file.Close()
 
-			if err != nil {
-				tracker.Error(err)
-				event.Critical(tracker.Report("fatalError"))
-				break
-			}
+      if err != nil {
+        tracker.Error(err)
+        event.Critical(tracker.Report("fatalError"))
+        break
+      }
 
-			reader := csv.NewReader(bufio.NewReader(file))
-			reader.Comma = ';'
-			reader.LazyQuotes = true
-			// ligne de titre
-			reader.Read()
+      reader := csv.NewReader(bufio.NewReader(file))
+      reader.Comma = ';'
+      reader.LazyQuotes = true
+      // ligne de titre
+      reader.Read()
 
-			for {
-				row, err := reader.Read()
-				if err == io.EOF {
-					break
-				} else if err != nil {
-				} else {
+      for {
+        row, err := reader.Read()
+        if err == io.EOF {
+          break
+        } else if err != nil {
+        } else {
           date, err := urssafToDate(row[field["Periode"]])
           if err != nil { date = time.Now() }
 
           if siret, err := mapping.GetSiret(row[field["NumeroCompte"]], date); err == nil {
-						cotisation := Cotisation{}
-						cotisation.key = siret
-						cotisation.NumeroCompte = row[field["NumeroCompte"]]
-						cotisation.Periode, err = urssafToPeriod(row[field["Periode"]])
-						tracker.Error(err)
-						cotisation.PeriodeDebit = row[field["PeriodeDebit"]]
-						cotisation.Recouvrement, err = strconv.ParseFloat(strings.Replace(row[field["Recouvrement"]], ",", ".", -1), 64)
-						tracker.Error(err)
-						cotisation.Encaisse, err = strconv.ParseFloat(strings.Replace(row[field["Encaisse"]], ",", ".", -1), 64)
-						tracker.Error(err)
-						cotisation.Du, err = strconv.ParseFloat(strings.Replace(row[field["Du"]], ",", ".", -1), 64)
-						tracker.Error(err)
-						cotisation.Ecriture = row[field["Ecriture"]]
+            cotisation := Cotisation{}
+            cotisation.key = siret
+            cotisation.NumeroCompte = row[field["NumeroCompte"]]
+            cotisation.Periode, err = urssafToPeriod(row[field["Periode"]])
+            tracker.Error(err)
+            cotisation.Encaisse, err = strconv.ParseFloat(strings.Replace(row[field["Encaisse"]], ",", ".", -1), 64)
+            tracker.Error(err)
+            cotisation.Du, err = strconv.ParseFloat(strings.Replace(row[field["Du"]], ",", ".", -1), 64)
+            tracker.Error(err)
 
-						if !tracker.ErrorInCycle() {
-							outputChannel <- cotisation
-						} else {
-							//event.Debug(tracker.Report("errors"))
-						}
-					}
-				}
-				tracker.Next()
-			}
-			event.Info(tracker.Report("abstract"))
-			file.Close()
-		}
-		close(eventChannel)
-		close(outputChannel)
-	}()
-	return outputChannel, eventChannel
+            if !tracker.ErrorInCycle() {
+              outputChannel <- cotisation
+            } else {
+              //event.Debug(tracker.Report("errors"))
+            }
+          }
+        }
+        tracker.Next()
+      }
+      event.Info(tracker.Report("abstract"))
+      file.Close()
+    }
+    close(eventChannel)
+    close(outputChannel)
+  }()
+  return outputChannel, eventChannel
 }
--- dbmongo/lib/urssaf/delai.go
+++ dbmongo/lib/urssaf/delai.go
@@ -1,144 +1,141 @@
 package urssaf
 
 import (
-	"bufio"
-	"dbmongo/lib/engine"
-	"encoding/csv"
-	//"errors"
-	"io"
-	"os"
-	"strconv"
-	"strings"
-	"time"
+  "bufio"
+  "dbmongo/lib/engine"
+  "encoding/csv"
+  //"errors"
+  "io"
+  "os"
+  "strconv"
+  "strings"
+  "time"
 
-	"github.com/chrnin/gournal"
-	"github.com/spf13/viper"
+  "github.com/chrnin/gournal"
+  "github.com/spf13/viper"
 )
 
 // Delai tuple fichier ursaff
 type Delai struct {
-	key               string    `hash:"-"`
-	NumeroCompte      string    `json:"numero_compte" bson:"numero_compte"`
-	NumeroContentieux string    `json:"numero_contentieux" bson:"numero_contentieux"`
-	DateCreation      time.Time `json:"date_creation" bson:"date_creation"`
-	DateEcheanche     time.Time `json:"date_echeance" bson:"date_echeance"`
-	DureeDelai        int       `json:"duree_delai" bson:"duree_delai"`
-	Denomination      string    `json:"denomination" bson:"denomination"`
-	Indic6m           string    `json:"indic_6m" bson:"indic_6m"`
-	AnneeCreation     int       `json:"annee_creation" bson:"annee_creation"`
-	MontantEcheancier float64   `json:"montant_echeancier" bson:"montant_echeancier"`
-	NumeroStructure   string    `json:"numero_structure" bson:"numero_structure"`
-	Stade             string    `json:"stade" bson:"stade"`
-	Action            string    `json:"action" bson:"action"`
+  key               string    `hash:"-"`
+  NumeroCompte      string    `json:"numero_compte" bson:"numero_compte"`
+  NumeroContentieux string    `json:"numero_contentieux" bson:"numero_contentieux"`
+  DateCreation      time.Time `json:"date_creation" bson:"date_creation"`
+  DateEcheanche     time.Time `json:"date_echeance" bson:"date_echeance"`
+  DureeDelai        int       `json:"duree_delai" bson:"duree_delai"`
+  Denomination      string    `json:"denomination" bson:"denomination"`
+  Indic6m           string    `json:"indic_6m" bson:"indic_6m"`
+  AnneeCreation     int       `json:"annee_creation" bson:"annee_creation"`
+  MontantEcheancier float64   `json:"montant_echeancier" bson:"montant_echeancier"`
+  Stade             string    `json:"stade" bson:"stade"`
+  Action            string    `json:"action" bson:"action"`
 }
 
 // Key _id de l'objet
 func (delai Delai) Key() string {
-	return delai.key
+  return delai.key
 }
 
 // Scope de l'objet
 func (delai Delai) Scope() string {
-	return "etablissement"
+  return "etablissement"
 }
 
 // Type de l'objet
 func (delai Delai) Type() string {
-	return "delai"
+  return "delai"
 }
 
 // Parser fonction d'extraction des délais
 func parseDelai(batch engine.AdminBatch, mapping Comptes) (chan engine.Tuple, chan engine.Event) {
-	outputChannel := make(chan engine.Tuple)
-	eventChannel := make(chan engine.Event)
+  outputChannel := make(chan engine.Tuple)
+  eventChannel := make(chan engine.Event)
 
-	field := map[string]int{
-		"NumeroCompte":      0,
-		"NumeroContentieux": 1,
-		"DateCreation":      2,
-		"DateEcheanche":     3,
-		"DureeDelai":        4,
-		"Denomination":      5,
-		"Indic6m":           6,
-		"AnneeCreation":     7,
-		"MontantEcheancier": 8,
-		"NumeroStructure":   9,
-		"Stade":             10,
-		"Action":            11,
-	}
+  field := map[string]int{
+    "NumeroCompte":      2,
+    "NumeroContentieux": 3,
+    "DateCreation":      4,
+    "DateEcheanche":     5,
+    "DureeDelai":        6,
+    "Denomination":      7,
+    "Indic6m":           8,
+    "AnneeCreation":     9,
+    "MontantEcheancier": 10,
+    "Stade":             11,
+    "Action":            12,
+  }
 
-	event := engine.Event{
-		Code:    "delaiParser",
-		Channel: eventChannel,
-	}
+  event := engine.Event{
+    Code:    "delaiParser",
+    Channel: eventChannel,
+  }
 
-	go func() {
+  go func() {
 
-		for _, path := range batch.Files["delai"] {
-			tracker := gournal.NewTracker(
-				map[string]string{"path": path},
-				engine.TrackerReports)
+    for _, path := range batch.Files["delai"] {
+      tracker := gournal.NewTracker(
+        map[string]string{"path": path},
+        engine.TrackerReports)
 
-			file, err := os.Open(viper.GetString("APP_DATA") + path)
+        file, err := os.Open(viper.GetString("APP_DATA") + path)
 
-			if err != nil {
-				tracker.Error(err)
-				event.Critical(tracker.Report("fatalError"))
-				break
-			} else {
+        if err != nil {
+          tracker.Error(err)
+          event.Critical(tracker.Report("fatalError"))
+          break
+        } else {
 
-				reader := csv.NewReader(bufio.NewReader(file))
-				reader.Comma = ';'
-				reader.Read()
-				for {
-					row, err := reader.Read()
-					if err == io.EOF {
-						break
-					} else if err != nil {
-						tracker.Error(err)
-						event.Debug(tracker.Report("invalidLine"))
-						break
-					} else {
-            date, err := time.Parse("2006-01-02", row[field["DateCreation"]])
-            if err != nil { date = time.Now() }
-						if siret, err := mapping.GetSiret(row[field["NumeroCompte"]], date); err == nil {
-							delai := Delai{}
-							delai.key = siret
-							delai.NumeroCompte = row[field["NumeroCompte"]]
-							delai.NumeroContentieux = row[field["NumeroContentieux"]]
-							delai.DateCreation, err = time.Parse("2006-01-02", row[field["DateCreation"]])
-							tracker.Error(err)
-							delai.DateEcheanche, err = time.Parse("2006-01-02", row[field["DateEcheanche"]])
-							tracker.Error(err)
-							delai.DureeDelai, err = strconv.Atoi(row[field["DureeDelai"]])
-							delai.Denomination = row[field["Denomination"]]
-							delai.Indic6m = row[field["Indic6m"]]
-							delai.AnneeCreation, err = strconv.Atoi(row[field["AnneeCreation"]])
-							tracker.Error(err)
-							delai.MontantEcheancier, err = strconv.ParseFloat(strings.Replace(row[field["MontantEcheancier"]], ",", ".", -1), 64)
-							tracker.Error(err)
-							delai.NumeroStructure = row[field["NumeroStructure"]]
-							delai.Stade = row[field["Stade"]]
-							delai.Action = row[field["Action"]]
-							if !tracker.ErrorInCycle() {
-								outputChannel <- delai
-							} else {
-								//event.Debug(tracker.Report("errors"))
-							}
-						} else {
-              //tracker.Error(errors.New("Compte absent du mapping : " + row[field["NumeroCompte"]]))
-							//event.Debug(tracker.Report("invalidLine"))
-						}
-					}
-					tracker.Next()
-				}
-				file.Close()
-				event.Debug(tracker.Report("abstract"))
-			}
-		}
-		close(outputChannel)
-		close(eventChannel)
-	}()
+          reader := csv.NewReader(bufio.NewReader(file))
+          reader.Comma = ';'
+          reader.Read()
+          for {
+            row, err := reader.Read()
+            if err == io.EOF {
+              break
+            } else if err != nil {
+              tracker.Error(err)
+              event.Debug(tracker.Report("invalidLine"))
+              break
+            } else {
+              date, err := time.Parse("2006-01-02", row[field["DateCreation"]])
+              if err != nil { date = time.Now() }
+              if siret, err := mapping.GetSiret(row[field["NumeroCompte"]], date); err == nil {
+                delai := Delai{}
+                delai.key = siret
+                delai.NumeroCompte = row[field["NumeroCompte"]]
+                delai.NumeroContentieux = row[field["NumeroContentieux"]]
+                delai.DateCreation, err = time.Parse("2006-01-02", row[field["DateCreation"]])
+                tracker.Error(err)
+                delai.DateEcheanche, err = time.Parse("2006-01-02", row[field["DateEcheanche"]])
+                tracker.Error(err)
+                delai.DureeDelai, err = strconv.Atoi(row[field["DureeDelai"]])
+                delai.Denomination = row[field["Denomination"]]
+                delai.Indic6m = row[field["Indic6m"]]
+                delai.AnneeCreation, err = strconv.Atoi(row[field["AnneeCreation"]])
+                tracker.Error(err)
+                delai.MontantEcheancier, err = strconv.ParseFloat(strings.Replace(row[field["MontantEcheancier"]], ",", ".", -1), 64)
+                tracker.Error(err)
+                delai.Stade = row[field["Stade"]]
+                delai.Action = row[field["Action"]]
+                if !tracker.ErrorInCycle() {
+                  outputChannel <- delai
+                } else {
+                  //event.Debug(tracker.Report("errors"))
+                }
+              } else {
+                //tracker.Error(errors.New("Compte absent du mapping : " + row[field["NumeroCompte"]]))
+                //event.Debug(tracker.Report("invalidLine"))
+              }
+            }
+            tracker.Next()
+          }
+          file.Close()
+          event.Debug(tracker.Report("abstract"))
+        }
+      }
+      close(outputChannel)
+      close(eventChannel)
+    }()
 
-	return outputChannel, eventChannel
-}
+    return outputChannel, eventChannel
+  }
