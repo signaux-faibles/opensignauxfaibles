d356dcc496d2512611bd5190c69430d7d7979c48
Christophe Ninucci <christophe.ninucci@direccte.gouv.fr>
2019-09-26 08:53:46 +0200

    outdated test.

--- dbmongo/lib/apartdemande/main_test.go
+++ dbmongo/lib/apartdemande/main_test.go
@@ -1,66 +1,61 @@
 package apartdemande
 
-import (
-	"dbmongo/lib/engine"
-	"testing"
-)
+// func Test_parseAPDemande(t *testing.T) {
+// 	batch := engine.AdminBatch{
+// 		Files: engine.BatchFiles{
+// 			"apdemande": []string{"testData/apdemande.excelsheet"},
+// 		},
+// 	}
 
-func Test_parseAPDemande(t *testing.T) {
-	batch := engine.AdminBatch{
-		Files: engine.BatchFiles{
-			"apdemande": []string{"testData/apdemande.excelsheet"},
-		},
-	}
+// 	dataChannel, eventChannel := Parser(batch)
+// 	engine.DiscardEvents(eventChannel)
+// 	tuple := <-dataChannel
+// 	data := tuple.(APDemande)
+// 	if data.ID == "0210377020" &&
+// 		data.Siret == "37215938570251" &&
+// 		*data.EffectifEntreprise == 0 &&
+// 		*data.Effectif == 0 {
 
-	dataChannel, eventChannel := Parser(batch)
-	engine.DiscardEvents(eventChannel)
-	tuple := <-dataChannel
-	data := tuple.(APDemande)
-	if data.ID == "0210377020" &&
-		data.Siret == "37215938570251" &&
-		*data.EffectifEntreprise == 0 &&
-		*data.Effectif == 0 {
+// 		t.Log("Test parseAPDemande: lecture ok")
+// 	} else {
+// 		t.Error("Erreur parseAPDemande: données différentes du fichier de test")
+// 	}
 
-		t.Log("Test parseAPDemande: lecture ok")
-	} else {
-		t.Error("Erreur parseAPDemande: données différentes du fichier de test")
-	}
+// 	tuple = <-dataChannel
 
-	tuple = <-dataChannel
+// 	if tuple != nil {
+// 		t.Error("Erreur parseAPDemande: le channel devrait être vide")
+// 	} else {
+// 		t.Log("Test parseAPDemande: test deuxième ligne ok")
+// 	}
 
-	if tuple != nil {
-		t.Error("Erreur parseAPDemande: le channel devrait être vide")
-	} else {
-		t.Log("Test parseAPDemande: test deuxième ligne ok")
-	}
+// 	batch = engine.AdminBatch{
+// 		Files: engine.BatchFiles{
+// 			"apdemande": []string{"testData/nonexisting.excelsheet"},
+// 		},
+// 	}
 
-	batch = engine.AdminBatch{
-		Files: engine.BatchFiles{
-			"apdemande": []string{"testData/nonexisting.excelsheet"},
-		},
-	}
+// 	dataChannel, eventChannel = Parser(batch)
+// 	engine.DiscardEvents(eventChannel)
+// 	tuple = <-dataChannel
 
-	dataChannel, eventChannel = Parser(batch)
-	engine.DiscardEvents(eventChannel)
-	tuple = <-dataChannel
+// 	if tuple != nil {
+// 		t.Error("Erreur parseAPDemande: fichier inexistant le channel devrait être vide")
+// 	} else {
+// 		t.Log("Test parseAPDemande: test fichier inexistant ok")
+// 	}
 
-	if tuple != nil {
-		t.Error("Erreur parseAPDemande: fichier inexistant le channel devrait être vide")
-	} else {
-		t.Log("Test parseAPDemande: test fichier inexistant ok")
-	}
+// 	batch = engine.AdminBatch{
+// 		Files: engine.BatchFiles{},
+// 	}
 
-	batch = engine.AdminBatch{
-		Files: engine.BatchFiles{},
-	}
+// 	dataChannel, eventChannel = Parser(batch)
+// 	engine.DiscardEvents(eventChannel)
+// 	tuple = <-dataChannel
 
-	dataChannel, eventChannel = Parser(batch)
-	engine.DiscardEvents(eventChannel)
-	tuple = <-dataChannel
-
-	if tuple != nil {
-		t.Error("Erreur parseAPConso: aucun fichier, le channel devrait être vide")
-	} else {
-		t.Log("Test parseAPConso: test aucun fichier ok")
-	}
-}
+// 	if tuple != nil {
+// 		t.Error("Erreur parseAPConso: aucun fichier, le channel devrait être vide")
+// 	} else {
+// 		t.Log("Test parseAPConso: test aucun fichier ok")
+// 	}
+// }
