29d63f850f6af0e683c3f270b99a65ee5161125e
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-04-17 15:24:55 +0200

    Revert "fix: use prettier instead of sed, to also fix splitting of long lines"
    This reverts commit 0ef0d1e336273955e7f808ae25553e15bb2dc048.

--- dbmongo/js/common/raison_sociale.js
+++ dbmongo/js/common/raison_sociale.js
@@ -1,22 +1,13 @@
-function raison_sociale(
-  denomination_unite_legale,
-  nom_unite_legale,
-  nom_usage_unite_legale,
-  prenom1_unite_legale,
-  prenom2_unite_legale,
-  prenom3_unite_legale,
-  prenom4_unite_legale
-) {
-  debug("running raison_sociale()");
+function raison_sociale(denomination_unite_legale, nom_unite_legale, nom_usage_unite_legale, prenom1_unite_legale, prenom2_unite_legale, prenom3_unite_legale, prenom4_unite_legale) {
+  debug('running raison_sociale()');
   if (!nom_usage_unite_legale) {
     var nom_usage_unite_legale = "";
-  } else {
+  }
+  else {
     var nom_usage_unite_legale = nom_usage_unite_legale + "/";
   }
-  var raison_sociale =
-    denomination_unite_legale ||
-    (
-      nom_unite_legale +
+  var raison_sociale = denomination_unite_legale ||
+    (nom_unite_legale +
       "*" +
       nom_usage_unite_legale +
       prenom1_unite_legale +
@@ -26,7 +17,6 @@ function raison_sociale(
       (prenom3_unite_legale || "") +
       " " +
       (prenom4_unite_legale || "") +
-      " "
-    ).trim() + "/";
+      " ").trim() + "/";
   return raison_sociale;
 }
