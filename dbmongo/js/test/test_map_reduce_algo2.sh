18455b487eca635c5248aed4e372aba637d8f1fb
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-04-20 15:15:50 +0200

    fix: clean up after running js tests + add comments

--- dbmongo/js/test/test_map_reduce_algo2.sh
+++ dbmongo/js/test/test_map_reduce_algo2.sh
@@ -1,12 +1,12 @@
+# Download realistic data set
 mkdir ./test_data_algo2
 scp stockage:/home/centos/opensignauxfaibles_tests/reduce_test_data.json ./test_data_algo2/
 
-# Tests here
-
+# Run tests
 echo "ISODate = (dateString) => new Date(dateString); NumberInt = (int) => int; testData = $(cat ./test_data_algo2/reduce_test_data.json)" > ./test_data_algo2/reduce_test_data.js
 cat ../common/*.js >./test_data_algo2/jsFunctions.js
 cat ../reduce.algo2/*.js >>./test_data_algo2/jsFunctions.js
 jsc ./test_data_algo2/reduce_test_data.js ./test_data_algo2/jsFunctions.js ./test_map_reduce_algo2.js > ./test_data_algo2/stdout.log
 
 # Clean up
-# rm -rf ./test_data_algo2
+rm -rf ./test_data_algo2
