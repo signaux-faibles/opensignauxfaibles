ea723c629c650b71c2698459fb210cf18427d516
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-04-20 15:56:45 +0200

    linting: add missing semicolons

--- dbmongo/js/test/test_map_reduce_algo2.js
+++ dbmongo/js/test/test_map_reduce_algo2.js
@@ -13,11 +13,11 @@ f = this;
 
 // Define global parameters that are required by JS functions
 actual_batch = "2002_1";
-date_debut = new Date("2014-01-01")
-date_fin = new Date("2016-01-01")
+date_debut = new Date("2014-01-01");
+date_fin = new Date("2016-01-01");
 serie_periode = f.generatePeriodSerie(date_debut, date_fin);
-includes = {"all": true}
-offset_effectif = 2
+includes = { "all": true };
+offset_effectif = 2;
 
 // Run a map() function designed for MongoDB, i.e. that calls emit() an
 // inderminate number of times, instead of returning one value per iteration.
