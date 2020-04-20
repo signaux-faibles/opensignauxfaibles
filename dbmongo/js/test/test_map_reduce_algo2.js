34a5714c706273ecb3ad5b3ff4e0fd7965dfa525
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-04-20 11:05:05 +0200

    On collecte le resultat du map dans stdout.log

--- dbmongo/js/test/test_map_reduce_algo2.js
+++ dbmongo/js/test/test_map_reduce_algo2.js
@@ -1,10 +1,11 @@
 
 
 const notreMap = (testData) => {
-  const results = [];
-  emit = (result) => results.push(result);
-  map(testData); // will call emit an inderminate number of times
+  const results = {};
+  emit = (key, value) => results[key] = value;
+  map.call(testData); // will call emit an inderminate number of times
+  // testData contains _id and value properties. testData is passed as this
   return results;
 };
 
-debug(JSON.stringify(notreMap(testData), null, 2));
+debug(JSON.stringify(notreMap(testData[0]), null, 2));
