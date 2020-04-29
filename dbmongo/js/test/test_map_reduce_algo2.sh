1d4f8e14dfee899b567abff81c939576327be517
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-04-29 10:46:51 +0200

    feat: diff + possibility to update the golden file.

--- dbmongo/js/test/test_map_reduce_algo2.sh
+++ dbmongo/js/test/test_map_reduce_algo2.sh
@@ -7,7 +7,7 @@
 # Download realistic data set
 TMP_PATH="./test_data_algo2"
 mkdir ${TMP_PATH}
-scp -r stockage:/home/centos/opensignauxfaibles_tests/ ${TMP_PATH}/
+scp stockage:/home/centos/opensignauxfaibles_tests/* ${TMP_PATH}/
 
 # Prepare test data set
 JSON_TEST_DATASET="$(cat ./test_data_algo2/reduce_test_data.json)"
@@ -17,14 +17,18 @@ echo "makeTestData = ({ ISODate, NumberInt }) => (${JSON_TEST_DATASET});" \
 # Run tests
 jsc ${TMP_PATH}/reduce_test_data.js ../common/*.js ../reduce.algo2/*.js ./test_map_reduce_algo2.js \
   > ${TMP_PATH}/map_stdout.log
-cat ${TMP_PATH}/map_stdout.log
 
 if [ "$1" == "--update" ]; then
   cp ${TMP_PATH}/map_stdout.log ${TMP_PATH}/map_golden.log
   scp ${TMP_PATH}/map_golden.log stockage:/home/centos/opensignauxfaibles_tests/
 fi
 
-# TODO: compare map_stdout.log with golden file, return non-zero exit code if any difference is found
+# compare map_stdout.log with golden file, return non-zero exit code if any difference is found
+DIFF=$(diff ${TMP_PATH}/map_stdout.log ${TMP_PATH}/map_golden.log)
+if [ "${DIFF}" != "" ]; then
+  echo "Test failed, because of diff: ${DIFF}"
+  exit 1
+fi
 
 # Clean up
 # TODO: make sure to delete the file in the case of error.
