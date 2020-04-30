42664fa60168bbcaf419df78b1a46109d284b8bc
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-04-30 14:09:04 +0200

    ✅ exclude _test file from public

--- dbmongo/js/test/test_public.sh
+++ dbmongo/js/test/test_public.sh
@@ -2,7 +2,8 @@
 
 shopt -s extglob # enable exclusion of test files in wildcard
 
-result_public=$(jsc ../public/*.js ../common/!(*_test).js helpers/fakes.js objects.js ../public/_test.js 2>&1)
+# TODO: use `2>&1` instead of `set -e`, in all sh tests
+result_public=$(jsc ../public/!(*_test).js ../common/!(*_test).js helpers/fakes.js objects.js ../public/_test.js 2>&1)
 if [ "$result_public" != 'true' ]; then
   echo "$result_public"
   exit 1
