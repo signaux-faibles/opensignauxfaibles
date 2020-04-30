f79e305d14a5b7f3d2838ca623a1aaf37a7a3b6a
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-04-30 11:58:22 +0200

    green: test_public fonctionne

--- dbmongo/js/test/test_public.sh
+++ dbmongo/js/test/test_public.sh
@@ -1,8 +1,9 @@
 # This file is run by dbmongo/js_test.go.
 
-set -e
+set -e # stop on errors
+shopt -s extglob # enable exclusion of test files in wildcard
 
-result_public=$(jsc ../public/*.js ../common/*.js public/lib_public.js objects.js public/test_public.js)
+result_public=$(jsc ../public/*.js ../common/!(*_test).js public/lib_public.js objects.js public/test_public.js)
 if [ "$result_public" != 'true' ]; then
   echo "$result_public"
   exit 1
