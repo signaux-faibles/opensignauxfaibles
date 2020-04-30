a2a554d716923b83daefaeab64002c6de3f0446a
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-04-30 11:09:03 +0200

    ✅ test_compact.sh runs test files at their new location

--- dbmongo/js/test/test_compact.sh
+++ dbmongo/js/test/test_compact.sh
@@ -1,18 +1,18 @@
 # This file is run by dbmongo/js_test.go.
 
-result_add=$(jsc ../compact/currentState.js compact/test_current_state.js)
+result_add=$(jsc ../compact/currentState.js ../compact/currentState_test.js)
 if [ "$result_add" != 'true' ]; then
   echo "$result_add"
   exit 1
 fi
 
-result_add=$(jsc ../compact/currentState.js ../compact/reduce.js ./testing.js ./compact/test_reduce.js)
+result_add=$(jsc ../compact/currentState.js ../compact/reduce.js ./testing.js ../compact/reduce_test.js)
 if [ "$result_add" != 'true' ]; then
   echo "$result_add"
   exit 1
 fi
 
-result_finalize=$(jsc ../compact/currentState.js ../compact/finalize.js ../compact/complete_reporder.js ./testing.js ./compact/test_finalize.js)
+result_finalize=$(jsc ../compact/currentState.js ../compact/finalize.js ../compact/complete_reporder.js ./testing.js ../compact/finalize_test.js)
 if [ "$result_finalize" != 'true' ]; then
   echo "$result_finalize"
   exit 1
