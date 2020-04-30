d5653d91d2e6837e04c2b8b5c410b68a9d1ec022
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-04-30 14:41:08 +0200

    refactor: spread js includes

--- dbmongo/js/test/test_algo2.sh
+++ dbmongo/js/test/test_algo2.sh
@@ -3,23 +3,48 @@
 set -e # will stop on any error
 shopt -s extglob # enable exclusion of test files in wildcard
 
-result_add=$(jsc ../reduce.algo2/!(*_test).js ../common/!(*_test).js helpers/testing.js algo2/lib_algo2.js ../reduce.algo2/add_test.js)
+result_add=$(jsc \
+  ../reduce.algo2/!(*_test).js \
+  ../common/!(*_test).js \
+  helpers/testing.js \
+  algo2/lib_algo2.js \
+  ../reduce.algo2/add_test.js \
+  )
 if [ "$result_add" != 'true' ]; then
   exit 1
 fi
 
-result_lookAhead=$(jsc ../reduce.algo2/!(*_test).js ../common/!(*_test).js helpers/testing.js algo2/lib_algo2.js ../reduce.algo2/lookAhead_test.js)
+result_lookAhead=$(jsc \
+  ../reduce.algo2/!(*_test).js \
+  ../common/!(*_test).js \
+  helpers/testing.js \
+  algo2/lib_algo2.js \
+  ../reduce.algo2/lookAhead_test.js \
+  )
 if [ "$result_lookAhead" != 'true' ]; then
   exit 1
 fi
 
-result_cibleApprentissage=$(jsc ../reduce.algo2/!(*_test).js ../common/!(*_test).js helpers/testing.js algo2/lib_algo2.js ../reduce.algo2/cibleApprentissage_test.js)
+result_cibleApprentissage=$(jsc \
+  ../reduce.algo2/!(*_test).js \
+  ../common/!(*_test).js \
+  helpers/testing.js \
+  algo2/lib_algo2.js \
+  ../reduce.algo2/cibleApprentissage_test.js \
+  )
 if [ "$result_cibleApprentissage" != 'true' ]; then
   exit 1
 fi
 
 # TODO pourquoi ce test est commenté ?
-# result_mapreduce=$(jsc ../reduce.algo2/!(*_test).js ../common/!(*_test).js algo2/lib_algo2.js data/naf.js data/objects.js ./reduce.algo2/_test.js)
+# result_mapreduce=$(jsc \
+#   ../reduce.algo2/!(*_test).js \
+#   ../common/!(*_test).js \
+#   algo2/lib_algo2.js \
+#   data/naf.js \
+#   data/objects.js \
+#   ./reduce.algo2/_test.js\
+#   )
 # if [ "$result_mapreduce" != 'true' ]; then
 #   exit 1
 # fi
