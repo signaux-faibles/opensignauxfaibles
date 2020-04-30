4faff800e334edcf8a940a06e522de66fbe541f8
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-04-30 11:49:51 +0200

    partial ✅ : exclude test files when including common/*.js

--- dbmongo/js/test/test_common.sh
+++ dbmongo/js/test/test_common.sh
@@ -1,8 +1,9 @@
 # This file is run by dbmongo/js_test.go.
 
-set -e
+set -e # stop on errors
+shopt -s extglob # enable exclusion of test files in wildcard
 
-result_raison_sociale=$(jsc ../common/*.js helpers/testing.js ../common/raison_sociale_test.js)
+result_raison_sociale=$(jsc ../common/!(*_test).js helpers/testing.js ../common/raison_sociale_test.js)
 if [ "$result_raison_sociale" != 'true' ]; then
   echo "$result_raison_sociale"
   exit 1
