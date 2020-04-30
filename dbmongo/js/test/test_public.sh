72694be742b935271330e8479f972441f57d6e42
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-04-30 11:56:46 +0200

    red: on révèle une erreur depuis test_public, qui n'était pas exécuté.

--- dbmongo/js/test/test_public.sh
+++ dbmongo/js/test/test_public.sh
@@ -2,4 +2,8 @@
 
 set -e
 
-jsc ../public/*.js ../common/*.js public/lib_public.js objects.js public/test_public.js
+result_public=$(jsc ../public/*.js ../common/*.js public/lib_public.js objects.js public/test_public.js)
+if [ "$result_public" != 'true' ]; then
+  echo "$result_public"
+  exit 1
+fi
