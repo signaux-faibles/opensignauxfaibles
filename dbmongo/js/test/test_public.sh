02c9344083de4846d9e135f73984d1d0617d1317
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-04-30 14:12:13 +0200

    🔴 spread and order js includes

--- dbmongo/js/test/test_public.sh
+++ dbmongo/js/test/test_public.sh
@@ -3,7 +3,13 @@
 shopt -s extglob # enable exclusion of test files in wildcard
 
 # TODO: use `2>&1` instead of `set -e`, in all sh tests
-result_public=$(jsc ../public/!(*_test).js ../common/!(*_test).js helpers/fakes.js objects.js ../public/_test.js 2>&1)
+result_public=$(jsc \
+  helpers/fakes.js \
+  ../common/!(*_test).js \
+  objects.js \
+  ../public/!(*_test).js \
+  ../public/_test.js \
+  2>&1)
 if [ "$result_public" != 'true' ]; then
   echo "$result_public"
   exit 1
