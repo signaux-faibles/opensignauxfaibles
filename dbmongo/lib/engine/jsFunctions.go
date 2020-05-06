222c4400861a3496a237e597825848db44af6852
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-05-06 10:59:13 +0200

    red: map échoue avec "use strict";

--- dbmongo/lib/engine/jsFunctions.go
+++ dbmongo/lib/engine/jsFunctions.go
@@ -1579,11 +1579,14 @@ db.getCollection("Features").createIndex({
   return output
 }
 `,
-"map": `function map () {
+"map": `
+function map () {
+
+  "use strict";
   let v = f.flatten(this.value, actual_batch)
 
   if (v.scope == "etablissement") {
-    let o = f.outputs(v, serie_periode)
+    o = f.outputs(v, serie_periode)
     let output_array = o[0] // [ OutputValue ] // in chronological order
     let output_indexed = o[1] // { Periode -> OutputValue } // OutputValue: cf outputs()
 
