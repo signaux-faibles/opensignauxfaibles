c67b5f6d0668db7a03ee3e47c141cacb9f197ed7
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-05-06 16:28:37 +0200

    add comments to make jsParams more searchable

--- dbmongo/lib/engine/reduce.go
+++ dbmongo/lib/engine/reduce.go
@@ -281,14 +281,14 @@ func reduceDefineScope(batch AdminBatch, algo string, types []string) (bson.M, e
 		"date_debut":             batch.Params.DateDebut,
 		"date_fin":               batch.Params.DateFin,
 		"date_fin_effectif":      batch.Params.DateFinEffectif,
-		"serie_periode":          misc.GenereSeriePeriode(batch.Params.DateDebut, batch.Params.DateFin),
+		"serie_periode":          misc.GenereSeriePeriode(batch.Params.DateDebut, batch.Params.DateFin), // <-- jsParams.serie_periode
 		"serie_periode_annuelle": misc.GenereSeriePeriodeAnnuelle(batch.Params.DateDebut, batch.Params.DateFin),
 		"offset_effectif":        (batch.Params.DateFinEffectif.Year()-batch.Params.DateFin.Year())*12 + int(batch.Params.DateFinEffectif.Month()-batch.Params.DateFin.Month()),
-		"actual_batch":           batch.ID.Key,
-		"naf":                    naf,
+		"actual_batch":           batch.ID.Key, // <-- jsParams.actual_batch
+		"naf":                    naf,          // <-- jsParams.naf
 		"batches":                GetBatchesID(),
 		"types":                  GetTypes(),
-		"includes":               includes,
+		"includes":               includes, // <-- jsParams.includes
 	}
 
 	scope := bson.M{
