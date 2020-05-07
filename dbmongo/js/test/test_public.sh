6c3b6639d5bbd47fea892c909b5f8643fe99f20b
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-05-07 10:29:58 +0200

    🔴 load and test "public" JS files in strict mode

--- dbmongo/js/test/test_public.sh
+++ dbmongo/js/test/test_public.sh
@@ -3,12 +3,12 @@
 shopt -s extglob # enable exclusion of test files in wildcard
 
 result_public=$(jsc \
-  helpers/fakes.js \
-  helpers/fake_emit_for_public.js \
-  data/objects.js \
-  ../common/!(*_test).js \
-  ../public/!(*_test).js \
-  ../public/_test.js \
+  --strict-file=helpers/fakes.js \
+  --strict-file=helpers/fake_emit_for_public.js \
+  --strict-file=data/objects.js \
+  --strict-file=../common/!(*_test).js \
+  --strict-file=../public/!(*_test).js \
+  --strict-file=../public/_test.js \
   2>&1)
 if [ "$result_public" != 'true' ]; then
   echo "$result_public"
