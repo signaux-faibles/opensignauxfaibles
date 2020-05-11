4f6ec0bc1f30795a54b45013d9d7ad0bf9b0c08b
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-05-11 17:23:23 +0200

    remove dummy test

--- dbmongo/js/reduce.algo2/map_tests.ts
+++ dbmongo/js/reduce.algo2/map_tests.ts
@@ -1,10 +1,6 @@
 import test from 'ava'
 import { map } from './map.js'
 
-test('one plus two equals three', t => {
-  t.is(1 + 2, 3)
-})
-
 test('map is a function', t => {
   t.is(typeof map, 'function')
 })
