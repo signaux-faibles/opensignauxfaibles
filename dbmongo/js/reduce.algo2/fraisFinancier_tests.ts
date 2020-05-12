f76d9942be437d53b835fe31c9cde2699b842594
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-05-12 11:30:26 +0200

    🔵 refacto: shared diane builder

--- dbmongo/js/reduce.algo2/fraisFinancier_tests.ts
+++ dbmongo/js/reduce.algo2/fraisFinancier_tests.ts
@@ -1,6 +1,15 @@
 import test from 'ava'
 import { fraisFinancier } from './fraisFinancier.js'
 
+const fakeDiane = () => ({
+  interets: 50,
+  excedent_brut_d_exploitation: 1000,
+  produits_financiers: 100,
+  produit_exceptionnel: 120,
+  charge_exceptionnelle: 450,
+  charges_financieres: 160
+})
+
 test(`fraisFinancier est calculé selon la formule:
  interets / (
   excedent_brut_d_exploitation +
@@ -8,14 +17,7 @@ test(`fraisFinancier est calculé selon la formule:
   produit_exceptionnel -
   charge_exceptionnelle -
   charges_financieres ) * 100`, t => {
-  const diane = {
-    interets: 50,
-    excedent_brut_d_exploitation: 1000,
-    produits_financiers: 100,
-    produit_exceptionnel: 120,
-    charge_exceptionnelle: 450,
-    charges_financieres: 160
-  }
+  const diane = fakeDiane()
   const resultat = diane.interets / (diane.excedent_brut_d_exploitation +
     diane.produits_financiers + diane.produit_exceptionnel -
     diane.charge_exceptionnelle - diane.charges_financieres) * 100
@@ -25,14 +27,7 @@ test(`fraisFinancier est calculé selon la formule:
 const proprietes = ["interets", "excedent_brut_d_exploitation", "produits_financiers", "produit_exceptionnel", "charge_exceptionnelle", "charges_financieres"]
 proprietes.forEach((propriete) =>
   test(`fraisFinancier est nul si "${propriete}" n'est pas disponible dans Diane`, t => {
-    const diane = {
-      interets: 50,
-      excedent_brut_d_exploitation: 1000,
-      produits_financiers: 100,
-      produit_exceptionnel: 120,
-      charge_exceptionnelle: 450,
-      charges_financieres: 160
-    }
+    const diane = fakeDiane()
     diane[propriete] = null
     t.is(fraisFinancier(diane), null)
   })
