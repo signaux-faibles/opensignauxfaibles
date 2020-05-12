46f9bac92d10b0958998d4281a150ba06910ad0a
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-05-12 11:28:34 +0200

    fix 2eme test

--- dbmongo/js/reduce.algo2/fraisFinancier_tests.ts
+++ dbmongo/js/reduce.algo2/fraisFinancier_tests.ts
@@ -25,11 +25,17 @@ test(`fraisFinancier est calculé selon la formule:
 const proprietes = ["interets", "excedent_brut_d_exploitation", "produits_financiers", "produit_exceptionnel", "charge_exceptionnelle", "charges_financieres"]
 proprietes.forEach((propriete) =>
   test(`fraisFinancier est nul si "${propriete}" n'est pas disponible dans Diane`, t => {
-    const diane = {}
+    const diane = {
+      interets: 50,
+      excedent_brut_d_exploitation: 1000,
+      produits_financiers: 100,
+      produit_exceptionnel: 120,
+      charge_exceptionnelle: 450,
+      charges_financieres: 160
+    }
+    diane[propriete] = null
     t.is(fraisFinancier(diane), null)
   })
 )
-
-
 
 // $ node_modules/.bin/ava-ts ./reduce.algo2/fraisFinancier_tests.ts
