147030f4f628c48607d1c1d1151a97280ebdf2b1
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-05-12 18:55:57 +0200

    fix: "Unexpected var, use let or const instead"

--- dbmongo/js/common/raison_sociale.ts
+++ dbmongo/js/common/raison_sociale.ts
@@ -3,18 +3,14 @@ declare function debug(string) // supported by jsc, to print in stdout
 function raison_sociale(
   denomination_unite_legale: string,
   nom_unite_legale: string,
-  nom_usage_unite_legale: string,
+  nom_usage_unite_legale = "",
   prenom1_unite_legale: string,
   prenom2_unite_legale: string,
   prenom3_unite_legale: string,
   prenom4_unite_legale: string
 ): string {
   "use strict"
-  if (!nom_usage_unite_legale) {
-    var nom_usage_unite_legale = ""
-  } else {
-    var nom_usage_unite_legale = nom_usage_unite_legale + "/"
-  }
+  nom_usage_unite_legale = nom_usage_unite_legale + "/"
   const raison_sociale =
     denomination_unite_legale ||
     (
