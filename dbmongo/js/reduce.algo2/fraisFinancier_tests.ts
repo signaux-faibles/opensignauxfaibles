d0e4d1671763005dfcdd4740720a4bc0d20c3d35
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-05-12 11:10:55 +0200

    ✅ ajout d'un test décrivant le calcul de fraisFinancier

--- dbmongo/js/reduce.algo2/fraisFinancier_tests.ts
+++ dbmongo/js/reduce.algo2/fraisFinancier_tests.ts
@@ -5,3 +5,24 @@ test(`fraisFinancier est nul si "interets" n'est pas disponible dans Diane`, t =
   const diane = {}
   t.is(fraisFinancier(diane), null)
 })
+
+test(`fraisFinancier est calculé selon la formule:
+ interets / (
+  excedent_brut_d_exploitation +
+  produits_financiers +
+  produit_exceptionnel -
+  charge_exceptionnelle -
+  charges_financieres ) * 100`, t => {
+  const diane = {
+    interets: 50,
+    excedent_brut_d_exploitation: 1000,
+    produits_financiers: 100,
+    produit_exceptionnel: 120,
+    charge_exceptionnelle: 450,
+    charges_financieres: 160 
+  }
+  const resultat = diane["interets"] / (diane["excedent_brut_d_exploitation"] + diane["produits_financiers"] + diane["produit_exceptionnel"] - diane["charge_exceptionnelle"] - diane["charges_financieres"] ) * 100
+  t.is(fraisFinancier(diane), resultat)
+})
+
+// $ node_modules/.bin/ava-ts ./reduce.algo2/fraisFinancier_tests.ts
