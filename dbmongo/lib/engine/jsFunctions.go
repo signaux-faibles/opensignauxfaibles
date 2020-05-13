f183c94fd6d315dcb6c1c97a717c4aac23d5d366
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-05-13 16:08:13 +0200

    go generate

--- dbmongo/lib/engine/jsFunctions.go
+++ dbmongo/lib/engine/jsFunctions.go
@@ -1515,15 +1515,31 @@ db.getCollection("Features").createIndex({
 }`,
 "fraisFinancier": `function fraisFinancier(diane) {
     "use strict";
-    if (("interets" in diane) && (diane["interets"] !== null) &&
-        ("excedent_brut_d_exploitation" in diane) && (diane["excedent_brut_d_exploitation"] !== null) &&
-        ("produits_financiers" in diane) && (diane["produits_financiers"] !== null) &&
-        ("charges_financieres" in diane) && (diane["charges_financieres"] !== null) &&
-        ("charge_exceptionnelle" in diane) && (diane["charge_exceptionnelle"] !== null) &&
-        ("produit_exceptionnel" in diane) && (diane["produit_exceptionnel"] !== null) &&
-        diane["excedent_brut_d_exploitation"] + diane["produits_financiers"] + diane["produit_exceptionnel"] - diane["charge_exceptionnelle"] - diane["charges_financieres"] != 0) {
-        return diane["interets"] / (diane["excedent_brut_d_exploitation"] + diane["produits_financiers"] + diane["produit_exceptionnel"] -
-            diane["charge_exceptionnelle"] - diane["charges_financieres"]) * 100;
+    if ("interets" in diane &&
+        diane["interets"] !== null &&
+        "excedent_brut_d_exploitation" in diane &&
+        diane["excedent_brut_d_exploitation"] !== null &&
+        "produits_financiers" in diane &&
+        diane["produits_financiers"] !== null &&
+        "charges_financieres" in diane &&
+        diane["charges_financieres"] !== null &&
+        "charge_exceptionnelle" in diane &&
+        diane["charge_exceptionnelle"] !== null &&
+        "produit_exceptionnel" in diane &&
+        diane["produit_exceptionnel"] !== null &&
+        diane["excedent_brut_d_exploitation"] +
+            diane["produits_financiers"] +
+            diane["produit_exceptionnel"] -
+            diane["charge_exceptionnelle"] -
+            diane["charges_financieres"] !=
+            0) {
+        return ((diane["interets"] /
+            (diane["excedent_brut_d_exploitation"] +
+                diane["produits_financiers"] +
+                diane["produit_exceptionnel"] -
+                diane["charge_exceptionnelle"] -
+                diane["charges_financieres"])) *
+            100);
     }
     else {
         return null;
