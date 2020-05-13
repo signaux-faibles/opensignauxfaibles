3d068945852e7001b8a008b6e0ada52c41d295e0
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-05-13 17:40:12 +0200

    ajout de commentaires pour les fonctions exportées

--- dbmongo/lib/engine/typescript.go
+++ dbmongo/lib/engine/typescript.go
@@ -15,6 +15,8 @@ func shouldTranspile(filePath string) bool {
 		path.Ext(filePath) == ".ts"
 }
 
+// ListTsFiles retourne la liste des fichiers TypeScript transpilable en JavaScript
+// en cherchant récursivement depuis le répertoire jsRootDir.
 func ListTsFiles(jsRootDir string) []string {
 	var files []string
 	err := filepath.Walk(jsRootDir, func(filePath string, info os.FileInfo, err error) error {
@@ -29,6 +31,8 @@ func ListTsFiles(jsRootDir string) []string {
 	return files
 }
 
+// DeleteTranspiledFiles supprime les fichiers JavaScript résultant de la
+// transpilation des fichiers TypeScript listés dans tsFiles.
 func DeleteTranspiledFiles(tsFiles []string) {
 	for _, tsFile := range tsFiles {
 		ext := path.Ext(tsFile)
@@ -43,6 +47,8 @@ func DeleteTranspiledFiles(tsFiles []string) {
 	}
 }
 
+// TranspileTsFunctions convertit les fichiers TypeScript listés dans tsFiles
+// au format JavaScript.
 func TranspileTsFunctions(tsFiles []string) {
 	cmd := exec.Command("npx", append([]string{"typescript", "--listFiles", "--lib", "es5", "--skipLibCheck", "--noImplicitUseStrict"}, tsFiles...)...) // output: .js files
 	cmd.Stdout = os.Stdout
