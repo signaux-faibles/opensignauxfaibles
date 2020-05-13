45bd8c3191f08b943a942f63abc6c9c9701b22d1
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-05-13 15:18:33 +0200

    fix failure in test_common.sh

--- dbmongo/js/common/raison_sociale.js
+++ dbmongo/js/common/raison_sociale.js
@@ -1,11 +1,12 @@
 function raison_sociale /*eslint-disable-line @typescript-eslint/no-unused-vars */(denomination_unite_legale, nom_unite_legale, nom_usage_unite_legale, prenom1_unite_legale, prenom2_unite_legale, prenom3_unite_legale, prenom4_unite_legale) {
     "use strict";
-    if (nom_usage_unite_legale === void 0) { nom_usage_unite_legale = ""; }
-    nom_usage_unite_legale = nom_usage_unite_legale + "/";
+    var nomUsageUniteLegale = nom_usage_unite_legale
+        ? nom_usage_unite_legale + "/"
+        : "";
     var raison_sociale = denomination_unite_legale ||
         (nom_unite_legale +
             "*" +
-            nom_usage_unite_legale +
+            nomUsageUniteLegale +
             prenom1_unite_legale +
             " " +
             (prenom2_unite_legale || "") +
--- dbmongo/js/common/raison_sociale.ts
+++ dbmongo/js/common/raison_sociale.ts
@@ -3,20 +3,22 @@ declare function debug(string) // supported by jsc, to print in stdout
 function raison_sociale /*eslint-disable-line @typescript-eslint/no-unused-vars */(
   denomination_unite_legale: string,
   nom_unite_legale: string,
-  nom_usage_unite_legale = "",
+  nom_usage_unite_legale: string,
   prenom1_unite_legale: string,
   prenom2_unite_legale: string,
   prenom3_unite_legale: string,
   prenom4_unite_legale: string
 ): string {
   "use strict"
-  nom_usage_unite_legale = nom_usage_unite_legale + "/"
+  const nomUsageUniteLegale = nom_usage_unite_legale
+    ? nom_usage_unite_legale + "/"
+    : ""
   const raison_sociale =
     denomination_unite_legale ||
     (
       nom_unite_legale +
       "*" +
-      nom_usage_unite_legale +
+      nomUsageUniteLegale +
       prenom1_unite_legale +
       " " +
       (prenom2_unite_legale || "") +
