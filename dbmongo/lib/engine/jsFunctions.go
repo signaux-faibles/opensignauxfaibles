f3f0d4c4426272b221e54e9979439506fa7ab15f
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-05-14 14:18:21 +0200

    go generate

--- dbmongo/lib/engine/jsFunctions.go
+++ dbmongo/lib/engine/jsFunctions.go
@@ -1308,34 +1308,43 @@ db.getCollection("Features").createIndex({
   
   
   `,
-"delais": `function delais (v, output_indexed) {
-  "use strict";
-  Object.keys(v.delai).map(
-    function (hash) {
-      var delai = v.delai[hash]
-      var date_creation = new Date(Date.UTC(delai.date_creation.getUTCFullYear(), delai.date_creation.getUTCMonth(), 1, 0, 0, 0, 0))
-      var date_echeance = new Date(Date.UTC(delai.date_echeance.getUTCFullYear(), delai.date_echeance.getUTCMonth(), 1, 0, 0, 0, 0))
-      var pastYearTimes = f.generatePeriodSerie(date_creation, date_echeance).map(function (date) { return date.getTime() })
-      pastYearTimes.map(
-        function(time){
-          if (time in output_indexed) {
-            var remaining_months = (date_echeance.getUTCMonth() - new Date(time).getUTCMonth()) +
-              12*(date_echeance.getUTCFullYear() - new Date(time).getUTCFullYear())
-            output_indexed[time].delai = remaining_months
-            output_indexed[time].duree_delai = delai.duree_delai
-            output_indexed[time].montant_echeancier = delai.montant_echeancier
+"delais": `// Object golang défini dans dbmongo/lib/urssaf/delai.go
 
-            if (delai.duree_delai > 0){
-              output_indexed[time].ratio_dette_delai = (output_indexed[time].montant_part_patronale +
-                output_indexed[time].montant_part_ouvriere - delai.montant_echeancier * remaining_months * 30 /
-                (delai.duree_delai)) /
-                delai.montant_echeancier
+
+
+function delais(v, output_indexed) {
+    "use strict";
+    Object.keys(v.delai).map(function (hash) {
+        var delai = v.delai[hash];
+        // On arrondit les dates au premier jour du mois.
+        var date_creation = new Date(Date.UTC(delai.date_creation.getUTCFullYear(), delai.date_creation.getUTCMonth(), 1, 0, 0, 0, 0));
+        var date_echeance = new Date(Date.UTC(delai.date_echeance.getUTCFullYear(), delai.date_echeance.getUTCMonth(), 1, 0, 0, 0, 0));
+        // Création d'un tableau de timestamps à raison de 1 par mois.
+        var pastYearTimes = exports.f
+            .generatePeriodSerie(date_creation, date_echeance)
+            .map(function (date) {
+            return date.getTime();
+        });
+        pastYearTimes.map(function (time) {
+            if (time in output_indexed) {
+                var remaining_months = date_echeance.getUTCMonth() -
+                    new Date(time).getUTCMonth() +
+                    12 *
+                        (date_echeance.getUTCFullYear() - new Date(time).getUTCFullYear());
+                output_indexed[time].delai = remaining_months;
+                output_indexed[time].duree_delai = delai.duree_delai;
+                output_indexed[time].montant_echeancier = delai.montant_echeancier;
+                if (delai.duree_delai > 0) {
+                    output_indexed[time].ratio_dette_delai =
+                        (output_indexed[time].montant_part_patronale +
+                            output_indexed[time].montant_part_ouvriere -
+                            (delai.montant_echeancier * remaining_months * 30) /
+                                delai.duree_delai) /
+                            delai.montant_echeancier;
+                }
             }
-          }
-        }
-      )
-    }
-  )
+        });
+    });
 }`,
 "detteFiscale": `function detteFiscale (diane){
   "use strict";
