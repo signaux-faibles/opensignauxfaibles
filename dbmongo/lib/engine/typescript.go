5a13606756927ad116eaf346f8be95713238e8f3
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-05-14 16:02:19 +0200

    fix: relative path error to js/globals.ts, when running go generate

--- dbmongo/lib/engine/typescript.go
+++ dbmongo/lib/engine/typescript.go
@@ -47,11 +47,9 @@ func DeleteTranspiledFiles(tsFiles []string) {
 	}
 }
 
-// TranspileTsFunctions convertit les fichiers TypeScript listés dans tsFiles
-// au format JavaScript.
-func TranspileTsFunctions(tsFiles []string) {
-	// cmd := exec.Command("npx", append([]string{"typescript", "--listFiles", "--lib", "es5", "--skipLibCheck", "--noImplicitUseStrict"}, tsFiles...)...) // output: .js files
-	cmd := exec.Command("npx", append([]string{"typescript", "--listFiles", "--p", "js/tsconfig.json"})...) // output: .js files
+// TranspileTsFunctions convertit les fichiers TypeScript au format JavaScript.
+func TranspileTsFunctions(jsRootDir string) {
+	cmd := exec.Command("npx", append([]string{"typescript", "--listFiles", "--p", filepath.Join(jsRootDir, "tsconfig.json")})...) // output: .js files
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err := cmd.Run()
