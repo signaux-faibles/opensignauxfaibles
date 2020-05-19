54baf06c2e4d511e1be52167637bab7f721db21d
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-05-19 12:31:28 +0200

    fix(TS): Passage de delais.js en TypeScript + tests (#49)
    Effectué en pair coding avec Pierre:
    
    - Renommage de delais.js en delais.ts
    - Définition du type `Delai` à partir de `dbmongo/lib/urssaf/delai.go`
    - Écriture de tests unitaires avec Ava
    
    Modifications effectuées ensuite pour que tous les tests passent:
    
    - Déclaration de la variable globale `f` dans le fichier `globals.ts` + inclusion dans `tsconfig.json` pour qu'il soit bien lu lors de la transpilation
    - Faire en sorte que `go generate` et `go test` fassent de même, en utilisant `tsconfig.json` au lieu de passer les préférences de transpilation sous forme de flags => ajout de ces flags dans `tsconfig.json` et changement de la signature de fonction `TranspileTsFiles()`
    - Formatage de `fakes.js`

---
+++ dbmongo/js/reduce.algo2/delais.ts
@@ -0,0 +1,73 @@
+// Object golang défini dans dbmongo/lib/urssaf/delai.go
+export type Delai = {
+  numero_compte: string
+  numero_contentieux: string
+  date_creation: Date
+  date_echeance: Date
+  duree_delai: number // nombre de jours entre date_creation et date_echeance
+  denomination: string
+  indic_6m: string
+  annee_creation: number
+  montant_echeancier: number // exprimé en euros
+  stade: string
+  action: string
+}
+
+export type DelaiMap = { [key: string]: Delai }
+
+export function delais(v: { delai: DelaiMap }, output_indexed: object): void {
+  "use strict"
+  Object.keys(v.delai).map(function (hash) {
+    const delai = v.delai[hash]
+    // On arrondit les dates au premier jour du mois.
+    const date_creation = new Date(
+      Date.UTC(
+        delai.date_creation.getUTCFullYear(),
+        delai.date_creation.getUTCMonth(),
+        1,
+        0,
+        0,
+        0,
+        0
+      )
+    )
+    const date_echeance = new Date(
+      Date.UTC(
+        delai.date_echeance.getUTCFullYear(),
+        delai.date_echeance.getUTCMonth(),
+        1,
+        0,
+        0,
+        0,
+        0
+      )
+    )
+    // Création d'un tableau de timestamps à raison de 1 par mois.
+    const pastYearTimes = f // eslint-disable-line no-undef
+      .generatePeriodSerie(date_creation, date_echeance)
+      .map(function (date) {
+        return date.getTime()
+      })
+    pastYearTimes.map(function (time) {
+      if (time in output_indexed) {
+        const remaining_months =
+          date_echeance.getUTCMonth() -
+          new Date(time).getUTCMonth() +
+          12 *
+            (date_echeance.getUTCFullYear() - new Date(time).getUTCFullYear())
+        output_indexed[time].delai = remaining_months
+        output_indexed[time].duree_delai = delai.duree_delai
+        output_indexed[time].montant_echeancier = delai.montant_echeancier
+
+        if (delai.duree_delai > 0) {
+          output_indexed[time].ratio_dette_delai =
+            (output_indexed[time].montant_part_patronale +
+              output_indexed[time].montant_part_ouvriere -
+              (delai.montant_echeancier * remaining_months * 30) /
+                delai.duree_delai) /
+            delai.montant_echeancier
+        }
+      }
+    })
+  })
+}
---
+++ dbmongo/js/reduce.algo2/delais_tests.ts
@@ -0,0 +1,94 @@
+import test from "ava"
+import "../globals"
+import { delais, Delai, DelaiMap } from "./delais"
+
+const janvier = new Date("2014-01-01")
+const fevrier = new Date("2014-02-01")
+const mars = new Date("2014-03-01")
+
+f = {
+  generatePeriodSerie: function (/*date_creation: Date, date_echeance: Date*/): Date[] {
+    return [janvier, fevrier, mars]
+  },
+}
+
+const nbDays = (firstDate: Date, secondDate: Date): number => {
+  const oneDay = 24 * 60 * 60 * 1000 // hours*minutes*seconds*milliseconds
+  return Math.round(
+    Math.abs((firstDate.getTime() - secondDate.getTime()) / oneDay)
+  )
+}
+
+const makeDelai = (firstDate: Date, secondDate: Date): Delai => ({
+  numero_compte: undefined,
+  numero_contentieux: undefined,
+  date_creation: firstDate,
+  date_echeance: secondDate,
+  duree_delai: nbDays(firstDate, secondDate),
+  denomination: undefined,
+  indic_6m: undefined,
+  annee_creation: undefined,
+  montant_echeancier: 1000,
+  stade: undefined,
+  action: undefined,
+})
+
+const testProperty = (
+  t: any,
+  propertyName: string,
+  expectedFebruary: number,
+  expectedMarch: number
+): void => {
+  const delaiTest = makeDelai(new Date("2014-01-03"), new Date("2014-03-05"))
+  const delaiMap: DelaiMap = {
+    abc: delaiTest,
+  }
+  const output_indexed = {}
+  output_indexed[fevrier.getTime()] = {
+    montant_part_patronale: 600, // TODO: n'inclure ces valeurs que dans les tests qui en ont besoin
+    montant_part_ouvriere: 0,
+  }
+  output_indexed[mars.getTime()] = {
+    montant_part_patronale: 600,
+    montant_part_ouvriere: 0,
+  }
+  delais({ delai: delaiMap }, output_indexed)
+  t.is(output_indexed[fevrier.getTime()][propertyName], expectedFebruary)
+  t.is(output_indexed[mars.getTime()][propertyName], expectedMarch)
+}
+
+test("la propriété delai représente le nombre de mois complets restants du délai", (t) => {
+  testProperty(t, "delai", 1, 0)
+})
+
+test("la propriété duree_delai représente la durée totale en jours du délai", (t) => {
+  const dureeEnJours = nbDays(new Date("2014-01-03"), new Date("2014-03-05"))
+  testProperty(t, "duree_delai", dureeEnJours, dureeEnJours)
+})
+
+test("la propriété montant_echeancier représente le montant en euros des cotisations sociales couvertes par le délai", (t) => {
+  testProperty(t, "montant_echeancier", 1000, 1000)
+})
+
+test.todo(
+  "la propriété ratio_dette_delai représente la déviation du remboursement de la dette par rapport à un remboursement linéaire sur la durée du délai"
+  /*(t) => {
+    // TODO: Inclure la formule dans la documentation de ce test
+    // TODO: populer montant_part_patronale et montant_part_ouvriere dans output_indexed
+    testProperty(t, "ratio_dette_delai", 1000, 1000)
+  }*/
+)
+
+test("un délai en dehors de la période d'intérêt est ignorée", (t) => {
+  const delaiTest = makeDelai(new Date("2013-01-03"), new Date("2013-03-05"))
+  const delaiMap: DelaiMap = {
+    abc: delaiTest,
+  }
+  const output_indexed = {}
+  output_indexed[fevrier.getTime()] = {}
+  delais({ delai: delaiMap }, output_indexed)
+  t.deepEqual(Object.keys(output_indexed), [fevrier.getTime().toString()])
+})
+
+// TODO: ajouter des tests sur les autres propriétés retournées
+// TODO: ajouter des tests sur les cas limites => table-based testing
