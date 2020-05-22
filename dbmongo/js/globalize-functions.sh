e2450fae85d27db9acc6c4445c672cdf0a38ee89
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-05-22 15:18:32 +0200

    fix: prevent "import" statements in JS files (not supported by JSC)

--- dbmongo/js/globalize-functions.sh
+++ dbmongo/js/globalize-functions.sh
@@ -3,3 +3,4 @@
 # We use perl because sed adds an empty line at the end of every js file,
 # which was adding changes to git's staging, while debugging failing tests.
 perl -pi'' -e 's/^export //' ./**/*.js
+perl -pi'' -e 's/^import .*$//g' ./**/*.js
