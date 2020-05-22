0a8be27e5f71ed973b701982726338abd68da648
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-05-22 11:48:25 +0200

    convert "compact" JS files to TS + fix errors

---
+++ dbmongo/js/compact/complete_reporder.ts
@@ -0,0 +1,35 @@
+export function complete_reporder(key, object) {
+  "use strict"
+  const batches = Object.keys(object.batch)
+  batches.sort()
+  const missing = {}
+  serie_periode.forEach((p) => {
+    missing[p.getTime()] = true
+  })
+
+  batches.forEach((batch) => {
+    const reporder = object.batch[batch].reporder || {}
+
+    Object.keys(reporder).forEach((ro) => {
+      if (!missing[reporder[ro].periode.getTime()]) {
+        delete object.batch[batch].reporder[ro]
+      } else {
+        missing[reporder[ro].periode.getTime()] = false
+      }
+    })
+  })
+
+  const lastBatch = batches[batches.length - 1]
+  serie_periode
+    .filter((p) => missing[p.getTime()])
+    .forEach((p) => {
+      const reporder_obj = object.batch[lastBatch].reporder || {}
+      reporder_obj[p.toString()] = {
+        random_order: Math.random(),
+        periode: p,
+        siret: key,
+      }
+      object.batch[lastBatch].reporder = reporder_obj
+    })
+  return object
+}
--- dbmongo/js/compact/finalize.ts
+++ dbmongo/js/compact/finalize.ts
@@ -1,23 +1,21 @@
-function finalize(k, o) {
-  "use strict";
-  o.index = {"algo1":false,
-    "algo2":false}
-
+export function finalize(k, o) {
+  "use strict"
+  o.index = { algo1: false, algo2: false }
 
   if (o.scope == "entreprise") {
     o.index.algo1 = true
     o.index.algo2 = true
   } else {
     // Est-ce que l'un des batchs a un effectif ?
-    var batches = Object.keys(o.batch)
-    batches.some(batch => {
-      let hasEffectif = Object.keys(o.batch[batch].effectif || {}).length > 0
+    const batches = Object.keys(o.batch)
+    batches.some((batch) => {
+      const hasEffectif = Object.keys(o.batch[batch].effectif || {}).length > 0
       o.index.algo1 = hasEffectif
       o.index.algo2 = hasEffectif
-      return (hasEffectif)
+      return hasEffectif
     })
     // Complete reporder if missing
     o = f.complete_reporder(k, o)
   }
-  return(o)
+  return o
 }
