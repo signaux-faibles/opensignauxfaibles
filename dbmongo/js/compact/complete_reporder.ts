0cedb2955bfa8b22194bd9c40da2a9ed3342f7ea
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-05-25 10:41:53 +0200

    added types in complete_reporder() => fixed lint issues

--- dbmongo/js/compact/complete_reporder.ts
+++ dbmongo/js/compact/complete_reporder.ts
@@ -1,6 +1,40 @@
 import "../globals.ts"
 
-export function complete_reporder(key, object) {
+type Siret = string
+
+type Scope = "etablissement" | "entreprise"
+
+type ReduceIndexFlags = {
+  algo1: boolean
+  algo2: boolean
+}
+
+type Periode = Date
+
+type RepOrder = {
+  random_order: number
+  periode: Periode
+  siret: Siret
+}
+
+type BatchValue = {
+  reporder: { [periode: string]: RepOrder }
+}
+
+type BatchValues = { [batchKey: string]: BatchValue }
+
+type RawDataValues = {
+  scope: Scope
+  index: ReduceIndexFlags
+  batch: BatchValues
+}
+
+// complete_reporder ajoute une propriété `reporder` pour chaque couple
+// SIRET+période, afin d'assurer la reproductibilité de l'échantillonage.
+export function complete_reporder(
+  siret: Siret,
+  object: RawDataValues
+): RawDataValues {
   "use strict"
   const batches = Object.keys(object.batch)
   batches.sort()
@@ -29,7 +63,7 @@ export function complete_reporder(key, object) {
       reporder_obj[p.toString()] = {
         random_order: Math.random(),
         periode: p,
-        siret: key,
+        siret: siret,
       }
       object.batch[lastBatch].reporder = reporder_obj
     })
