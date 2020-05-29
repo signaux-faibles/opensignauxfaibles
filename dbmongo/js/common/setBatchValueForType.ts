eaf5752eab7de069e37c3711a7aab24dfca86157
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-05-29 17:53:29 +0200

    fix codacy warning "Unnecessary semicolon" (because the semicolon was automatically added by eslint+prettier on save...) https://app.codacy.com/gh/signaux-faibles/opensignauxfaibles/file/45181146929/issues/source?bid=18037842&fileBranchId=18308380#l26

--- dbmongo/js/common/setBatchValueForType.ts
+++ dbmongo/js/common/setBatchValueForType.ts
@@ -23,6 +23,7 @@ export function setBatchValueForType(
       // This switch should be exhaustive: cover all the keys defined in the BatchValue type.
       // => Warning TS(2345) if we miss a case, e.g. Argument of type '"new_effectif"' is not assignable to parameter of type 'never'.
       // source: https://stackoverflow.com/a/61806149/592254
+      // eslint-disable-next-line no-extra-semi
       ;((caseVal: never): void => {
         throw new Error(`case "${caseVal}" should be added to switch`)
       })(typeName)
