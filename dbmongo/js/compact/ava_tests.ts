dd8510d4cbaf53ee8bdf9b8410f29e951523e43d
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-06-01 15:13:14 +0200

    🧹 reorder functions + update comments

--- dbmongo/js/compact/ava_tests.ts
+++ dbmongo/js/compact/ava_tests.ts
@@ -24,6 +24,15 @@ const removeRandomOrder = (obj: object): object => {
   return obj
 }
 
+const runMongoMap = (mapFct: () => void, keyVal: object): object => {
+  const results = {}
+  globalThis.emit = (key: string, value: any): void => {
+    results[key] = value
+  }
+  mapFct.call(keyVal)
+  return results
+}
+
 // input data from test-api.sh
 const importedData = {
   _id: "random123abc",
@@ -39,7 +48,7 @@ const importedData = {
   },
 }
 
-// output data from test-api.sh
+// output data inspired by test-api.sh
 const expected = [
   {
     _id: "01234567891011",
@@ -67,15 +76,6 @@ const expected = [
     },
   },
 ]
-
-const runMongoMap = (mapFct: () => void, keyVal: object): object => {
-  const results = {}
-  globalThis.emit = (key: string, value: any): void => {
-    results[key] = value
-  }
-  mapFct.call(keyVal)
-  return results
-}
 
 test(`exécution complète de la chaine "compact"`, (t: ExecutionContext) => {
   // 1. map
@@ -114,7 +114,7 @@ test(`exécution complète de la chaine "compact"`, (t: ExecutionContext) => {
   const global = globalThis as any
   global.serie_periode = f.generatePeriodSerie(
     ISODate("2015-12-01T00:00:00.000+0000"),
-    ISODate("2016-02-01T00:00:00.000+0000")
+    ISODate("2016-01-31T00:00:00.000+0000")
   )
   const index: ReduceIndexFlags = { algo1: true, algo2: true }
   const finalizeKey = reduceKey
@@ -124,23 +124,4 @@ test(`exécution complète de la chaine "compact"`, (t: ExecutionContext) => {
     { _id: finalizeKey, value: removeRandomOrder(finalizeResultValue) },
   ]
   t.deepEqual(finalizeResults, expected as unknown)
-  // => sample of `actual` VS `expected`:
-  //   -             'Tue Oct 01 2019 02:00:00 GMT+0200 (GMT+02:00)': {
-  //   -               periode: Date 2019-10-01 00:00:00 UTC {},
-  //   -               random_order: 0.19479352943685613,
-  //   -               siret: '01234567891011',
-  //   -             },
-  //   -             'Wed Jan 01 2014 01:00:00 GMT+0100 (GMT+01:00)': {
-  //   -               periode: Date 2014-01-01 00:00:00 UTC {},
-  //   -               random_order: 0.6133162030905268,
-  //   -               siret: '01234567891011',
-  //   -             },
-  //   +             'Fri Apr 01 2016 00:00:00 GMT+0000 (UTC)': {
-  //   +               periode: Date 2016-04-01 00:00:00 UTC {},
-  //   +               siret: '01234567891011',
-  //   +             },
-  //   +             'Fri Aug 01 2014 00:00:00 GMT+0000 (UTC)': {
-  //   +               periode: Date 2014-08-01 00:00:00 UTC {},
-  //   +               siret: '01234567891011',
-  //   +             },
 })
