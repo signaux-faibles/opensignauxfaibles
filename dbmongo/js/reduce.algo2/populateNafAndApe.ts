23a3211b08971e0cc633735556a57d2b419e0085
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-06-16 15:21:50 +0200

    populateNafAndApe.js --> TS

--- dbmongo/js/reduce.algo2/populateNafAndApe.ts
+++ dbmongo/js/reduce.algo2/populateNafAndApe.ts
@@ -1,11 +1,40 @@
-function populateNafAndApe(output_indexed, naf) {
+type CodeAPE = string
+type CodeAPENiveau2 = string
+type CodeAPENiveau3 = string
+type CodeAPENiveau4 = string
+type CodeNAF = string
+
+type Output = {
+  code_ape: CodeAPE
+  code_naf: CodeNAF
+  libelle_naf: string
+  code_ape_niveau2: CodeAPENiveau2
+  code_ape_niveau3: CodeAPENiveau3
+  code_ape_niveau4: CodeAPENiveau4
+  libelle_ape2: string
+  libelle_ape3: string
+  libelle_ape4: string
+  libelle_ape5: string
+}
+
+export function populateNafAndApe(
+  output_indexed: { [k: string]: Output },
+  naf: {
+    n5to1: Record<CodeAPE, CodeNAF>
+    n1: Record<CodeNAF, string>
+    n2: Record<CodeAPENiveau2, string>
+    n3: Record<CodeAPENiveau3, string>
+    n4: Record<CodeAPENiveau4, string>
+    n5: Record<CodeAPE, string>
+  }
+): void {
   "use strict"
   Object.keys(output_indexed).forEach((k) => {
     if (
       "code_ape" in output_indexed[k] &&
       output_indexed[k].code_ape !== null
     ) {
-      var code_ape = output_indexed[k].code_ape
+      const code_ape = output_indexed[k].code_ape
       output_indexed[k].code_naf = naf.n5to1[code_ape]
       output_indexed[k].libelle_naf = naf.n1[output_indexed[k].code_naf]
       output_indexed[k].code_ape_niveau2 = code_ape.substring(0, 2)
@@ -21,5 +50,3 @@ function populateNafAndApe(output_indexed, naf) {
     }
   })
 }
-
-exports.populateNafAndApe = populateNafAndApe
