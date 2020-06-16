08f5a7b2a2eb4e313625e7f285bda0e17b868696
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-06-16 16:05:24 +0200

    sirene_ul.js + dependances --> TS

---
+++ dbmongo/js/reduce.algo2/sirene_ul.ts
@@ -0,0 +1,54 @@
+import * as f from "../common/raison_sociale"
+
+type SireneUL = {
+  raison_sociale: string
+  nom_unite_legale: string
+  nom_usage_unite_legale: string
+  prenom1_unite_legale: string
+  prenom2_unite_legale: string
+  prenom3_unite_legale: string
+  prenom4_unite_legale: string
+  statut_juridique: unknown
+  date_creation: Date
+}
+
+type V = {
+  sirene_ul: Record<string, SireneUL>
+}
+
+type MutableOutputArray = {
+  periode: Date
+  raison_sociale: unknown
+  statut_juridique: unknown
+  date_creation_entreprise: number | null // année
+  age_entreprise: number | null // en années
+}[]
+
+export function sirene_ul(v: V, output_array: MutableOutputArray): void {
+  "use strict"
+  const sireneHashes = Object.keys(v.sirene_ul || {})
+  output_array.forEach((val) => {
+    if (sireneHashes.length != 0) {
+      const sirene = v.sirene_ul[sireneHashes[sireneHashes.length - 1]]
+      val.raison_sociale = f.raison_sociale(
+        sirene.raison_sociale,
+        sirene.nom_unite_legale,
+        sirene.nom_usage_unite_legale,
+        sirene.prenom1_unite_legale,
+        sirene.prenom2_unite_legale,
+        sirene.prenom3_unite_legale,
+        sirene.prenom4_unite_legale
+      )
+      val.statut_juridique = sirene.statut_juridique || null
+      val.date_creation_entreprise = sirene.date_creation
+        ? sirene.date_creation.getFullYear()
+        : null
+      if (val.date_creation_entreprise) {
+        val.age_entreprise =
+          sirene.date_creation && sirene.date_creation >= new Date("1901/01/01")
+            ? val.periode.getFullYear() - val.date_creation_entreprise
+            : null
+      }
+    }
+  })
+}
