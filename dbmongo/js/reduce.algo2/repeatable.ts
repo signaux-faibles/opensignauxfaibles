ae69d088c06a1440d7a13b94cd5c67493985900b
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-06-16 15:34:29 +0200

    repeatable.js --> TS

---
+++ dbmongo/js/reduce.algo2/repeatable.ts
@@ -0,0 +1,14 @@
+export function repeatable(
+  rep: Record<string, { periode: Periode; random_order: number }>
+): Record<string, { random_order: number }> {
+  "use strict"
+  const output_repeatable = {} as Record<string, { random_order: number }>
+  Object.keys(rep).forEach((hash) => {
+    const one_rep = rep[hash]
+    const periode = one_rep.periode.getTime()
+    output_repeatable[periode] = output_repeatable[periode] || {}
+    output_repeatable[periode].random_order = one_rep.random_order
+  })
+
+  return output_repeatable
+}
--- dbmongo/lib/engine/jsFunctions.go
+++ dbmongo/lib/engine/jsFunctions.go
@@ -2169,16 +2169,15 @@ db.getCollection("Features").createIndex({
     }, {});
 }`,
 "repeatable": `function repeatable(rep) {
-  "use strict"
-  let output_repeatable = {}
-  Object.keys(rep).forEach((hash) => {
-    var one_rep = rep[hash]
-    var periode = one_rep.periode.getTime()
-    output_repeatable[periode] = output_repeatable[periode] || {}
-    output_repeatable[periode].random_order = one_rep.random_order
-  })
-
-  return output_repeatable
+    "use strict";
+    const output_repeatable = {};
+    Object.keys(rep).forEach((hash) => {
+        const one_rep = rep[hash];
+        const periode = one_rep.periode.getTime();
+        output_repeatable[periode] = output_repeatable[periode] || {};
+        output_repeatable[periode].random_order = one_rep.random_order;
+    });
+    return output_repeatable;
 }`,
 "sirene": `function sirene(v, output_array) {
   "use strict"
