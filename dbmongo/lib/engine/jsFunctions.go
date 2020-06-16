c239981fed2d8a90d43f078ca99c763ca2b04c48
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-06-16 18:42:01 +0200

    go generate

--- dbmongo/lib/engine/jsFunctions.go
+++ dbmongo/lib/engine/jsFunctions.go
@@ -1610,43 +1610,34 @@ db.getCollection("Features").createIndex({
     }
 }`,
 "flatten": `function flatten(v, actual_batch) {
-  "use strict"
-  var res = Object.keys(v.batch || {})
-    .sort()
-    .filter((batch) => batch <= actual_batch)
-    .reduce(
-      (m, batch) => {
+    "use strict";
+    const res = Object.keys(v.batch || {})
+        .sort()
+        .filter((batch) => batch <= actual_batch)
+        .reduce((m, batch) => {
         // Types intéressants = nouveaux types, ou types avec suppressions
-        var delete_types = Object.keys(
-          (v.batch[batch].compact || {}).delete || {}
-        )
-        var new_types = Object.keys(v.batch[batch])
-        var all_interesting_types = [
-          ...new Set([...delete_types, ...new_types]),
-        ]
-
+        const delete_types = Object.keys((v.batch[batch].compact || {}).delete || {});
+        const new_types = Object.keys(v.batch[batch]);
+        const all_interesting_types = [
+            ...new Set([...delete_types, ...new_types]),
+        ];
         all_interesting_types.forEach((type) => {
-          m[type] = m[type] || {}
-          // On supprime les clés qu'il faut
-          if (
-            v.batch[batch] &&
-            v.batch[batch].compact &&
-            v.batch[batch].compact.delete &&
-            v.batch[batch].compact.delete[type] &&
-            v.batch[batch].compact.delete[type] != {}
-          ) {
-            v.batch[batch].compact.delete[type].forEach((hash) => {
-              delete m[type][hash]
-            })
-          }
-          Object.assign(m[type], v.batch[batch][type])
-        })
-        return m
-      },
-      { key: v.key, scope: v.scope }
-    )
-
-  return res
+            m[type] = m[type] || {};
+            // On supprime les clés qu'il faut
+            if (v.batch[batch] &&
+                v.batch[batch].compact &&
+                v.batch[batch].compact.delete &&
+                v.batch[batch].compact.delete[type]) {
+                v.batch[batch].compact.delete[type].forEach((hash) => {
+                    if (typeof m[type] === "object" && m[type][hash])
+                        delete m[type][hash];
+                });
+            }
+            Object.assign(m[type], v.batch[batch][type]);
+        });
+        return m;
+    }, { key: v.key, scope: v.scope });
+    return res;
 }`,
 "fraisFinancier": `function fraisFinancier(diane) {
     "use strict";
