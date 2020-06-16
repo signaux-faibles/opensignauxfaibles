02d56170b41498ad2fb44ce2a190566fae8150f6
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-06-16 14:53:08 +0200

    cibleApprentissage.js --> TS

--- dbmongo/js/reduce.algo2/cibleApprentissage.d.ts
+++
@@ -1,4 +0,0 @@
-export function cibleApprentissage(
-  output_indexed: { [k: string]: { tag_failure; tag_default } },
-  n_months: number
-): { [k: string]: { time_til_default; time_til_failure } }
--- dbmongo/lib/engine/jsFunctions.go
+++ dbmongo/lib/engine/jsFunctions.go
@@ -1034,45 +1034,31 @@ db.getCollection("Features").createIndex({
   })
 }`,
 "cibleApprentissage": `function cibleApprentissage(output_indexed, n_months) {
-  "use strict"
-
-  // Mock two input instead of one for future modification
-  var output_cotisation = output_indexed
-  var output_procol = output_indexed
-  // replace with const
-  var all_keys = Object.keys(output_indexed)
-  //
-
-  var merged_info = all_keys.reduce(function (m, k) {
-    m[k] = {
-      outcome: Boolean(
-        output_procol[k].tag_failure || output_cotisation[k].tag_default
-      ),
-    }
-    return m
-  }, {})
-
-  var output_outcome = f.lookAhead(merged_info, "outcome", n_months, true)
-  var output_default = f.lookAhead(
-    output_cotisation,
-    "tag_default",
-    n_months,
-    true
-  )
-  var output_failure = f.lookAhead(output_procol, "tag_failure", n_months, true)
-
-  var output_cible = all_keys.reduce(function (m, k) {
-    m[k] = {}
-
-    if (output_outcome[k]) m[k] = output_outcome[k]
-    if (output_default[k])
-      m[k].time_til_default = output_default[k].time_til_outcome
-    if (output_failure[k])
-      m[k].time_til_failure = output_failure[k].time_til_outcome
-    return m
-  }, {})
-
-  return output_cible
+    "use strict";
+    // Mock two input instead of one for future modification
+    const output_cotisation = output_indexed;
+    const output_procol = output_indexed;
+    // replace with const
+    const all_keys = Object.keys(output_indexed);
+    //
+    const merged_info = all_keys.reduce(function (m, k) {
+        m[k] = {
+            outcome: Boolean(output_procol[k].tag_failure || output_cotisation[k].tag_default),
+        };
+        return m;
+    }, {});
+    const output_outcome = f.lookAhead(merged_info, "outcome", n_months, true);
+    const output_default = f.lookAhead(output_cotisation, "tag_default", n_months, true);
+    const output_failure = f.lookAhead(output_procol, "tag_failure", n_months, true);
+    const output_cible = all_keys.reduce(function (m, k) {
+        const outputTimes = {};
+        if (output_default[k])
+            outputTimes.time_til_default = output_default[k].time_til_outcome;
+        if (output_failure[k])
+            outputTimes.time_til_failure = output_failure[k].time_til_outcome;
+        return Object.assign(Object.assign({}, m), { [k]: Object.assign(Object.assign({}, output_outcome[k]), outputTimes) });
+    }, {});
+    return output_cible;
 }`,
 "compareDebit": `function compareDebit(a, b) {
   "use strict"
