ee8e783e26d8c7bf73c8e9ccf10947355c6e2764
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-06-29 11:47:02 +0200

    wip: extraction de applyPatchesToBatch

---
+++ dbmongo/js/compact/applyPatchesToBatch.ts
@@ -0,0 +1,33 @@
+import "../globals"
+
+type DataType = string // TODO: use BatchDataType instead
+
+export function applyPatchesToBatch(
+  hashToAdd: Record<DataType, Set<DataHash>>,
+  hashToDelete: Record<DataType, Set<DataHash>>,
+  stockTypes: DataType[],
+  currentBatch: BatchValue
+) {
+  // 5. On met à jour reduced_value
+  // -------------------------------
+  stockTypes.forEach((type) => {
+    if (hashToDelete[type]) {
+      currentBatch.compact = currentBatch.compact || { delete: {} }
+      currentBatch.compact.delete = currentBatch.compact.delete || {}
+      currentBatch.compact.delete[type] = [...hashToDelete[type]]
+    }
+  })
+
+  // filtrage des données en fonction de new_types et hashToAdd
+  type AllValueTypesButCompact = Exclude<keyof BatchValue, "compact">
+  const typesToAdd = Object.keys(hashToAdd) as AllValueTypesButCompact[]
+  typesToAdd.forEach((type) => {
+    currentBatch[type] = [...hashToAdd[type]].reduce(
+      (typedBatchValues, hash) => ({
+        ...typedBatchValues,
+        [hash]: currentBatch[type]?.[hash],
+      }),
+      {}
+    )
+  })
+}
