02719ea2f630a25af35c055db5b9fd12a25a9ee8
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-07-06 10:48:12 +0200

    utiliser deepEqual() au lieu de compare() + error_expected est inutile

--- dbmongo/js/reduce.algo2/add_tests.ts
+++ dbmongo/js/reduce.algo2/add_tests.ts
@@ -6,25 +6,21 @@ const testCases = [
   {
     data: { "2015-01-01": { any_value: true } },
     data_to_add: {},
-    error_expected: true,
-    expected: null,
+    expected: { "2015-01-01": { any_value: true } },
   },
   {
     data: { "2015-01-01": { any_value: true } },
     data_to_add: { "2015-01-01": {} },
-    error_expected: false,
     expected: { "2015-01-01": { any_value: true } },
   },
   {
     data: { "2015-01-01": { any_value: true } },
     data_to_add: { "2015-01-01": { any_value: false } },
-    error_expected: false,
     expected: { "2015-01-01": { any_value: false } },
   },
   {
     data: { "2015-01-01": { any_value: true } },
     data_to_add: { "2015-01-01": { other_value: false } },
-    error_expected: false,
     expected: { "2015-01-01": { any_value: true, other_value: false } },
   },
   {
@@ -36,7 +32,6 @@ const testCases = [
       "2015-01-01": { other_value: false },
       "2015-02-01": { other_value: false },
     },
-    error_expected: false,
     expected: {
       "2015-01-01": { any_value: true, other_value: false },
       "2015-02-01": { any_value: true, other_value: false },
@@ -46,23 +41,7 @@ const testCases = [
 
 testCases.forEach(({ expected, ...testCase }, number) => {
   test.serial(`add(): case #${number}`, (t: ExecutionContext) => {
-    if (testCase.error_expected) {
-      const result = add(testCase["data_to_add"], testCase["data"])
-      t.is(typeof result, "undefined")
-    }
-    if (expected) {
-      add(testCase["data_to_add"], testCase["data"])
-      t.true(compare(testCase["data"], expected))
-    }
+    add(testCase["data_to_add"], testCase["data"])
+    t.deepEqual(testCase["data"], expected)
   })
 })
-
-function compare(a: any, b: any) {
-  if (Object.keys(a).length !== Object.keys(b).length) {
-    return false
-  }
-  const equal = Object.keys(a).every(function (k) {
-    return JSON.stringify(a[k]) === JSON.stringify(b[k])
-  })
-  return equal
-}
