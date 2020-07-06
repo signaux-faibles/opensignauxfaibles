c471247c2b9cbb2c58a7737018d77872ae6195d4
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-07-06 10:36:55 +0200

    add_test.js --> add_tests.ts

---
+++ dbmongo/js/reduce.algo2/add_tests.ts
@@ -0,0 +1,68 @@
+import test, { ExecutionContext } from "ava"
+import "../globals"
+import { add } from "./add"
+
+const testCases = [
+  {
+    data: { "2015-01-01": { any_value: true } },
+    data_to_add: {},
+    error_expected: true,
+    expected: null,
+  },
+  {
+    data: { "2015-01-01": { any_value: true } },
+    data_to_add: { "2015-01-01": {} },
+    error_expected: false,
+    expected: { "2015-01-01": { any_value: true } },
+  },
+  {
+    data: { "2015-01-01": { any_value: true } },
+    data_to_add: { "2015-01-01": { any_value: false } },
+    error_expected: false,
+    expected: { "2015-01-01": { any_value: false } },
+  },
+  {
+    data: { "2015-01-01": { any_value: true } },
+    data_to_add: { "2015-01-01": { other_value: false } },
+    error_expected: false,
+    expected: { "2015-01-01": { any_value: true, other_value: false } },
+  },
+  {
+    data: {
+      "2015-01-01": { any_value: true },
+      "2015-02-01": { any_value: true },
+    },
+    data_to_add: {
+      "2015-01-01": { other_value: false },
+      "2015-02-01": { other_value: false },
+    },
+    error_expected: false,
+    expected: {
+      "2015-01-01": { any_value: true, other_value: false },
+      "2015-02-01": { any_value: true, other_value: false },
+    },
+  },
+]
+
+testCases.forEach(({ expected, ...testCase }, number) => {
+  test.serial(`add(): case #${number}`, (t: ExecutionContext) => {
+    if (testCase.error_expected) {
+      const result = add(testCase["data_to_add"], testCase["data"])
+      t.is(typeof result, "undefined")
+    } else {
+      // TODO: also run this check when error_expected is present ?
+      add(testCase["data_to_add"], testCase["data"])
+      t.true(compare(testCase["data"], expected))
+    }
+  })
+})
+
+function compare(a: any, b: any) {
+  if (Object.keys(a).length !== Object.keys(b).length) {
+    return false
+  }
+  const equal = Object.keys(a).every(function (k) {
+    return JSON.stringify(a[k]) === JSON.stringify(b[k])
+  })
+  return equal
+}
