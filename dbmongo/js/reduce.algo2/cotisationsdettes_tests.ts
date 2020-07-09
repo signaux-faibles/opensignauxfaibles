8766227fb65156654d8e7cff69e168b36c139351
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-07-09 17:38:33 +0200

    tests plus concis

--- dbmongo/js/reduce.algo2/cotisationsdettes_tests.ts
+++ dbmongo/js/reduce.algo2/cotisationsdettes_tests.ts
@@ -1,23 +1,22 @@
 import "../globals"
 import test, { ExecutionContext } from "ava"
-import { cotisationsdettes, SortieCotisationsDettes } from "./cotisationsdettes"
+import { cotisationsdettes } from "./cotisationsdettes"
 
 test("La variable cotisation représente les cotisations sociales dues à une période donnée", (t: ExecutionContext) => {
   const date = new Date("2018-01-01")
   const datePlusUnMois = new Date("2018-02-01")
-  const periode = { start: date, end: datePlusUnMois }
+
   const v: DonnéesCotisation & DonnéesDebit = {
     cotisation: {
       hash1: {
-        periode,
+        periode: { start: date, end: datePlusUnMois },
         du: 100,
       },
     },
     debit: {},
   }
-  const actual: Record<number, SortieCotisationsDettes> = cotisationsdettes(v, [
-    date.getTime(),
-  ])
+
+  const actual = cotisationsdettes(v, [date.getTime()])
 
   const expected = {
     [date.getTime()]: {
