6a649365c63863f9ec0e3ed9a74711a570c600ca
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-07-13 11:50:11 +0200

    green: test du report des dettes

--- dbmongo/js/reduce.algo2/cotisationsdettes_tests.ts
+++ dbmongo/js/reduce.algo2/cotisationsdettes_tests.ts
@@ -1,6 +1,6 @@
 import "../globals"
 import test, { ExecutionContext } from "ava"
-import { cotisationsdettes, SortieCotisationsDettes } from "./cotisationsdettes"
+import { cotisationsdettes } from "./cotisationsdettes"
 import { generatePeriodSerie } from "../common/generatePeriodSerie"
 import { dateAddMonth } from "./dateAddMonth"
 
@@ -67,42 +67,6 @@ test.only("Le montant de dette d'une période est reporté dans les périodes su
   }
 
   const actual = cotisationsdettes(v, periode)
-
-  const montants = {
-    montant_part_ouvriere: 100,
-    montant_part_patronale: 200,
-    interessante_urssaf: false,
-  }
-
-  const montantsUnMois = {
-    ...montants,
-    montant_part_ouvriere_past_1: 100,
-    montant_part_patronale_past_1: 200,
-  }
-
-  const montantsDeuxMois = {
-    ...montantsUnMois,
-    montant_part_ouvriere_past_2: 100,
-    montant_part_patronale_past_2: 200,
-  }
-
-  const montantsTroisMois = {
-    ...montantsDeuxMois,
-    montant_part_ouvriere_past_3: 100,
-    montant_part_patronale_past_3: 200,
-  }
-
-  const montantsSixMois = {
-    ...montantsTroisMois,
-    montant_part_ouvriere_past_6: 100,
-    montant_part_patronale_past_6: 200,
-  }
-
-  const montantsDouzeMois = {
-    ...montantsSixMois,
-    montant_part_ouvriere_past_12: 100,
-    montant_part_patronale_past_12: 200,
-  }
 
   const expectedMontantPartOuvrière = [
     100,
@@ -212,30 +176,30 @@ test.only("Le montant de dette d'une période est reporté dans les périodes su
     t.deepEqual(actual[dateAddMonth(dateDebut, période).getTime()], expected)
   }
 
-  t.deepEqual(actual[dateAddMonth(dateDebut, 1).getTime()], montantsUnMois)
-  t.deepEqual(actual[dateAddMonth(dateDebut, 2).getTime()], montantsDeuxMois)
-  t.deepEqual(actual[dateAddMonth(dateDebut, 3).getTime()], montantsTroisMois)
-  t.deepEqual(actual[dateAddMonth(dateDebut, 4).getTime()], {
-    ...montantsTroisMois,
-    montant_part_ouvriere: 0,
-    montant_part_patronale: 0,
-  } as SortieCotisationsDettes)
-  t.deepEqual(actual[dateAddMonth(dateDebut, 5).getTime()], {
-    ...montantsTroisMois,
-    montant_part_ouvriere: 0,
-    montant_part_patronale: 0,
-  } as SortieCotisationsDettes)
-  t.deepEqual(actual[dateAddMonth(dateDebut, 6).getTime()], {
-    ...montantsSixMois,
-    montant_part_ouvriere: 0,
-    montant_part_patronale: 0,
-  } as SortieCotisationsDettes)
-  t.deepEqual(actual[dateAddMonth(dateDebut, 7).getTime()], montantsSixMois)
-  t.deepEqual(actual[dateAddMonth(dateDebut, 8).getTime()], montantsSixMois)
-  t.deepEqual(actual[dateAddMonth(dateDebut, 9).getTime()], montantsSixMois)
-  t.deepEqual(actual[dateAddMonth(dateDebut, 10).getTime()], montantsSixMois)
-  t.deepEqual(actual[dateAddMonth(dateDebut, 11).getTime()], montantsSixMois)
-  t.deepEqual(actual[dateAddMonth(dateDebut, 12).getTime()], montantsDouzeMois)
+  // t.deepEqual(actual[dateAddMonth(dateDebut, 1).getTime()], montantsUnMois)
+  // t.deepEqual(actual[dateAddMonth(dateDebut, 2).getTime()], montantsDeuxMois)
+  // t.deepEqual(actual[dateAddMonth(dateDebut, 3).getTime()], montantsTroisMois)
+  // t.deepEqual(actual[dateAddMonth(dateDebut, 4).getTime()], {
+  //   ...montantsTroisMois,
+  //   montant_part_ouvriere: 0,
+  //   montant_part_patronale: 0,
+  // } as SortieCotisationsDettes)
+  // t.deepEqual(actual[dateAddMonth(dateDebut, 5).getTime()], {
+  //   ...montantsTroisMois,
+  //   montant_part_ouvriere: 0,
+  //   montant_part_patronale: 0,
+  // } as SortieCotisationsDettes)
+  // t.deepEqual(actual[dateAddMonth(dateDebut, 6).getTime()], {
+  //   ...montantsSixMois,
+  //   montant_part_ouvriere: 0,
+  //   montant_part_patronale: 0,
+  // } as SortieCotisationsDettes)
+  // t.deepEqual(actual[dateAddMonth(dateDebut, 7).getTime()], montantsSixMois)
+  // t.deepEqual(actual[dateAddMonth(dateDebut, 8).getTime()], montantsSixMois)
+  // t.deepEqual(actual[dateAddMonth(dateDebut, 9).getTime()], montantsSixMois)
+  // t.deepEqual(actual[dateAddMonth(dateDebut, 10).getTime()], montantsSixMois)
+  // t.deepEqual(actual[dateAddMonth(dateDebut, 11).getTime()], montantsSixMois)
+  // t.deepEqual(actual[dateAddMonth(dateDebut, 12).getTime()], montantsDouzeMois)
 })
 
 test("interessante_urssaf est vrai quand l'entreprise n'a pas eu de débit (dette) sur les 6 derniers mois", (t: ExecutionContext) => {
