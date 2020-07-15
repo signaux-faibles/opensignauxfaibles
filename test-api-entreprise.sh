347662b9faa8af203634b59ee692fc49800d29e9
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-07-15 12:04:35 +0200

    test avec des données non confidentielles

--- test-api-entreprise.sh
+++ test-api-entreprise.sh
@@ -3,9 +3,6 @@
 # Test de bout en bout de l'API /datapi/exportEntreprise.
 #
 # Inspiré de test-api.sh.
-#
-# Ce test requiert l'accès à un serveur privé, et n'est donc pas inclus dans la
-# suite de tests exécutée en Integration Continue.
 
 # Interrompre le conteneur Docker d'une exécution précédente de ce test, si besoin
 docker stop sf-mongodb
@@ -16,10 +13,6 @@ set -e # will stop the script if any command fails with a non-zero exit code
 DATA_DIR=$(pwd)/tmp-opensignauxfaibles-data-raw
 mkdir -p "${DATA_DIR}"
 trap "{ killall dbmongo; [ -f config.toml ] && rm config.toml; [ -f config.backup.toml ] && mv config.backup.toml config.toml; docker stop sf-mongodb; rm -rf ${DATA_DIR}; echo \"✨ Cleaned up temp directory\"; }" EXIT
-
-echo ""
-echo "🚚 Downloading realistic data set..."
-scp "stockage:/home/centos/opensignauxfaibles_tests/*" "${DATA_DIR}/"
 
 echo ""
 echo "🐳 Starting MongoDB container..."
@@ -39,33 +32,9 @@ cp config-sample.toml config.toml
 perl -pi'' -e "s,/foo/bar/data-raw,sample-data-raw," config.toml
 
 echo ""
-echo "📄 Inserting test data..."
+echo "📝 Inserting test data..."
 sleep 1 # give some time for MongoDB to start
 cat > "${DATA_DIR}/db_popul.js" << CONTENTS
-  db.Scores.remove({})
-  db.Scores.insertMany([
-    {
-        "_id": "score1",
-        "siret" : "01234567891011",
-        "periode" : "2014-01-01",
-        "score" : 0.97,
-        "batch" : "2002_1",
-        "timestamp" : ISODate("2014-01-01T00:00:00.000+0000"),
-        "algo" : "algo_avec_urssaf",
-        "alert" : "Alerte seuil F1"
-    },
-    {
-        "_id": "score2",
-        "siret" : "01234567891011",
-        "periode" : "2015-01-01",
-        "score" : 0.98,
-        "batch" : "2002_1",
-        "timestamp" : ISODate("2015-01-01T00:00:00.000+0000"),
-        "algo" : "algo_avec_urssaf",
-        "alert" : "Alerte seuil F1"
-    },
-  ])
-
   db.Admin.remove({})
   db.Admin.insertOne({
     "_id" : {
@@ -81,31 +50,87 @@ cat > "${DATA_DIR}/db_popul.js" << CONTENTS
     ],
     "param" : {
         "date_debut" : ISODate("2014-01-01T00:00:00.000+0000"),
-        "date_fin" : ISODate("2016-01-01T00:00:00.000+0000"),
-        "date_fin_effectif" : ISODate("2016-03-01T00:00:00.000+0000")
+        "date_fin" : ISODate("2014-03-01T00:00:00.000+0000"),
+        "date_fin_effectif" : ISODate("2014-03-01T00:00:00.000+0000")
     },
     "name" : "TestData"
   })
 
-  db.RawData.remove({})
-  db.RawData.insertMany(
-CONTENTS
-cat >> "${DATA_DIR}/db_popul.js" < "${DATA_DIR}/reduce_test_data.json"
-echo ")" >> "${DATA_DIR}/db_popul.js"
+  db.ImportedData.remove({})
+  db.ImportedData.insertMany([
+    {
+        "_id": "etab1",
+        "value": {
+            "batch": {
+                "2002_1": {}
+            },
+            "scope": "etablissement",
+            "index": {
+                "algo2": true
+            },
+            "key": "01234567891011"
+        }
+    }
+  ])
 
-docker exec -i sf-mongodb mongo signauxfaibles < "${DATA_DIR}/db_popul.js"
+  db.Scores.remove({})
+  db.Scores.insertMany([
+    {
+        "_id": "score1",
+        "siret" : "01234567891011",
+        "periode" : "2014-01-01",
+        "score" : 0.97,
+        "batch" : "2002_1",
+        "timestamp" : ISODate("2014-01-01T00:00:00.000+0000"),
+        "algo" : "algo_avec_urssaf",
+        "alert" : "Alerte seuil F1"
+    },
+    {
+        "_id": "score2",
+        "siret" : "01234567891011",
+        "periode" : "2014-02-01",
+        "score" : 0.98,
+        "batch" : "2002_1",
+        "timestamp" : ISODate("2014-02-01T00:00:00.000+0000"),
+        "algo" : "algo_avec_urssaf",
+        "alert" : "Alerte seuil F1"
+    },
+  ])
+
+  db.RawData.remove({})
+  db.Features.remove({})
+  db.Public.remove({})
+  db.Features_debug.remove({})
+  db.Public_debug.remove({})
+CONTENTS
+
+docker exec -i sf-mongodb mongo signauxfaibles < "${DATA_DIR}/db_popul.js" >/dev/null
 
 echo ""
 echo "⚙️ Computing Features and Public collections thru dbmongo API..."
 ./dbmongo &
 sleep 2 # give some time for dbmongo to start
-http --ignore-stdin :5000/api/data/public batch=2002_1
-EXPORT_FILE=$(http --ignore-stdin :5000/datapi/exportEntreprise batch=2002_1 | tr -d '"')
+echo "- POST /api/data/compact 👉 $(http --print=b --ignore-stdin :5000/api/data/compact fromBatchKey=2002_1)"
+echo "- POST /api/data/public 👉 $(http --print=b --ignore-stdin :5000/api/data/public batch=2002_1 key=012345678)"
+
+docker exec -i sf-mongodb mongo --quiet signauxfaibles > test-api.output.txt << CONTENTS
+  print("// Documents from db.RawData, after call to /api/data/compact:");
+  db.RawData.find().toArray();
+  db.Public_debug.renameCollection("Public")
+  print("// Documents from db.Public, after call to /api/data/public:");
+  db.Public.find().toArray();
+CONTENTS
 
 echo ""
-echo "🆎 Diff between expected and actual output:"
-diff --brief "${DATA_DIR}/test-api-entreprise_golden.json" "${EXPORT_FILE}"
-echo "✅ No diff. The reduce API works as usual."
+echo "🚚 Asking API to export enterprise data..."
+EXPORT_FILE=$(http --ignore-stdin :5000/datapi/exportEntreprise batch=2002_1 | tr -d '"')
+echo "- POST /datapi/exportEntreprise 👉 ${EXPORT_FILE}"
+
 echo ""
-rm "${EXPORT_FILE}"
+# Diff between expected and actual output
+cd ..
+diff --brief "test-api-entreprise.golden-master.json" "dbmongo/${EXPORT_FILE}"
+echo "✅ No diff. The export worked as expected."
+echo ""
+rm "dbmongo/${EXPORT_FILE}"
 # Now, the "trap" commands will run, to clean up.
