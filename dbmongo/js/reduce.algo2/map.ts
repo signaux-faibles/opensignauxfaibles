55e0daa2b51a45898364cdc08a9555a605b31ce5
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-07-22 17:48:41 +0200

    ✅ correction de l'erreur

--- dbmongo/js/reduce.algo2/map.ts
+++ dbmongo/js/reduce.algo2/map.ts
@@ -237,7 +237,7 @@ export function map(this: {
         )
 
         for (const periode of series) {
-          const rest = omit(
+          const rest = f.omit(
             v.diane[hash] as EntréeDiane & {
               marquee: unknown
               nom_entreprise: unknown
