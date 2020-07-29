80c89a20317d736f00ae38da2d113d2d2742d836
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-07-29 11:52:23 +0200

    ❌ correction naïve de la formule cf https://github.com/signaux-faibles/opensignauxfaibles/pull/107#discussion_r460986688

--- dbmongo/js/reduce.algo2/cotisation.ts
+++ dbmongo/js/reduce.algo2/cotisation.ts
@@ -71,8 +71,11 @@ export function cotisation(
       out.ratio_dette =
         (input.montant_part_ouvriere + input.montant_part_patronale) /
         out.cotisation_moy12m
-      out.ratio_dette_moy12m =
-        (moyenne(montantsPO) + moyenne(montantsPP)) / out.cotisation_moy12m
+      out.ratio_dette_moy12m = moyenne(
+        montantsPO.map(
+          (_, i) => (montantsPO[i] + montantsPP[i]) / cotisations[i]
+        )
+      )
     }
     // Remplace dans cibleApprentissage
     //val.dette_any_12m = (val.montantsPA || []).reduce((p,c) => (c >=
