8fd5a6a3a441ddcc70ae8b0881928d4428d7d3c5
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-07-29 14:51:52 +0200

    fix: alignement des accumulateurs de montants + pas de moyennes en cas de valeurs manquantes

--- dbmongo/js/reduce.algo2/cotisation.ts
+++ dbmongo/js/reduce.algo2/cotisation.ts
@@ -9,9 +9,9 @@ export type Input = {
 }
 
 export type SortieCotisation = {
-  cotisation_moy12m: number
+  cotisation_moy12m?: number
   ratio_dette: number
-  ratio_dette_moy12m: number
+  ratio_dette_moy12m?: number
   tag_debit: boolean
   tag_default: boolean
 }
@@ -25,14 +25,16 @@ export function cotisation(
 
   const f = { generatePeriodSerie, dateAddMonth } // DO_NOT_INCLUDE_IN_JSFUNCTIONS_GO
 
-  const moyenne = (valeurs: number[] = []): number =>
-    valeurs.reduce((p, c) => p + c, 0) / (valeurs.length || 1)
+  const moyenne = (valeurs: (number | undefined)[] = []): number | undefined =>
+    valeurs.some((val) => typeof val === "undefined")
+      ? undefined
+      : (valeurs as number[]).reduce((p, c) => p + c, 0) / (valeurs.length || 1)
 
   // calcul de cotisation_moyenne sur 12 mois
   const futureArrays: ParPériode<{
-    cotisations: number[]
-    montantsPP: number[]
-    montantsPO: number[]
+    cotisations: (number | undefined)[]
+    montantsPP: (number | undefined)[]
+    montantsPO: (number | undefined)[]
   }> = {}
 
   Object.keys(output_indexed).forEach((periode) => {
@@ -51,12 +53,9 @@ export function cotisation(
         montantsPP: [],
         montantsPO: [],
       })
-      if (input.cotisation !== undefined)
-        future.cotisations.push(input.cotisation)
-      if (input.montant_part_patronale !== undefined)
-        future.montantsPP.push(input.montant_part_patronale)
-      if (input.montant_part_ouvriere !== undefined)
-        future.montantsPO.push(input.montant_part_ouvriere)
+      future.cotisations.push(input.cotisation)
+      future.montantsPP.push(input.montant_part_patronale)
+      future.montantsPO.push(input.montant_part_ouvriere)
     })
 
     // Calcul des cotisations moyennes à partir des valeurs accumulées ci-dessus
@@ -64,6 +63,7 @@ export function cotisation(
     const out = (sortieCotisation[periode] = sortieCotisation[periode] || {})
     out.cotisation_moy12m = moyenne(cotisations)
     if (
+      typeof out.cotisation_moy12m !== "undefined" &&
       out.cotisation_moy12m > 0 &&
       input.montant_part_ouvriere !== undefined &&
       input.montant_part_patronale !== undefined
@@ -71,8 +71,10 @@ export function cotisation(
       out.ratio_dette =
         (input.montant_part_ouvriere + input.montant_part_patronale) /
         out.cotisation_moy12m
-      out.ratio_dette_moy12m =
-        (moyenne(montantsPO) + moyenne(montantsPP)) / out.cotisation_moy12m
+      const [moyPO, moyPP] = [moyenne(montantsPO), moyenne(montantsPP)]
+      if (typeof moyPO === "number" && typeof moyPP === "number") {
+        out.ratio_dette_moy12m = (moyPO + moyPP) / out.cotisation_moy12m
+      }
     }
     // Remplace dans cibleApprentissage
     //val.dette_any_12m = (val.montantsPA || []).reduce((p,c) => (c >=
