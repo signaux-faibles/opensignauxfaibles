f4640e3783635dc2ecdede9306bf1087064c3528
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-07-29 13:52:16 +0200

    ✅ test: La variable cotisation_moy12m décroit quand une cotisation est passée

--- dbmongo/js/reduce.algo2/cotisation_tests.ts
+++ dbmongo/js/reduce.algo2/cotisation_tests.ts
@@ -9,11 +9,13 @@ const dateDebut = new Date("2018-01-01")
 const dateFin = dateAddMonth(dateDebut, dureeEnMois)
 const periodeSerie = generatePeriodSerie(dateDebut, dateFin)
 
-const forEachMonth = (fct: (periode: Date) => Partial<Input>) =>
+const forEachMonth = (
+  fct: ({ periode, month }: { periode: Date; month: number }) => Partial<Input>
+) =>
   periodeSerie.reduce(
-    (acc, periode) => ({
+    (acc, periode, month) => ({
       ...acc,
-      [periode.getTime()]: { periode, ...fct(periode) },
+      [periode.getTime()]: { periode, ...fct({ periode, month }) },
     }),
     {}
   )
@@ -25,6 +27,27 @@ const testedProps = [
     name: "cotisation_moy12m",
     input: forEachMonth(() => ({ cotisation: 10 })),
     expected: new Array(periodeSerie.length).fill(10),
+  },
+  {
+    assertion:
+      "La variable cotisation_moy12m décroit quand une cotisation est passée",
+    name: "cotisation_moy12m",
+    input: forEachMonth(({ month }) => ({ cotisation: month === 0 ? 10 : 0 })),
+    expected: [
+      10,
+      10 / 2,
+      10 / 3,
+      10 / 4,
+      10 / 5,
+      10 / 6,
+      10 / 7,
+      10 / 8,
+      10 / 9,
+      10 / 10,
+      10 / 11,
+      10 / 12,
+      0,
+    ],
   },
 ]
 
