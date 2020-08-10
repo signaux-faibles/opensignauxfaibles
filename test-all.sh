02faaf98e94b3a416355a2a2acb3cefe121ab1e4
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-08-10 13:37:48 +0200

    lisibilité: ajout de sauts de lignes

--- test-all.sh
+++ test-all.sh
@@ -13,18 +13,34 @@ function indent {
 
 # Optional tests (cannot stop the script)
 
-heading "pick specified node.js version" && (. ~/.nvm/nvm.sh; cd ./dbmongo/js && nvm use) 2>&1 | indent
+heading "pick specified node.js version"
+(. ~/.nvm/nvm.sh; cd ./dbmongo/js && nvm use) 2>&1 | indent
 
 # Mandatory tests (can stop the script)
 
 set -e # will stop the script if any command fails with a non-zero exit code
 set -o pipefail # ... even for tests which pipe their output to indent
 
-heading "npm install" && (cd ./dbmongo/js && npm install) 2>&1 | indent
-heading "npm test" && (cd ./dbmongo/js && npm run lint && npm test) 2>&1 | indent
-heading "go test" && (cd ./dbmongo && go test ./...) 2>&1 | indent && \
-heading "go generate" && (cd ./dbmongo/lib/engine && go generate .) 2>&1 | indent
-heading "go build" && (killall dbmongo 2>/dev/null || true; cd ./dbmongo && go build) 2>&1 | indent
-heading "test-api.sh" && ./tests/test-api.sh 2>&1 | indent
-heading "test-api-reduce.sh" && ./tests/test-api-reduce.sh 2>&1 | indent
-heading "test-api-reduce-2.sh" && ./tests/test-api-reduce-2.sh 2>&1 | indent
+heading "npm install"
+(cd ./dbmongo/js && npm install) 2>&1 | indent
+
+heading "npm test"
+(cd ./dbmongo/js && npm run lint && npm test) 2>&1 | indent
+
+heading "go test"
+(cd ./dbmongo && go test ./...) 2>&1 | indent && \
+
+heading "go generate"
+(cd ./dbmongo/lib/engine && go generate .) 2>&1 | indent
+
+heading "go build"
+(killall dbmongo 2>/dev/null || true; cd ./dbmongo && go build) 2>&1 | indent
+
+heading "test-api.sh"
+./tests/test-api.sh 2>&1 | indent
+
+heading "test-api-reduce.sh"
+./tests/test-api-reduce.sh 2>&1 | indent
+
+heading "test-api-reduce-2.sh"
+./tests/test-api-reduce-2.sh 2>&1 | indent
