4b2a267ea89f7f3732359c43f83056083e6f0066
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-08-10 13:28:23 +0200

    fix: ne pas faire échouer test-all.sh si killall ne trouve pas de process dbmongo

--- test-all.sh
+++ test-all.sh
@@ -24,7 +24,7 @@ heading "npm install" && (cd ./dbmongo/js && npm install) 2>&1 | indent
 heading "npm test" && (cd ./dbmongo/js && npm run lint && npm test) 2>&1 | indent
 heading "go test" && (cd ./dbmongo && go test ./...) 2>&1 | indent && \
 heading "go generate" && (cd ./dbmongo/lib/engine && go generate .) 2>&1 | indent
-heading "go build" && (killall dbmongo 2>/dev/null; cd ./dbmongo && go build) 2>&1 | indent
+heading "go build" && (killall dbmongo 2>/dev/null || true; cd ./dbmongo && go build) 2>&1 | indent
 heading "test-api.sh" && ./tests/test-api.sh 2>&1 | indent
 heading "test-api-reduce.sh" && ./tests/test-api-reduce.sh 2>&1 | indent
 heading "test-api-reduce-2.sh" && ./tests/test-api-reduce-2.sh 2>&1 | indent
