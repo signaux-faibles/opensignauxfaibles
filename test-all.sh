31e334b0a01e3612c0518ca7faa4e36e4fb53cb4
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-08-10 13:18:06 +0200

    ✨ simplification de test-all.sh

--- test-all.sh
+++ test-all.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -o pipefail # so any failing test can stop the script despite piping to indent
-
 function heading {
   echo ""
   echo "–––––"
@@ -13,12 +11,20 @@ function indent {
   sed 's/^/  /'
 }
 
-heading "pick specified node.js version (optional)" && (. ~/.nvm/nvm.sh && cd ./dbmongo/js && nvm use) | indent; \
-heading "npm install" && (cd ./dbmongo/js && npm install) | indent && \
-heading "npm test" && (cd ./dbmongo/js && npm run lint && npm test) | indent && \
+# Optional tests (cannot stop the script)
+
+heading "pick specified node.js version" && (. ~/.nvm/nvmc.sh; cd ./dbmongo/js && nvm use) | indent
+
+# Mandatory tests (can stop the script)
+
+set -e # will stop the script if any command fails with a non-zero exit code
+set -o pipefail # ... even for tests which pipe their output to indent
+
+heading "npm install" && (cd ./dbmongo/js && npm install) | indent
+heading "npm test" && (cd ./dbmongo/js && npm run lint && npm test) | indent
 heading "go test" && (cd ./dbmongo && go test ./...) | indent && \
-heading "go generate" && (cd ./dbmongo/lib/engine && go generate .) | indent && \
-heading "go build" && (killall dbmongo >/dev/null; cd ./dbmongo && go build) | indent && \
-heading "test-api.sh" && ./tests/test-api.sh | indent && \
-heading "test-api-reduce.sh" && ./tests/test-api-reduce.sh | indent && \
+heading "go generate" && (cd ./dbmongo/lib/engine && go generate .) | indent
+heading "go build" && (killall dbmongo >/dev/null; cd ./dbmongo && go build) | indent
+heading "test-api.sh" && ./tests/test-api.sh | indent
+heading "test-api-reduce.sh" && ./tests/test-api-reduce.sh | indent
 heading "test-api-reduce-2.sh" && ./tests/test-api-reduce-2.sh | indent
