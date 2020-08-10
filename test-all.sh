d5bd14c2923bf3ffd6b7060df22c8a5a58fcc16b
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-08-10 13:27:48 +0200

    fix: indenter aussi stderr

--- test-all.sh
+++ test-all.sh
@@ -13,18 +13,18 @@ function indent {
 
 # Optional tests (cannot stop the script)
 
-heading "pick specified node.js version" && (. ~/.nvm/nvmc.sh; cd ./dbmongo/js && nvm use) | indent
+heading "pick specified node.js version" && (. ~/.nvm/nvm.sh; cd ./dbmongo/js && nvm use) 2>&1 | indent
 
 # Mandatory tests (can stop the script)
 
 set -e # will stop the script if any command fails with a non-zero exit code
 set -o pipefail # ... even for tests which pipe their output to indent
 
-heading "npm install" && (cd ./dbmongo/js && npm install) | indent
-heading "npm test" && (cd ./dbmongo/js && npm run lint && npm test) | indent
-heading "go test" && (cd ./dbmongo && go test ./...) | indent && \
-heading "go generate" && (cd ./dbmongo/lib/engine && go generate .) | indent
-heading "go build" && (killall dbmongo 2>/dev/null; cd ./dbmongo && go build) | indent
-heading "test-api.sh" && ./tests/test-api.sh | indent
-heading "test-api-reduce.sh" && ./tests/test-api-reduce.sh | indent
-heading "test-api-reduce-2.sh" && ./tests/test-api-reduce-2.sh | indent
+heading "npm install" && (cd ./dbmongo/js && npm install) 2>&1 | indent
+heading "npm test" && (cd ./dbmongo/js && npm run lint && npm test) 2>&1 | indent
+heading "go test" && (cd ./dbmongo && go test ./...) 2>&1 | indent && \
+heading "go generate" && (cd ./dbmongo/lib/engine && go generate .) 2>&1 | indent
+heading "go build" && (killall dbmongo 2>/dev/null; cd ./dbmongo && go build) 2>&1 | indent
+heading "test-api.sh" && ./tests/test-api.sh 2>&1 | indent
+heading "test-api-reduce.sh" && ./tests/test-api-reduce.sh 2>&1 | indent
+heading "test-api-reduce-2.sh" && ./tests/test-api-reduce-2.sh 2>&1 | indent
