3d30a8241f792a5c88fa049dae57b7fb34b37eac
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-08-11 17:20:43 +0200

    dealWithProcols.js --> .ts

---
+++ dbmongo/js/public/dealWithProcols.ts
@@ -0,0 +1,31 @@
+import { altaresToHuman, AltaresToHumanRes } from "../common/altaresToHuman"
+import { procolToHuman, ProcolToHumanRes } from "../common/procolToHuman"
+
+type Result = {
+  etat: AltaresToHumanRes | ProcolToHumanRes
+  date_procol: Date
+}
+
+export function dealWithProcols(
+  data_source: Record<DataHash, EntréeDefaillances>,
+  altar_or_procol: "altares" | "procol"
+): Result[] {
+  const f = { altaresToHuman, procolToHuman } // DO_NOT_INCLUDE_IN_JSFUNCTIONS_GO
+
+  return Object.keys(data_source || {})
+    .reduce((events, hash) => {
+      const the_event = data_source[hash]
+
+      let etat: AltaresToHumanRes | ProcolToHumanRes = null
+      if (altar_or_procol === "altares")
+        etat = f.altaresToHuman(the_event.code_evenement)
+      else if (altar_or_procol === "procol")
+        etat = f.procolToHuman(the_event.action_procol, the_event.stade_procol)
+
+      if (etat !== null)
+        events.push({ etat: etat, date_procol: new Date(the_event.date_effet) })
+
+      return events
+    }, [] as Result[])
+    .sort((a, b) => a.date_procol.getTime() - b.date_procol.getTime())
+}
--- dbmongo/lib/engine/jsFunctions.go
+++ dbmongo/lib/engine/jsFunctions.go
@@ -773,24 +773,21 @@ db.getCollection("Features").createIndex({
     result.setDate(result.getDate() + nbDays);
     return result;
 }`,
-"dealWithProcols": `function dealWithProcols(data_source, altar_or_procol, output_indexed){
-  "use strict";
-  return Object.keys(data_source || {}).reduce((events,hash) => {
-    var the_event = data_source[hash]
+"dealWithProcols": `function dealWithProcols(data_source, altar_or_procol) {
 
-    let etat = {}
-    if (altar_or_procol == "altares")
-      etat = f.altaresToHuman(the_event.code_evenement);
-    else if (altar_or_procol == "procol")
-      etat = f.procolToHuman(the_event.action_procol, the_event.stade_procol);
-
-    if (etat != null)
-      events.push({"etat": etat, "date_procol": new Date(the_event.date_effet)})
-
-    return(events)
-  },[]).sort(
-    (a,b) => {return(a.date_procol.getTime() > b.date_procol.getTime())}
-  )
+    return Object.keys(data_source || {})
+        .reduce((events, hash) => {
+        const the_event = data_source[hash];
+        let etat = null;
+        if (altar_or_procol === "altares")
+            etat = f.altaresToHuman(the_event.code_evenement);
+        else if (altar_or_procol === "procol")
+            etat = f.procolToHuman(the_event.action_procol, the_event.stade_procol);
+        if (etat !== null)
+            events.push({ etat: etat, date_procol: new Date(the_event.date_effet) });
+        return events;
+    }, [])
+        .sort((a, b) => a.date_procol.getTime() - b.date_procol.getTime());
 }`,
 "debits": `function debits(vdebit) {
   "use strict";
