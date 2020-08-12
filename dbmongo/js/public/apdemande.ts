3f142759e6d2ce9369497193a1fb641ee07a99fe
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-08-12 11:15:55 +0200

    apdemande.js --> .ts

---
+++ dbmongo/js/public/apdemande.ts
@@ -0,0 +1,9 @@
+import * as f from "./iterable"
+
+export function apdemande(
+  apdemande: Record<DataHash, EntréeApDemande>
+): EntréeApDemande[] {
+  return f
+    .iterable(apdemande)
+    .sort((p1, p2) => (p1.periode < p2.periode ? 1 : -1))
+}
