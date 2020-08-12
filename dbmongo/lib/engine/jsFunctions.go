8953faa15b4a8a3dcb560a8982e678e95452aaa6
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-08-12 11:53:52 +0200

    go generate

--- dbmongo/lib/engine/jsFunctions.go
+++ dbmongo/lib/engine/jsFunctions.go
@@ -741,31 +741,24 @@ db.getCollection("Features").createIndex({
     return c.length > 0 ? c[c.length - 1] : undefined;
 }`,
 "cotisations": `function cotisations(vcotisation) {
-  "use strict";
-  var offset_cotisation = 0 
-  var value_cotisation = {}
-  
-  // Répartition des cotisations sur toute la période qu'elle concerne
-  vcotisation = vcotisation || {}
-  Object.keys(vcotisation).forEach(function (h) {
-    var cotisation = vcotisation[h]
-    var periode_cotisation = f.generatePeriodSerie(cotisation.periode.start, cotisation.periode.end)
-    periode_cotisation.forEach(date_cotisation => {
-      let date_offset = f.dateAddMonth(date_cotisation, offset_cotisation)
-      value_cotisation[date_offset.getTime()] = (value_cotisation[date_offset.getTime()] || []).concat(cotisation.du / periode_cotisation.length)
-    })
-  })
 
-  var output_cotisation = []
-
-  serie_periode.forEach(p => {
-    output_cotisation.push(
-      (value_cotisation[p.getTime()] || []) 
-        .reduce((m,c) => m+c, 0)
-    )
-  })
-
-  return(output_cotisation)
+    const offset_cotisation = 0;
+    const value_cotisation = {};
+    // Répartition des cotisations sur toute la période qu'elle concerne
+    vcotisation = vcotisation || {};
+    Object.keys(vcotisation).forEach(function (h) {
+        const cotisation = vcotisation[h];
+        const periode_cotisation = f.generatePeriodSerie(cotisation.periode.start, cotisation.periode.end);
+        periode_cotisation.forEach((date_cotisation) => {
+            const date_offset = f.dateAddMonth(date_cotisation, offset_cotisation);
+            value_cotisation[date_offset.getTime()] = (value_cotisation[date_offset.getTime()] || []).concat([cotisation.du / periode_cotisation.length]);
+        });
+    });
+    const output_cotisation = [];
+    serie_periode.forEach((p) => {
+        output_cotisation.push((value_cotisation[p.getTime()] || []).reduce((m, c) => m + c, 0));
+    });
+    return output_cotisation;
 }`,
 "dateAddDay": `function dateAddDay(date, nbDays) {
     "use strict";
