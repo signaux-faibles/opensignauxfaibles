3c063946d374497b8d5778750b81404e28588341
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-08-13 15:11:01 +0200

    recover filter.go

---
+++ dbmongo/lib/marshal/filter.go
@@ -0,0 +1,114 @@
+package marshal
+
+import (
+	"bufio"
+	"encoding/csv"
+	"errors"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/signaux-faibles/opensignauxfaibles/dbmongo/lib/engine"
+	"github.com/signaux-faibles/opensignauxfaibles/dbmongo/lib/sfregexp"
+
+	"github.com/spf13/viper"
+)
+
+// IsFiltered determines if the siret must be filtered or not
+func IsFiltered(id string, cache engine.Cache, batch *engine.AdminBatch) (bool, error) {
+
+	validSiret := sfregexp.RegexpDict["siret"].MatchString(id)
+	validSiren := sfregexp.RegexpDict["siren"].MatchString(id)
+	if !validSiret && !validSiren {
+		return true, nil
+	}
+
+	filter, err := getSirenFilter(cache, batch, readFilterFiles)
+	if err != nil {
+		return false, err
+	}
+
+	// if no filter, then all ids pass
+	if filter == nil {
+		return false, nil
+	}
+	return !filter[id[0:9]], nil
+}
+
+// getSirenFilter reads the filter from cache if it cans, or else it reads it
+// from input files and stores it in cache
+func getSirenFilter(cache engine.Cache, batch *engine.AdminBatch, fr filterReader) (map[string]bool, error) {
+
+	value, err := cache.Get("filter")
+
+	if err == nil {
+		filter, ok := value.(map[string]bool)
+		if ok {
+			return filter, nil
+		} else {
+			return nil, errors.New("Wrong format from existing field filter in cache")
+		}
+	}
+
+	paths := batch.Files["filter"]
+	if len(paths) == 0 {
+		// No filter
+		return nil, nil
+	}
+
+	basePath := viper.GetString("APP_DATA")
+	filter, err := fr(basePath, paths)
+	if err != nil {
+		return nil, err
+	}
+	cache.Set("filter", filter)
+	return filter, nil
+}
+
+type filterReader func(string, []string) (map[string]bool, error)
+
+// openAndReadFilters reads several files, reads their content and concatenate
+// it into a map[string]bool
+func readFilterFiles(basePath string, filenames []string) (map[string]bool, error) {
+	filter := make(map[string]bool)
+	for _, p := range filenames {
+		file, err := os.Open(filepath.Join(basePath, p))
+		defer file.Close()
+		if err != nil {
+			return nil, errors.New("Erreur à l'ouverture du fichier, " + err.Error())
+		}
+		err = readFilter(bufio.NewReader(file), filter)
+		if err != nil {
+			return nil, errors.New("Erreur à la lecture du fichier, " + err.Error())
+		}
+	}
+	return filter, nil
+}
+
+// readFilter reads the content of a io.Reader and adds it to an existing
+// filter
+func readFilter(reader io.Reader, filter map[string]bool) error {
+
+	csvreader := csv.NewReader(reader)
+	csvreader.Comma = ';'
+
+	sirenIndex := 0
+
+	for {
+		row, err := csvreader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+
+		siren := row[sirenIndex]
+
+		if sfregexp.RegexpDict["siren"].MatchString(siren) {
+			filter[siren] = true
+		} else {
+			return errors.New("Format de siren incorrect trouvé : " + siren)
+		}
+	}
+	return nil
+}
