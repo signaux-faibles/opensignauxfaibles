6d4158e588e5656a6586754b4bbd611c98ccd064
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-08-13 14:57:37 +0200

    🧹 EtablissementBrowse(), EtablissementBrowseParams

--- dbmongo/lib/engine/browse.go
+++ dbmongo/lib/engine/browse.go
@@ -1,8 +1,6 @@
 package engine
 
 import (
-	"github.com/signaux-faibles/opensignauxfaibles/dbmongo/lib/naf"
-
 	"github.com/globalsign/mgo/bson"
 )
 
@@ -24,112 +22,6 @@ func Search(params SearchParams) ([]interface{}, error) {
 			bson.M{"value.sirene": bson.M{"$exists": true}},
 		},
 		}).Limit(15).All(&result)
-	return result, err
-}
-
-// BrowseParams is type for params for prediction browser
-type BrowseParams struct {
-	Batch    string   `json:"batch"`
-	Naf1     string   `json:"naf1"`
-	Effectif int      `json:"effectif"`
-	Suivi    *bool    `json:"suivi"`
-	Ccsf     bool     `json:"ccsf"`
-	Procol   bool     `json:"procol"`
-	Limit    int      `json:"limit"`
-	Offset   int      `json:"offset"`
-	Zone     []string `json:"zone"`
-}
-
-// PredictionBrowse retourne la lise de prédiction filtrée pour la navigation
-func PredictionBrowse(params BrowseParams) (interface{}, error) {
-	var pipeline []bson.M
-
-	pipeline = append(pipeline, bson.M{"$match": bson.M{
-		"_id.batch": params.Batch,
-	}})
-
-	if params.Suivi != nil {
-		if *params.Suivi {
-			pipeline = append(pipeline, bson.M{"$match": bson.M{
-				"connu": true,
-			}})
-		} else {
-			pipeline = append(pipeline, bson.M{
-				"$match": bson.M{
-					"$and": []interface{}{
-						bson.M{"connu": false},
-						// bson.M{"procol": "in_bonis"},
-					},
-				},
-			})
-		}
-	}
-
-	pipeline = append(pipeline, bson.M{"$addFields": bson.M{"inZone": bson.M{"$in": []interface{}{"$departement", params.Zone}}}})
-
-	pipeline = append(pipeline, bson.M{"$match": bson.M{
-		"inZone": true,
-	}})
-
-	pipeline = append(pipeline, bson.M{"$project": bson.M{
-		"_id.scope":   "etablissement",
-		"_id.key":     "$_id.siret",
-		"_id.batch":   "$_id.batch",
-		"prob":        "$prob",
-		"diff":        "$diff",
-		"test":        params.Zone,
-		"inZone":      bson.M{"inZone": bson.M{"$in": []interface{}{"$departement", params.Zone}}},
-		"departement": "$departement",
-	}})
-
-	pipeline = append(pipeline, bson.M{"$sort": bson.M{
-		"prob": -1,
-	}})
-
-	pipeline = append(pipeline, bson.M{"$lookup": bson.M{
-		"from":         "Public",
-		"localField":   "_id",
-		"foreignField": "_id",
-		"as":           "etablissement"}})
-
-	pipeline = append(pipeline, bson.M{"$lookup": bson.M{
-		"from":         "Public",
-		"localField":   "etablissement.0.value.idEntreprise",
-		"foreignField": "_id",
-		"as":           "entreprise"}})
-
-	pipeline = append(pipeline, bson.M{"$addFields": bson.M{
-		"etablissement": bson.M{"$arrayElemAt": []interface{}{"$etablissement", 0}},
-		"entreprise":    bson.M{"$arrayElemAt": []interface{}{"$entreprise", 0}},
-	}})
-
-	pipeline = append(pipeline, bson.M{"$addFields": bson.M{
-		"etablissement": "$etablissement.value",
-		"entreprise":    "$entreprise.value",
-	}})
-
-	if params.Naf1 != "" {
-		pipeline = append(pipeline, bson.M{"$match": bson.M{
-			"etablissement.sirene.ape": bson.M{"$in": naf.Naf5from1(params.Naf1)},
-		}})
-	}
-
-	if params.Procol {
-		pipeline = append(pipeline, bson.M{"$match": bson.M{
-			"etablissement.procol": "in_bonis",
-		}})
-	}
-
-	pipeline = append(pipeline, bson.M{"$match": bson.M{
-		"etablissement.dernier_effectif.effectif": bson.M{"$gt": params.Effectif},
-	}})
-
-	pipeline = append(pipeline, bson.M{"$skip": params.Offset})
-
-	pipeline = append(pipeline, bson.M{"$limit": params.Limit})
-	var result = []interface{}{}
-	err := Db.DB.C("Prediction").Pipe(pipeline).All(&result)
-
 	return result, err
 }
 
