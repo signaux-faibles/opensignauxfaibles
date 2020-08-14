69668dc2a409b8d502a6d543600580ec85273799
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-08-14 11:11:28 +0200

    fix: keep tearing down, even if "No matching processes belonging to you were found"

--- tests/test-api-export.sh
+++ tests/test-api-export.sh
@@ -17,7 +17,14 @@ DATA_DIR=$(pwd)/tmp-opensignauxfaibles-data-raw
 mkdir -p "${DATA_DIR}"
 
 # Clean up on exit
-trap "{ echo -e \"${COLOR_DEFAULT}\"; tests/helpers/dbmongo-server.sh stop; tests/helpers/mongodb-container.sh stop; rm -rf ${DATA_DIR}; echo \"✨ Cleaned up temp directory\"; }" EXIT
+function teardown {
+    echo -e "${COLOR_DEFAULT}"
+    tests/helpers/dbmongo-server.sh stop || true # keep tearing down, even if "No matching processes belonging to you were found"
+    tests/helpers/mongodb-container.sh stop
+    rm -rf ${DATA_DIR}
+    echo "✨ Cleaned up temp directory"
+}
+trap teardown EXIT
 
 echo ""
 echo "🐳 Starting MongoDB container..."
