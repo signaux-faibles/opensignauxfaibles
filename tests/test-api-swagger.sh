dad68d78f286753efee430737bb6a22c5d302fae
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-08-20 14:50:29 +0200

    fix(swagger): Mise à jour des définitions Swagger de l'API (#121)
    ## Changements apportés
    
    - Retrait du code et documentation liées aux utilisateurs (login...), car apparemment non exploité
    - Ajout des routes `GET /api/data/entreprises` et `GET /api/data/etablissements` dans les defs Swagger de l'API
    - Correction de la route `POST /api/data/compact` et son paramètre `fromBatchKey` (renommé récemment)
    - Formatage du fichier `yaml`
    - Ajout d'un test permettant de valider `swagger.yaml` et vérifier qu'il ne reste aucun changement à commit dans `swagger.json`

---
+++ tests/test-api-swagger.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Test de validité des définitions API / Swagger.
+# Ce script doit être exécuté depuis la racine du projet. Ex: par test-all.sh.
+
+set -e # will stop the script if any command fails with a non-zero exit code
+
+# Check that swagger.yaml is valid
+npx @apidevtools/swagger-cli validate dbmongo/docs/swagger/swagger.yaml
+
+# Convert swagger.yaml to swagger.json
+(cd dbmongo/docs/swagger && ./convert-yaml-to-json.sh)
+
+# Check if swagger.json has changes that need to be committed
+PENDING_CHANGES=$(git diff dbmongo/docs/swagger/swagger.json | wc -l)
+if [[ "${PENDING_CHANGES}" -ne "0" ]]
+then
+    echo "⚠️  swagger.json has ${PENDING_CHANGES} pending changes. please commit them."
+    exit 1
+fi
+
+echo "✅  swagger.yaml and swagger.json are both valid and in sync."
+
+# Note: If you want to re-generate swagger.yaml based on Go annotations:
+# $ go run github.com/swaggo/swag/cmd/swag init
+# (see https://github.com/swaggo/swag#how-to-use-it-with-gin)
