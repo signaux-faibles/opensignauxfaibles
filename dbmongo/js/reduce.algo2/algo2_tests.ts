1622ac0c1773ce360e76d7106647c278d8388adf
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-09-01 14:03:04 +0200

    🔵 refacto: extract makeInput()

--- dbmongo/js/reduce.algo2/algo2_tests.ts
+++ dbmongo/js/reduce.algo2/algo2_tests.ts
@@ -11,14 +11,12 @@ import { reducer, invertedReducer } from "../test/helpers/reducers"
 import { runMongoMap, indexMapResultsByKey } from "../test/helpers/mongodb"
 import { setGlobals } from "../test/helpers/setGlobals"
 import { Scope } from "../RawDataTypes"
+import { EntréeDelai } from "../common/validDelai"
 
 const DAY_IN_MS = 24 * 60 * 60 * 1000
 
 // initialisation des paramètres globaux de reduce.algo2
-function initGlobalParams(
-  dateDebut = new Date("2014-01-01"),
-  dateFin = new Date("2018-02-01")
-) {
+const initGlobalParams = (dateDebut: Date, dateFin: Date) =>
   setGlobals({
     naf: nafValues,
     actual_batch: "1905",
@@ -28,13 +26,59 @@ function initGlobalParams(
     offset_effectif: 2,
     includes: { all: true },
   })
+
+const makeInput = (
+  dateDebut: Date,
+  dateFin: Date,
+  delaiOverrides?: Partial<EntréeDelai>
+) => {
+  const siret = "12345678901234"
+  const duréeDelai = 60 // en jours
+  return {
+    _id: siret,
+    value: {
+      key: siret,
+      scope: "etablissement" as Scope,
+      batch: {
+        "1905": {
+          cotisation: {},
+          debit: {
+            hashDette: {
+              periode: {
+                start: dateDebut,
+                end: dateFin,
+              },
+              numero_ecart_negatif: 1,
+              numero_historique: 2,
+              numero_compte: "",
+              date_traitement: dateDebut,
+              debit_suivant: "",
+              part_ouvriere: 60,
+              part_patronale: 0,
+            },
+          },
+          delai: {
+            hashDelai: {
+              date_creation: dateDebut,
+              date_echeance: new Date(
+                dateDebut.getTime() + duréeDelai * DAY_IN_MS
+              ),
+              duree_delai: duréeDelai,
+              montant_echeancier: 100,
+              ...delaiOverrides,
+            },
+          },
+        },
+      },
+    },
+  }
 }
 
 // Tests
 
 test("l'ordre de traitement des données n'influe pas sur les résultats", (t: ExecutionContext) => {
   testCases.forEach(({ _id, value }) => {
-    initGlobalParams()
+    initGlobalParams(new Date("2014-01-01"), new Date("2018-02-01"))
 
     const values = Object.values(
       indexMapResultsByKey(
@@ -60,47 +104,9 @@ test("l'ordre de traitement des données n'influe pas sur les résultats", (t: E
 
 test("delai_deviation_remboursement est calculé à partir d'un débit et d'une demande de délai de règlement de cotisations sociales", (t: ExecutionContext) => {
   const dateDebut = new Date("2018-01-01")
-  const datePlusUnMois = new Date("2018-02-01")
-  initGlobalParams(dateDebut, datePlusUnMois)
-  const siret = "12345678901234"
-  const duréeDelai = 60 // en jours
-  const input = {
-    _id: siret,
-    value: {
-      key: siret,
-      scope: "etablissement" as Scope,
-      batch: {
-        "1905": {
-          cotisation: {},
-          debit: {
-            hashDette: {
-              periode: {
-                start: dateDebut,
-                end: datePlusUnMois,
-              },
-              numero_ecart_negatif: 1,
-              numero_historique: 2,
-              numero_compte: "",
-              date_traitement: dateDebut,
-              debit_suivant: "",
-              part_ouvriere: 60,
-              part_patronale: 0,
-            },
-          },
-          delai: {
-            hashDelai: {
-              date_creation: dateDebut,
-              date_echeance: new Date(
-                dateDebut.getTime() + duréeDelai * DAY_IN_MS
-              ),
-              duree_delai: duréeDelai,
-              montant_echeancier: 100,
-            },
-          },
-        },
-      },
-    },
-  }
+  const dateFin = new Date("2018-02-01")
+  initGlobalParams(dateDebut, dateFin)
+  const input = makeInput(dateDebut, dateFin)
 
   const [res] = runMongoMap<EntréeMap, CléSortieMap, SortieMap>(map, [input])
   const finalCompanyData = Object.values(res.value)[0]
@@ -110,44 +116,9 @@ test("delai_deviation_remboursement est calculé à partir d'un débit et d'une 
 
 test("validation: le traitement est interrompu si duree_delai vaut zero", (t: ExecutionContext) => {
   const dateDebut = new Date("2018-01-01")
-  const datePlusUnMois = new Date("2018-02-01")
-  initGlobalParams(dateDebut, datePlusUnMois)
-  const siret = "12345678901234"
-  const input = {
-    _id: siret,
-    value: {
-      key: siret,
-      scope: "etablissement" as Scope,
-      batch: {
-        "1905": {
-          cotisation: {},
-          debit: {
-            hashDette: {
-              periode: {
-                start: dateDebut,
-                end: datePlusUnMois,
-              },
-              numero_ecart_negatif: 1,
-              numero_historique: 2,
-              numero_compte: "",
-              date_traitement: dateDebut,
-              debit_suivant: "",
-              part_ouvriere: 60,
-              part_patronale: 0,
-            },
-          },
-          delai: {
-            hashDelai: {
-              date_creation: dateDebut,
-              date_echeance: new Date(dateDebut.getTime() + 60 * DAY_IN_MS),
-              duree_delai: 0, // !
-              montant_echeancier: 100,
-            },
-          },
-        },
-      },
-    },
-  }
+  const dateFin = new Date("2018-02-01")
+  initGlobalParams(dateDebut, dateFin)
+  const input = makeInput(dateDebut, dateFin, { duree_delai: 0 })
 
   t.throws(
     () => runMongoMap<EntréeMap, CléSortieMap, SortieMap>(map, [input]),
