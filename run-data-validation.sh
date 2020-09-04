20aff4be10ab5ce49c8d3781d1b31be2e69d4b9d
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-09-04 10:40:35 +0200

    optim: affichage des documents invalides au fil de l'eau

--- run-data-validation.sh
+++ run-data-validation.sh
@@ -10,7 +10,7 @@ mongo "${DATABASE}" <<< "db.RawData.count()" # => 6929282 documents
 echo ""
 echo "💎 Validating data..."
 AGGREG_PREPARATION='
-    { $limit: 1000 }, // on ne traite que les 1000 premiers documents de RawData (TODO: à retirer)
+    { $limit: 100000 }, // on ne traite que les 100000 premiers documents de RawData (TODO: à retirer)
     { $project: { _id: 1, batches: { $objectToArray: "$value.batch" } } }, // => { _id, batches: Array<{ k: BatchKey, v: BatchValues }> }
     { $unwind: { path: "$batches", preserveNullAndEmptyArrays: false } }, // => { _id, batches: { k: BatchKey, v: BatchValues } }
     { $project: { _id: 1, batchKey: "$batches.k", "dataPerHash": { $objectToArray: "$batches.v" } } }, // => { _id, batchKey, dataPerHash: Array<{ k: DataType, v: ParHash<Data> }> }
@@ -20,12 +20,10 @@ AGGREG_PREPARATION='
     { $project: { _id: 1, batchKey: 1, dataType: 1, dataHash: "$dataPerHash.k", "dataObject": "$dataPerHash.v" } }, // => { _id, batchKey, dataType, dataHash, dataObject: Data }
 '
 cat > "dbvalid.js" << CONTENT
-  printjson(db.RawData.aggregate([
+  print("invalid records:");
+  db.RawData.aggregate([
     ${AGGREG_PREPARATION}
     {
-      \$facet: {
-        "invalid": [
-          {
             \$match: {
               dataType: "delai",
               \$nor: [
@@ -39,12 +37,10 @@ cat > "dbvalid.js" << CONTENT
                 }
               ]
             }
-          }
-        ]
-      }
-    },
-  ]).maxTimeMS(10 * 60 * 1000).toArray()[0]) // on limite la durée d'execution à 10 minutes max (TODO: à retirer)
+    }
+  ]).forEach(printjson);
+  print("done.");
 CONTENT
 
 cat "dbvalid.js"
-mongo "${DATABASE}" "dbvalid.js"
+time mongo "${DATABASE}" "dbvalid.js"
