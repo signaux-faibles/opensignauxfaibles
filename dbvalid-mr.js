57c9ce6048e91d3e277a873b382e87f987314265
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-09-04 12:17:46 +0200

    durée du map reduce pour 1000000 documents avec plus de vérifications: 1m45 => hypothèse: les requêtes sont de plus en plus rapides, grace à du caching ?

--- dbvalid-mr.js
+++ dbvalid-mr.js
@@ -8,7 +8,16 @@
       if (entriesToValidate) {
         for (const dataHash of Object.keys(entriesToValidate)) {
           const dataEntry = entriesToValidate[dataHash];
-          if (!(dataEntry.duree_delai > 0 && dataEntry.montant_echeancier > 0.01)) {
+          if (!(
+            dataEntry.date_creation &&
+            dataEntry.date_creation.getTime &&
+            dataEntry.date_echeance &&
+            dataEntry.date_echeance.getTime &&
+            typeof dataEntry.duree_delai === "number" &&
+            dataEntry.duree_delai > 0 &&
+            typeof dataEntry.montant_echeancier === "number" &&
+            dataEntry.montant_echeancier > 0.01
+          )) {
             emit(this._id, { batchKey, dataHash, dataEntry }); // return invalid entry
           }
         }
