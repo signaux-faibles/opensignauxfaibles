dd307937a049b4da5ea2540f4bb800f6978d3900
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-09-07 16:31:41 +0200

    don't need cat anymore

--- tests/helpers/populate-from-objects.sh
+++ tests/helpers/populate-from-objects.sh
@@ -11,21 +11,21 @@ function raw_data_documents {
   '
 }
 
-cat << CONTENTS
+echo "
   db.Admin.insertOne({
-    "_id" : {
-        "key" : "1905",
-        "type" : "batch"
+    '_id' : {
+        'key' : '1905',
+        'type' : 'batch'
     },
-    "param" : {
-        "date_debut" : ISODate("2014-01-01T00:00:00.000+0000"),
-        "date_fin" : ISODate("2016-01-01T00:00:00.000+0000"),
-        "date_fin_effectif" : ISODate("2016-03-01T00:00:00.000+0000")
+    'param' : {
+        'date_debut' : ISODate('2014-01-01T00:00:00.000+0000'),
+        'date_fin' : ISODate('2016-01-01T00:00:00.000+0000'),
+        'date_fin_effectif' : ISODate('2016-03-01T00:00:00.000+0000')
     },
-    "name" : "TestData"
+    'name' : 'TestData'
   })
 
   db.RawData.insertMany(
     $(raw_data_documents)
   )
-CONTENTS
+"
