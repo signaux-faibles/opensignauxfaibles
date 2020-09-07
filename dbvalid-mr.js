c498c3a5b3c89d6d9633384f7b5e392ac9948546
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-09-07 11:02:58 +0200

    nouveau test en prod avec itération sur toutes les clés de données

--- dbvalid-mr.js
+++ dbvalid-mr.js
@@ -1,24 +1,25 @@
   
   const mapFunction = function() {
-    const dataType = "delai";
     const invalidEntries = [];
     for (const batchKey of Object.keys(this.value.batch)) {
       const batchEntries = this.value.batch[batchKey];
-      const entriesToValidate = batchEntries[dataType];
-      if (entriesToValidate) {
-        for (const dataHash of Object.keys(entriesToValidate)) {
-          const dataEntry = entriesToValidate[dataHash];
-          if (!(
-            dataEntry.date_creation &&
-            dataEntry.date_creation.getTime &&
-            dataEntry.date_echeance &&
-            dataEntry.date_echeance.getTime &&
-            typeof dataEntry.duree_delai === "number" &&
-            dataEntry.duree_delai > 0 &&
-            typeof dataEntry.montant_echeancier === "number" &&
-            dataEntry.montant_echeancier > 0.01
-          )) {
-            emit(this._id, { batchKey, dataHash, dataEntry }); // return invalid entry
+      for (const dataType of Object.keys(batchEntries)) {
+        const dataEntries = batchEntries[dataType];
+        for (const dataHash of Object.keys(dataEntries)) {
+          const dataEntry = dataEntries[dataHash];
+          if (dataType === "delai") {
+            if (!(
+              dataEntry.date_creation &&
+              dataEntry.date_creation.getTime &&
+              dataEntry.date_echeance &&
+              dataEntry.date_echeance.getTime &&
+              typeof dataEntry.duree_delai === "number" &&
+              dataEntry.duree_delai > 0 &&
+              typeof dataEntry.montant_echeancier === "number" &&
+              dataEntry.montant_echeancier > 0.01
+            )) {
+              emit(this._id, { batchKey, dataHash, dataEntry }); // return invalid entry
+            }
           }
         }
       }
@@ -32,6 +33,7 @@
   printjson(db.RawData.mapReduce(
     mapFunction,
     reduceFunction, {
+      limit: 1000000,
       out: { inline: 1 }
     }
   ));
--- run-data-validation-mapreduce.sh
+++ run-data-validation-mapreduce.sh
@@ -11,25 +11,26 @@ echo ""
 echo "💎 Validating data..."
 FUNCTIONS='
   const mapFunction = function() {
-    const dataType = "delai";
     const invalidEntries = [];
     for (const batchKey of Object.keys(this.value.batch)) {
       const batchEntries = this.value.batch[batchKey];
-      const entriesToValidate = batchEntries[dataType];
-      if (entriesToValidate) {
-        for (const dataHash of Object.keys(entriesToValidate)) {
-          const dataEntry = entriesToValidate[dataHash];
-          if (!(
-            dataEntry.date_creation &&
-            dataEntry.date_creation.getTime &&
-            dataEntry.date_echeance &&
-            dataEntry.date_echeance.getTime &&
-            typeof dataEntry.duree_delai === "number" &&
-            dataEntry.duree_delai > 0 &&
-            typeof dataEntry.montant_echeancier === "number" &&
-            dataEntry.montant_echeancier > 0.01
-          )) {
-            emit(this._id, { batchKey, dataHash, dataEntry }); // return invalid entry
+      for (const dataType of Object.keys(batchEntries)) {
+        const dataEntries = batchEntries[dataType];
+        for (const dataHash of Object.keys(dataEntries)) {
+          const dataEntry = dataEntries[dataHash];
+          if (dataType === "delai") {
+            if (!(
+              dataEntry.date_creation &&
+              dataEntry.date_creation.getTime &&
+              dataEntry.date_echeance &&
+              dataEntry.date_echeance.getTime &&
+              typeof dataEntry.duree_delai === "number" &&
+              dataEntry.duree_delai > 0 &&
+              typeof dataEntry.montant_echeancier === "number" &&
+              dataEntry.montant_echeancier > 0.01
+            )) {
+              emit(this._id, { batchKey, dataHash, dataEntry }); // return invalid entry
+            }
           }
         }
       }
@@ -45,6 +46,7 @@ cat > "dbvalid-mr.js" << CONTENT
   printjson(db.RawData.mapReduce(
     mapFunction,
     reduceFunction, {
+      limit: 1000000,
       out: { inline: 1 }
     }
   ));
