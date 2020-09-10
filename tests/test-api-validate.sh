00040e5c0b56c146be6177aaa35e2cad2449c842
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-09-10 10:33:13 +0200

    feat(API): Validation des données par MongoDB / JSON Schema (#143)
    Contribue à #87. Alternative à PR #142.
    
    ## Approche
    
    - `POST /api/data/validate` retourne les entrées de données invalides, en itérant sur chaque entrée de chaque hash de chaque batch de chaque document de la collection `RawData`.
    - Par "entrée de données valide", on entend que les champs de ce type de données (ex: `delai`) respectent le type et les contraintes (ex: `duree_delai` doit être un nombre entier strictement positif) attendues par les chaînes de traitement `public` et `reduce.algo2`.
    - Les règles de validation sont exprimées à raison d'un fichier JSON Schema par type de données. Par exemple, le fichier [`delai.schema.json`](https://github.com/signaux-faibles/opensignauxfaibles/pull/143/files#diff-dd598302e68ab61325dfadc8a444f244) décrit les champs de l'entrée `delai` générée par le parseur des données URSSAF. Documentation de référence du format: https://docs.mongodb.com/manual/reference/operator/query/jsonSchema.
    - Les fichiers JSON Schema sont stockés dans `dbmongo/validation/*.schema.json`.
    - Bonus: le script `generate-types.sh` (appelé après la transpilation du code TS en JS) génère les types TS dans `GeneratedTypes.d.ts`, à partir des ces fichiers JSON Schema.
    
    Logiquement, cette API devrait être appelée après chaque appel à `/compact`, avant de lancer les chaînes de traitement `public` et `reduce.algo2`.
    
    ## Tâches
    
    - [x] tester la validation de valeur minimum `delai.duree_delai` exprimée en JSON Schema sur MongoDB
    - [x] tester la validation sur la collection `RawData` de la db de production => temps d'exécution sur 6 millions de documents: 33 minutes
    - [x] créer un point d'entrée API pour lancer la validation
    - [x] générer les types TypeScript à partir des modèles de validation de données exprimés en JSON Schema, avec [mongodb-json-schema-to-typescript](https://github.com/adrienjoly/mongodb-json-schema-to-typescript) (fork réalisé pour cette PR)

---
+++ tests/test-api-validate.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Test de bout en bout de POST /api/data/validate.
+# Inspiré de test-api-export.sh.
+# Ce script doit être exécuté depuis la racine du projet. Ex: par test-all.sh.
+
+tests/helpers/mongodb-container.sh stop
+
+set -e # will stop the script if any command fails with a non-zero exit code
+
+# Setup
+FLAGS="$*" # the script will update the golden file if "--update" flag was provided as 1st argument
+INPUT_FILE="tests/input-data/RawData.validation.json"
+GOLDEN_FILE="tests/output-snapshots/test-api-validate.golden.json"
+TMP_DIR="tests/tmp-test-execution-files"
+mkdir -p "${TMP_DIR}"
+
+# Clean up on exit
+function teardown {
+    tests/helpers/dbmongo-server.sh stop || true # keep tearing down, even if "No matching processes belonging to you were found"
+    tests/helpers/mongodb-container.sh stop
+}
+trap teardown EXIT
+
+PORT="27016" tests/helpers/mongodb-container.sh start
+
+MONGODB_PORT="27016" tests/helpers/dbmongo-server.sh setup
+
+echo ""
+echo "📝 Inserting test data..."
+sleep 1 # give some time for MongoDB to start
+tests/helpers/mongodb-container.sh run << CONTENT
+  db.RawData.insertMany($(cat ${INPUT_FILE}))
+CONTENT
+
+echo ""
+echo "💎 Computing the Public collection thru dbmongo API..."
+tests/helpers/dbmongo-server.sh start
+OUTPUT_GZ_FILE=dbmongo/$(http --print=b --ignore-stdin :5000/api/data/validate collection=RawData | tr -d '"')
+echo "- POST /api/data/validate 👉 ${OUTPUT_GZ_FILE}"
+OUTPUT_FILE="${TMP_DIR}/output.json"
+zcat < "${OUTPUT_GZ_FILE}" > "${OUTPUT_FILE}"
+
+tests/helpers/diff-or-update-golden-master.sh "${FLAGS}" "${GOLDEN_FILE}" "${OUTPUT_FILE}"
+
+rm "${OUTPUT_GZ_FILE}"
+rm -rf "${TMP_DIR}"
+# Now, the "trap" commands will clean up the rest.
