4313fd24a9ad6e6f8596fdeafd208b900d1f0ad2
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-09-14 11:15:59 +0200

    ❌  reproduction de la régression par un test

---
+++ tests/test-api-compact.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# Test de bout en bout de l'API "compact".
+# Ce script doit être exécuté depuis la racine du projet. Ex: par test-all.sh.
+
+tests/helpers/mongodb-container.sh stop
+
+set -e # will stop the script if any command fails with a non-zero exit code
+
+# Setup
+FLAGS="$*" # the script will update the golden file if "--update" flag was provided as 1st argument
+TMP_DIR="tests/tmp-test-execution-files"
+OUTPUT_FILE="${TMP_DIR}/test-api-compact.output.json"
+GOLDEN_FILE="tests/output-snapshots/test-api-compact.golden.json"
+mkdir -p "${TMP_DIR}"
+
+# Clean up on exit
+function teardown {
+    tests/helpers/dbmongo-server.sh stop || true # keep tearing down, even if "No matching processes belonging to you were found"
+    tests/helpers/mongodb-container.sh stop
+}
+trap teardown EXIT
+
+PORT="27016" tests/helpers/mongodb-container.sh start
+
+MONGODB_PORT="27016" tests/helpers/dbmongo-server.sh setup
+
+echo ""
+echo "📝 Inserting test data..."
+sleep 1 # give some time for MongoDB to start
+
+tests/helpers/mongodb-container.sh run > /dev/null << CONTENTS
+  db.Admin.insertOne({
+    "_id" : {
+        "key" : "2002_1",
+        "type" : "batch"
+    },
+    "param" : {
+        "date_debut" : ISODate("2014-01-01T00:00:00.000+0000"),
+        "date_fin" : ISODate("2016-01-01T00:00:00.000+0000"),
+        "date_fin_effectif" : ISODate("2016-03-01T00:00:00.000+0000")
+    },
+    "name" : "TestData"
+  })
+
+  db.ImportedData.insertOne({
+    "_id": "random123abc",
+    "value": {
+      "batch": {
+        "1910": {}
+      },
+      "scope": "etablissement",
+      "index": {
+        "algo2": true
+      },
+      "key": "01234567891011"
+    }
+  })
+
+  db.RawData.insertMany(
+    $(cat tests/input-data/RawData.sample.json)
+  )
+CONTENTS
+
+echo ""
+echo "💎 Compacting RawData thru dbmongo API..."
+tests/helpers/dbmongo-server.sh start
+echo "- POST /api/data/compact 👉 $(http --print=b --ignore-stdin :5000/api/data/compact fromBatchKey=1802)"
+
+(tests/helpers/mongodb-container.sh run \
+  | tests/helpers/remove-random_order.sh \
+  > "${OUTPUT_FILE}" \
+) <<< 'printjson(db.RawData.find().toArray());'
+
+# Display JS errors logged by MongoDB, if any
+tests/helpers/mongodb-container.sh exceptions || true
+
+tests/helpers/diff-or-update-golden-master.sh "${FLAGS}" "${GOLDEN_FILE}" "${OUTPUT_FILE}"
+
+rm -rf "${TMP_DIR}"
+# Now, the "trap" commands will clean up the rest.
