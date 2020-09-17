03e043ffd92107f71dbc1424f47e8cbd5bc54f65
chrnin <christophe.ninucci@direccte.gouv.fr>
2020-09-17 13:54:59 +0200

    le petit commentaire qui casse le test :)

--- dbmongo/lib/engine/jsFunctions.go
+++ dbmongo/lib/engine/jsFunctions.go
@@ -1025,7 +1025,7 @@ db.getCollection("Features").createIndex({
 }`,
 "sirene": `// Cette fonction retourne les données sirene les plus récentes
 function sirene(sireneArray) {
-    return sireneArray[sireneArray.length - 1] || {}; // TODO: vérifier que sireneArray est bien classé dans l'ordre chronologique
+    return sireneArray[sireneArray.length - 1] || {}; // TODO: vérifier que sireneArray est bien classé dans l'ordre chronologique -> c'est sûr qu'il ne l'est pas, vérifier que pour toute la base on a bien un objet sirene unique !
 }`,
 },
 "purgeBatch":{
