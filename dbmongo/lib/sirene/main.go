e3742032e61b55eb0ddd6428adda66985c020e85
chrnin <christophe.ninucci@direccte.gouv.fr>
2020-09-18 06:45:05 +0200

    more sirene fields in import

--- dbmongo/lib/sirene/main.go
+++ dbmongo/lib/sirene/main.go
@@ -23,21 +23,27 @@ type Sirene struct {
 	Siren                string     `json:"siren,omitempty" bson:"siren,omitempty"`
 	Nic                  string     `json:"nic,omitempty" bson:"nic,omitempty"`
 	Siege                bool       `json:"siege,omitempty" bson:"siege,omitempty"`
-	NumVoie              *string    `json:"numero_voie,omitempty" bson:"numero_voie,omitempty"`
-	IndRep               *string    `json:"indrep,omitempty" bson:"indrep,omitempty"`
-	TypeVoie             *string    `json:"type_voie,omitempty" bson:"type_voie,omitempty"`
-	Voie                 *string    `json:"voie,omitempty" bson:"voie,omitempty"`
-	CodePostal           *string    `json:"code_postal,omitempty" bson:"code_postal,omitempty"`
-	Cedex                *string    `json:"cedex,omitempty" bson:"cedex,omitempty"`
-	Departement          *string    `json:"departement,omitempty" bson:"departement,omitempty"`
-	Commune              *string    `json:"commune,omitempty" bson:"commune,omitempty"`
-	DistributionSpeciale *string    `json:"distribution_speciale,omitempty" bson:"distribution_speciale,omitempty"`
-	APE                  *string    `json:"ape,omitempty" bson:"ape,omitempty"`
-	CodeActivite         *string    `json:"code_activite,omitempty" bson:"code_activite,omitempty"`
-	NomenActivite        *string    `json:"nomen_activite,omitempty" bson:"nomen_activite,omitempty"`
+	ComplementAdresse    string     `json:"complement_adresse,omitempty" bson:"complement_adresse,omitempty"`
+	NumVoie              string     `json:"numero_voie,omitempty" bson:"numero_voie,omitempty"`
+	IndRep               string     `json:"indrep,omitempty" bson:"indrep,omitempty"`
+	TypeVoie             string     `json:"type_voie,omitempty" bson:"type_voie,omitempty"`
+	Voie                 string     `json:"voie,omitempty" bson:"voie,omitempty"`
+	Commune              string     `json:"commune,omitempty" bson:"commune,omitempty"`
+	CommuneEtranger      string     `json:"commune_etranger,omitempty" bson:"commune_etranger,omitempty"`
+	DistributionSpeciale string     `json:"distribution_speciale,omitempty" bson:"distribution_speciale,omitempty"`
+	CodeCommune          string     `json:"code_commune,omitempty" bson:"code_commune,omitempty"`
+	CodeCedex            string     `json:"code_cedex,omitempty" bson:"code_cedex,omitempty"`
+	Cedex                string     `json:"cedex,omitempty" bson:"cedex,omitempty"`
+	CodePaysEtranger     string     `json:"code_pays_etranger,omitempty" bson:"code_pays_etranger,omitempty"`
+	PaysEtranger         string     `json:"pays_etranger,omitempty" bson:"pays_etranger,omitempty"`
+	CodePostal           string     `json:"code_postal,omitempty" bson:"code_postal,omitempty"`
+	Departement          string     `json:"departement,omitempty" bson:"departement,omitempty"`
+	APE                  string     `json:"ape,omitempty" bson:"ape,omitempty"`
+	CodeActivite         string     `json:"code_activite,omitempty" bson:"code_activite,omitempty"`
+	NomenActivite        string     `json:"nomen_activite,omitempty" bson:"nomen_activite,omitempty"`
 	Creation             *time.Time `json:"date_creation,omitempty" bson:"date_creation,omitempty"`
-	Longitude            *float64   `json:"longitude,omitempty" bson:"longitude,omitempty"`
-	Latitude             *float64   `json:"latitude,omitempty" bson:"latitude,omitempty"`
+	Longitude            float64    `json:"longitude,omitempty" bson:"longitude,omitempty"`
+	Latitude             float64    `json:"latitude,omitempty" bson:"latitude,omitempty"`
 }
 
 var f = map[string]int{
@@ -100,7 +106,59 @@ var f = map[string]int{
 	"geo_l5":                                         56,
 }
 
-// Key id de l'objet
+var typeVoie = map[string]string{
+	"ALL":  "ALLÉE",
+	"AV":   "AVENUE",
+	"BD":   "BOULEVARD",
+	"CAR":  "CARREFOUR",
+	"CHE":  "CHEMIN",
+	"CHS":  "CHAUSSÉE",
+	"CITE": "CITÉ",
+	"COR":  "CORNICHE",
+	"CRS":  "COURS",
+	"DOM":  "DOMAINE",
+	"DSC":  "DESCENTE",
+	"ECA":  "ECART",
+	"ESP":  "ESPLANADE",
+	"FG":   "FAUBOURG",
+	"GR":   "GRANDE RUE",
+	"HAM":  "HAMEAU",
+	"HLE":  "HALLE",
+	"IMP":  "IMPASSE",
+	"LD":   "LIEU DIT",
+	"LOT":  "LOTISSEMENT",
+	"MAR":  "MARCHÉ",
+	"MTE":  "MONTÉE",
+	"PAS":  "PASSAGE",
+	"PL":   "PLACE",
+	"PLN":  "PLAINE",
+	"PLT":  "PLATEAU",
+	"PRO":  "PROMENADE",
+	"PRV":  "PARVIS",
+	"QUA":  "QUARTIER",
+	"QUAI": "QUAI",
+	"RES":  "RÉSIDENCE",
+	"RLE":  "RUELLE",
+	"ROC":  "ROCADE",
+	"RPT":  "ROND POINT",
+	"RTE":  "ROUTE",
+	"RUE":  "RUE",
+	"SEN":  "SENTE - SENTIER",
+	"SQ":   "SQUARE",
+	"TPL":  "TERRE-PLEIN",
+	"TRA":  "TRAVERSE",
+	"VLA":  "VILLA",
+	"VLGE": "VILLAGE",
+}
+
+var indRep = map[string]string{
+	"B": "BIS",
+	"T": "TER",
+	"Q": "QUATER",
+	"C": "QUINQUIES",
+}
+
+// Key id de l'objet",
 func (sirene Sirene) Key() string {
 	return sirene.Siren + sirene.Nic
 }
@@ -172,60 +230,49 @@ func Parser(cache engine.Cache, batch *engine.AdminBatch) (chan engine.Tuple, ch
 
 func readLineEtablissement(row []string, tracker *gournal.Tracker) Sirene {
 	sirene := Sirene{}
-
+	var err error
 	sirene.Siren = row[f["siren"]]
 	sirene.Nic = row[f["nic"]]
+	sirene.Siege, err = strconv.ParseBool(row[f["etablissementSiege"]])
+	tracker.Error(err)
 
-	if row[f["numeroVoieEtablissement"]] != "" {
-		sirene.NumVoie = &row[f["numeroVoieEtablissement"]]
-	}
-
-	if row[f["indiceRepetitionEtablissement"]] != "" {
-		sirene.IndRep = &row[f["indiceRepetitionEtablissement"]]
-	}
-
-	if row[f["typeVoieEtablissement"]] != "" {
-		sirene.TypeVoie = &row[f["typeVoieEtablissement"]]
-	}
-
-	if row[f["libelleVoieEtablissement"]] != "" {
-		sirene.Voie = &row[f["libelleVoieEtablissement"]]
-	}
+	sirene.ComplementAdresse = row[f["complementAdresseEtablissement"]]
+	sirene.NumVoie = row[f["numeroVoieEtablissement"]]
+	sirene.IndRep = indRep[row[f["indiceRepetitionEtablissement"]]]
+	sirene.TypeVoie = typeVoie[row[f["typeVoieEtablissement"]]]
+	sirene.Voie = row[f["libelleVoieEtablissement"]]
+	sirene.Commune = row[f["libelleCommuneEtablissement"]]
+	sirene.CommuneEtranger = row[f["libelleCommuneEtrangerEtablissement"]]
+	sirene.DistributionSpeciale = row[f["distributionSpecialeEtablissement"]]
+	sirene.CodeCommune = row[f["codeCommuneEtablissement"]]
+	sirene.CodeCedex = row[f["codeCedexEtablissement"]]
+	sirene.Cedex = row[f["libelleCedexEtablissement"]]
+	sirene.CodePaysEtranger = row[f["codePaysEtrangerEtablissement"]]
+	sirene.PaysEtranger = row[f["libellePaysEtrangerEtablissement"]]
 
 	if len(row[f["codePostalEtablissement"]]) > 2 {
-		sirene.CodePostal = &row[f["codePostalEtablissement"]]
+		sirene.CodePostal = row[f["codePostalEtablissement"]]
 		departement := row[f["codePostalEtablissement"]][0:2]
+		// traitement pour les départements de Corse
 		if row[f["codePostalEtablissement"]][0:3] == "201" || row[f["codePostalEtablissement"]][0:3] == "200" {
 			departement = "2A"
 		} else if row[f["codePostalEtablissement"]][0:2] == "20" {
 			departement = "2B"
 		}
-		sirene.Departement = &departement
+		sirene.Departement = departement
 	} else {
 		tracker.Error(errors.New("Code postal est manquant ou de format incorrect"))
-	}
-
-	if row[f["codeCedexEtablissement"]] != "" {
-		sirene.Cedex = &row[f["codeCedexEtablissement"]]
-	}
-
-	if row[f["distributionSpecialeEtablissement"]] != "" {
-		sirene.DistributionSpeciale = &row[f["distributionSpecialeEtablissement"]]
-	}
-
-	if row[f["libelleCommuneEtablissement"]] != "" {
-		sirene.Commune = &row[f["libelleCommuneEtablissement"]]
 	}
 
 	if row[f["activitePrincipaleEtablissement"]] != "" {
 		if row[f["nomenclatureActivitePrincipaleEtablissement"]] == "NAFRev2" {
 			ape := strings.Replace(row[f["activitePrincipaleEtablissement"]], ".", "", -1)
 			if matched, err := regexp.MatchString(`^[0-9]{4}[A-Z]$`, ape); err == nil && matched {
-				sirene.APE = &ape
+				sirene.APE = ape
 			}
 		} else {
-			sirene.CodeActivite = &row[f["activitePrincipaleEtablissement"]]
-			sirene.NomenActivite = &row[f["nomenclatureActivitePrincipaleEtablissement"]]
+			sirene.CodeActivite = row[f["activitePrincipaleEtablissement"]]
+			sirene.NomenActivite = row[f["nomenclatureActivitePrincipaleEtablissement"]]
 		}
 	}
 
@@ -236,12 +283,9 @@ func readLineEtablissement(row []string, tracker *gournal.Tracker) Sirene {
 	}
 	tracker.Error(err)
 
-	sirene.Siege, err = strconv.ParseBool(row[f["etablissementSiege"]])
-	tracker.Error(err)
-
 	long, err := strconv.ParseFloat(row[f["longitude"]], 64)
 	if err == nil {
-		sirene.Longitude = &long
+		sirene.Longitude = long
 	}
 	if row[48] != "" {
 		tracker.Error(err)
@@ -249,7 +293,7 @@ func readLineEtablissement(row []string, tracker *gournal.Tracker) Sirene {
 
 	lat, err := strconv.ParseFloat(row[f["latitude"]], 64)
 	if err == nil {
-		sirene.Latitude = &lat
+		sirene.Latitude = lat
 	}
 	if row[49] != "" {
 		tracker.Error(err)
