5145f843cdea18c3bbf84ca65aadd817ae81e04d
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-09-24 15:19:47 +0200

    🧹 suppresion test redondant

--- dbmongo/lib/bdf/main_test.go
+++ dbmongo/lib/bdf/main_test.go
@@ -12,11 +12,6 @@ var update = flag.Bool("update", false, "Update the expected test values in gold
 
 var testData = filepath.Join("testData", "bdfTestData.csv")
 
-func TestBdf(t *testing.T) {
-	var golden = filepath.Join("testData", "expectedBdf.json")
-	marshal.TestParserTupleOutput(t, Parser, marshal.NewCache(), "bdf", testData, golden, *update)
-}
-
 func TestBdfOutput(t *testing.T) {
 	var golden = filepath.Join("testData", "expectedBdfOutput.json")
 	marshal.TestParserOutput(t, Parser, marshal.NewCache(), "bdf", testData, golden, *update)
