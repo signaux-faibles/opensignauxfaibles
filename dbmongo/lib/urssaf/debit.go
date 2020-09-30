3f03d477f6739a3213aed2788af419e0749f8148
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-09-30 16:15:15 +0200

    fix: Renforcement du parseur Bdf (avant de s'intéresser aux autres) (#173)
    Effectué en pair avec Pierre.
    
    ## Changements apportés
    
    - correction de typo: `fataError ` --> `fatalError`
    - remplacement d'occurrences de `base.NewCriticError(errors.New("filterError"), "filter")` par `base.NewFilterError(errors.New("filterError"))`
    - bug fix: `NewFilterError()` retourne directement un `error`, au lieu de `*FilterError`
    - retrait du préfixe "Error while loading or applying filter", car cette erreur n'est générée que lorsqu'on parse des données liées à une entreprise qui doit être filtrée
    - ajout de `NewFilterNotice()`, un raccourci à `NewFilterError()`
    - ajout d'un test pour parseur BDF: "Should return only the companies listed in the filter file"
    - intégration des évenements/erreurs dans le golden file du test de parseur BDF, renommé de `expectedBdf.json` vers `expectedBdfOutput.json` => ajout du helper `TestParserOutput()`, en plus de `TestParserTupleOutput()`, et de `RunParser()` sur lequel il s'appuie.
    
    ## Questions ouvertes
    
    - Est-ce qu'on veut vraiment générer une erreur (`NewFilterNotice()` === `NewFilterError("ligne filtrée")`) à chaque fois qu'une ligne est filtrée ?
    
    ## Prochaines étapes suggérées
    
    - remplacer les appels à `NewFilterError(err)` par `NewFilterNotice()`
    - laisser la responsabilité de valider les SIRET/SIREN aux parseurs, plutôt qu'à `Filter` => rendre les fonctions plus simples: pures et sans retour possible d'erreurs, dans la mesure du possible
    - extraire la logique de parsing par ligne, dans le parseur de bdf, puis dans les autres.

--- dbmongo/lib/urssaf/debit.go
+++ dbmongo/lib/urssaf/debit.go
@@ -150,7 +150,7 @@ func ParserDebit(cache marshal.Cache, batch *base.AdminBatch) (chan marshal.Tupl
 						outputChannel <- debit
 					}
 				} else {
-					tracker.Error(base.NewCriticError(err, "filter"))
+					tracker.Error(base.NewFilterError(err))
 					continue
 				}
 
