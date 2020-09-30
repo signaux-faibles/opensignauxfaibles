e85ffd4bb4d79936184cddd4b914909f1367ad3b
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-09-30 14:43:45 +0200

    🔵 réutilisation du cache avec mappings

--- dbmongo/lib/urssaf/urssaf_test.go
+++ dbmongo/lib/urssaf/urssaf_test.go
@@ -10,9 +10,7 @@ import (
 
 var update = flag.Bool("update", false, "Update the expected test values in golden file")
 
-func TestDebit(t *testing.T) {
-	var golden = filepath.Join("testData", "expectedDebit.json")
-	var testData = filepath.Join("testData", "debitTestData.csv")
+func MakeCacheWithComptesMapping() marshal.Cache {
 	cache := marshal.NewCache()
 	cache.Set("comptes", marshal.MockComptesMapping(
 		map[string]string{
@@ -21,82 +19,44 @@ func TestDebit(t *testing.T) {
 			"450359886246036238": "222222222222222",
 		},
 	))
+	return cache
+}
 
+var cache = MakeCacheWithComptesMapping()
+
+func TestDebit(t *testing.T) {
+	var golden = filepath.Join("testData", "expectedDebit.json")
+	var testData = filepath.Join("testData", "debitTestData.csv")
 	marshal.TestParserTupleOutput(t, ParserDebit, cache, "debit", testData, golden, *update)
 }
 
 func TestDebitCorrompu(t *testing.T) {
 	var golden = filepath.Join("testData", "expectedDebitCorrompu.json")
 	var testData = filepath.Join("testData", "debitCorrompuTestData.csv")
-	cache := marshal.NewCache()
-	cache.Set("comptes", marshal.MockComptesMapping(
-		map[string]string{
-			"111982477292496174": "000000000000000",
-			"636043216536562844": "111111111111111",
-			"450359886246036238": "222222222222222",
-		},
-	))
-
 	marshal.TestParserOutput(t, ParserDebit, cache, "debit", testData, golden, *update)
 }
 
 func TestDelai(t *testing.T) {
 	var golden = filepath.Join("testData", "expectedDelai.json")
 	var testData = filepath.Join("testData", "delaiTestData.csv")
-	cache := marshal.NewCache()
-	cache.Set("comptes", marshal.MockComptesMapping(
-		map[string]string{
-			"111982477292496174": "000000000000000",
-			"636043216536562844": "111111111111111",
-			"450359886246036238": "222222222222222",
-		},
-	))
-
 	marshal.TestParserTupleOutput(t, ParserDelai, cache, "delai", testData, golden, *update)
 }
 
 func TestCcsf(t *testing.T) {
 	var golden = filepath.Join("testData", "expectedCcsf.json")
 	var testData = filepath.Join("testData", "ccsfTestData.csv")
-	cache := marshal.NewCache()
-	cache.Set("comptes", marshal.MockComptesMapping(
-		map[string]string{
-			"111982477292496174": "000000000000000",
-			"636043216536562844": "111111111111111",
-			"450359886246036238": "222222222222222",
-		},
-	))
-
 	marshal.TestParserTupleOutput(t, ParserCCSF, cache, "ccsf", testData, golden, *update)
 }
 
 func TestCotisation(t *testing.T) {
 	var golden = filepath.Join("testData", "expectedCotisation.json")
 	var testData = filepath.Join("testData", "cotisationTestData.csv")
-	cache := marshal.NewCache()
-	cache.Set("comptes", marshal.MockComptesMapping(
-		map[string]string{
-			"111982477292496174": "000000000000000",
-			"636043216536562844": "111111111111111",
-			"450359886246036238": "222222222222222",
-		},
-	))
-
 	marshal.TestParserTupleOutput(t, ParserCotisation, cache, "cotisation", testData, golden, *update)
 }
 
 func TestProcol(t *testing.T) {
 	var golden = filepath.Join("testData", "expectedProcol.json")
 	var testData = filepath.Join("testData", "procolTestData.csv")
-	cache := marshal.NewCache()
-	cache.Set("comptes", marshal.MockComptesMapping(
-		map[string]string{
-			"111982477292496174": "000000000000000",
-			"636043216536562844": "111111111111111",
-			"450359886246036238": "222222222222222",
-		},
-	))
-
 	marshal.TestParserTupleOutput(t, ParserProcol, cache, "procol", testData, golden, *update)
 }
 
