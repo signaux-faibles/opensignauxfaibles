ceece3e600a62c54ae1e1ef6a1d0584687904f9e
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-09-30 14:56:10 +0200

    wip: test de bout en bout pour /api/data/check

---
+++ tests/test-api-check.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# Test de bout en bout de l'API "check".
+# Ce script doit être exécuté depuis la racine du projet. Ex: par test-all.sh.
+
+tests/helpers/mongodb-container.sh stop
+
+set -e # will stop the script if any command fails with a non-zero exit code
+
+# Setup
+FLAGS="$*" # the script will update the golden file if "--update" flag was provided as 1st argument
+TMP_DIR="tests/tmp-test-execution-files"
+OUTPUT_FILE="${TMP_DIR}/test-api-check.output.txt"
+GOLDEN_FILE="tests/output-snapshots/test-api-check.golden.txt"
+mkdir -p "${TMP_DIR}"
+
+# Clean up on exit
+function teardown {
+    tests/helpers/dbmongo-server.sh stop || true # keep tearing down, even if "No matching processes belonging to you were found"
+    tests/helpers/mongodb-container.sh stop
+}
+trap teardown EXIT
+
+PORT="27016" tests/helpers/mongodb-container.sh start
+
+MONGODB_PORT="27016" tests/helpers/dbmongo-server.sh setup
+
+echo ""
+echo "📝 Inserting test data..."
+sleep 1 # give some time for MongoDB to start
+
+tests/helpers/mongodb-container.sh run > /dev/null << CONTENTS
+  db.Admin.insertOne({
+    "_id" : {
+        "key" : "1910",
+        "type" : "batch"
+    },
+    "files": {
+      "admin_urssaf": [
+        "/../lib/urssaf/testData/comptesTestData.csv"
+      ],
+      "debit": [
+        "/../lib/urssaf/testData/debitCorrompuTestData.csv"
+      ]
+    },
+    "param" : {
+        "date_debut" : ISODate("2001-01-01T00:00:00.000+0000"),
+        "date_fin" : ISODate("2019-02-01T00:00:00.000+0000")
+    }
+  })
+CONTENTS
+
+echo ""
+echo "💎 Parsing data thru dbmongo API..."
+tests/helpers/dbmongo-server.sh start
+echo "- POST /api/data/check 👉 $(http --print=b --ignore-stdin :5000/api/data/check batch=1910 noFilter:=true parsers:='["debit"]')"
+
+(tests/helpers/mongodb-container.sh run \
+  | perl -p -e 's/"[0-9a-z]{32}"/"______________Hash______________"/' \
+  | perl -p -e 's/"[0-9a-z]{24}"/"________ObjectId________"/' \
+  > "${OUTPUT_FILE}" \
+) << CONTENT
+print("// Documents from db.Journal:");
+printjson(db.Journal.find({ "event.report": { "\$exists": true } }).toArray().map(doc => ({
+  // note: we use map() to force the order of properties at every run of this test
+  event: {
+    headFilters: doc.event.headFilters,
+    headErrors: doc.event.headErrors,
+    headFatal: doc.event.headFatal,
+    report: doc.event.report,
+    batchKey: doc.event.batchKey
+  },
+  code: doc.code
+})));
+
+print("// Standard output from call to /api/data/check:");
+print("// TODO");
+CONTENT
+
+# Display JS errors logged by MongoDB, if any
+tests/helpers/mongodb-container.sh exceptions || true
+
+tests/helpers/diff-or-update-golden-master.sh "${FLAGS}" "${GOLDEN_FILE}" "${OUTPUT_FILE}"
+
+rm -rf "${TMP_DIR}"
+# Now, the "trap" commands will clean up the rest.
