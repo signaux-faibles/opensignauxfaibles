5d6aa1a49721e83efc72e53a637f11e446b9f6ab
chrnin <christophe.ninucci@direccte.gouv.fr>
2020-10-02 13:53:08 +0200

    cleanup

--- dbmongo/lib/ellisphere/main.go
+++ dbmongo/lib/ellisphere/main.go
@@ -68,21 +68,18 @@ func Parser(cache marshal.Cache, batch *base.AdminBatch) (chan marshal.Tuple, ch
 				continue
 			}
 			sheet := xlsxFile.Sheets[0]
-			err = sheet.ForEachRow(
+			sheet.ForEachRow(
 				func(row *xlsx.Row) error {
 					var ellisphere Ellisphere
 					err := row.ReadStruct(&ellisphere)
+					if err == nil {
+						outputChannel <- ellisphere
+					}
 					tracker.Error(err)
-					outputChannel <- ellisphere
+					tracker.Next()
 					return nil
 				},
 			)
-
-			if err != nil {
-				tracker.Error(err)
-			}
-
-			tracker.Next()
 
 			event.Info(tracker.Report("abstract"))
 		}
@@ -90,86 +87,3 @@ func Parser(cache marshal.Cache, batch *base.AdminBatch) (chan marshal.Tuple, ch
 
 	return outputChannel, eventChannel
 }
-
-// file, err := os.Open(viper.GetString("APP_DATA") + path)
-// if err != nil {
-// 	event.Critical(path + ": erreur à l'ouverture du fichier: " + err.Error())
-// 	return
-// }
-// reader := csv.NewReader(bufio.NewReader(file))
-// reader.Comma = ','
-// reader.LazyQuotes = true
-
-// event.Info(path + ": ouverture")
-
-// header, err := reader.Read()
-// if err != nil {
-// 	tracker.Error(err)
-// 	event.Debug(tracker.Report("invalidLine"))
-// 	break
-// }
-
-// f := make(map[string]int)
-// for idx, field := range header {
-// 	f[field] = idx
-// }
-// fields := []string{
-// 	"ID_DA",
-// 	"ETAB_SIRET",
-// 	"EFF_ENT",
-// 	"EFF_ETAB",
-// 	"DATE_STATUT",
-// 	"DATE_DEB",
-// 	"DATE_FIN",
-// 	"HTA",
-// 	"EFF_AUTO",
-// 	"MOTIF_RECOURS_SE",
-// 	"S_HEURE_CONSOM_TOT",
-// 	"S_EFF_CONSOM_TOT",
-// }
-
-// for _, field := range fields {
-// 	if _, found := f[field]; !found {
-// 		event.Critical("Import du fichier " + path + ". " + field + " non trouvé. Abandon.")
-// 		continue
-// 	}
-// }
-// for {
-// 	row, err := reader.Read()
-// 	if err == io.EOF {
-// 		break
-// 	} else if err != nil {
-// 		tracker.Error(err)
-// 		event.Debug(tracker.Report("invalidLine"))
-// 		break
-// 	}
-
-// 	if row[f["ETAB_SIRET"]] != "" {
-// 		apdemande := APDemande{}
-// 		apdemande.ID = row[f["ID_DA"]]
-// 		apdemande.Siret = row[f["ETAB_SIRET"]]
-// 		apdemande.EffectifEntreprise, err = misc.ParsePInt(row[f["EFF_ENT"]])
-// 		tracker.Error(err)
-// 		apdemande.Effectif, err = misc.ParsePInt(row[f["EFF_ETAB"]])
-// 		tracker.Error(err)
-// 		apdemande.DateStatut, err = time.Parse("02/01/2006", row[f["DATE_STATUT"]])
-// 		tracker.Error(err)
-// 		apdemande.Periode = misc.Periode{}
-// 		apdemande.Periode.Start, err = time.Parse("02/01/2006", row[f["DATE_DEB"]])
-// 		tracker.Error(err)
-// 		apdemande.Periode.End, err = time.Parse("02/01/2006", row[f["DATE_FIN"]])
-// 		tracker.Error(err)
-// 		apdemande.HTA, err = misc.ParsePFloat(row[f["HTA"]])
-// 		tracker.Error(err)
-// 		apdemande.MTA, err = misc.ParsePFloat(strings.ReplaceAll(row[f["MTA"]], ",", "."))
-// 		tracker.Error(err)
-// 		apdemande.EffectifAutorise, err = misc.ParsePInt(row[f["EFF_AUTO"]])
-// 		tracker.Error(err)
-// 		apdemande.MotifRecoursSE, err = misc.ParsePInt(row[f["MOTIF_RECOURS_SE"]])
-// 		tracker.Error(err)
-// 		apdemande.HeureConsommee, err = misc.ParsePFloat(row[f["S_HEURE_CONSOM_TOT"]])
-// 		tracker.Error(err)
-// 		apdemande.EffectifConsomme, err = misc.ParsePInt(row[f["S_EFF_CONSOM_TOT"]])
-// 		tracker.Error(err)
-// 		apdemande.MontantConsomme, err = misc.ParsePFloat(strings.ReplaceAll(row[f["S_MONTANT_CONSOM_TOT"]], ",", "."))
-// 		tracker.Error(err)
