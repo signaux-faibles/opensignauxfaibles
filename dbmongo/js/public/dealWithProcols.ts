d63c1d795c387bc6ea0f819a0688a4bfb0fb5630
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-10-14 10:13:02 +0200

    feat: Retrait des parseurs crp et altares (#197)
    Remplace les PRs #194 et #195, car ces données ne sont plus utilisées.

--- dbmongo/js/public/dealWithProcols.ts
+++ dbmongo/js/public/dealWithProcols.ts
@@ -1,26 +1,21 @@
 import { f } from "./functions"
-import { AltaresToHumanRes } from "../common/altaresToHuman"
 import { ProcolToHumanRes } from "../common/procolToHuman"
 import { EntréeDefaillances, ParHash } from "../RawDataTypes"
 
 export type SortieProcols = {
-  etat: AltaresToHumanRes | ProcolToHumanRes
+  etat: ProcolToHumanRes
   date_procol: Date
 }
 
 export function dealWithProcols(
-  data_source: ParHash<EntréeDefaillances> = {},
-  altar_or_procol: "altares" | "procol"
+  data_source: ParHash<EntréeDefaillances> = {}
 ): SortieProcols[] {
   return Object.keys(data_source)
     .reduce((events, hash) => {
       const the_event = data_source[hash]
 
-      let etat: AltaresToHumanRes | ProcolToHumanRes = null
-      if (altar_or_procol === "altares")
-        etat = f.altaresToHuman(the_event.code_evenement)
-      else if (altar_or_procol === "procol")
-        etat = f.procolToHuman(the_event.action_procol, the_event.stade_procol)
+      let etat: ProcolToHumanRes = null
+      etat = f.procolToHuman(the_event.action_procol, the_event.stade_procol)
 
       if (etat !== null)
         events.push({ etat, date_procol: new Date(the_event.date_effet) })
--- dbmongo/js/reduce.algo2/defaillances.ts
+++ dbmongo/js/reduce.algo2/defaillances.ts
@@ -5,11 +5,9 @@ import { EntréeDefaillances, ParPériode, ParHash } from "../RawDataTypes"
 export type SortieDefaillances = SortieProcols
 
 export function defaillances(
-  altares: ParHash<EntréeDefaillances>,
   procol: ParHash<EntréeDefaillances>,
   output_indexed: ParPériode<Partial<SortieDefaillances>>
 ): void {
   "use strict"
-  f.dealWithProcols(altares, "altares", output_indexed)
-  f.dealWithProcols(procol, "procol", output_indexed)
+  f.dealWithProcols(procol, output_indexed)
 }
