5ff7feed95119f82eb251f408ed7158eebec57f5
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-10-14 16:18:08 +0200

    🧹suppression de TestParserTupleOutput(), au profit de TestParserOutput()

--- dbmongo/lib/marshal/testing.go
+++ dbmongo/lib/marshal/testing.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"log"
-	"strings"
 	"sync"
 	"testing"
 	"time"
@@ -20,7 +19,7 @@ func MockComptesMapping(mapping map[string]string) Comptes {
 	MakeSiretDateArray := func(siret string) []SiretDate {
 		longAgo, _ := time.Parse("2006-01-02", "9999-01-02")
 		return []SiretDate{
-			SiretDate{
+			{
 				Siret: siret,
 				Date:  longAgo,
 			},
@@ -30,64 +29,6 @@ func MockComptesMapping(mapping map[string]string) Comptes {
 		mockComptes[compte] = MakeSiretDateArray(siret)
 	}
 	return mockComptes
-}
-
-// TestParserTupleOutput helps to test the output of a Parser. It compares
-// output Tuples with JSON stored in a golden file. If update = true, the
-// the golden file is updated.
-func TestParserTupleOutput( // TODO: à supprimer
-	t *testing.T,
-	parser Parser,
-	cache Cache,
-	parserType string,
-	inputFile string,
-	goldenFile string,
-	update bool,
-) {
-	batch := base.MockBatch(parserType, []string{inputFile})
-	var events chan Event
-	var tuples chan Tuple
-	tuples, events = parser(cache, &batch)
-	var firstCriticalEvent *Event
-
-	// intercepter et afficher les évènements pendant l'importation
-	var wg sync.WaitGroup
-	wg.Add(1)
-	defer wg.Wait() // pour éviter "panic: Log in goroutine after TestEffectif has completed"
-	go func() {
-		defer wg.Done()
-		for event := range events {
-			t.Logf("[%s] event: %v", event.Priority, event.Comment)
-			if event.Priority == Critical && firstCriticalEvent == nil {
-				firstCriticalEvent = &event
-			}
-		}
-	}()
-
-	actualJsons := []string{}
-	for tuple := range tuples {
-		json, err := GetJSON(tuple)
-		if err != nil {
-			log.Fatal(err)
-		}
-		actualJsons = append(actualJsons, string(json))
-	}
-
-	actual := "[" + strings.Join(actualJsons, ",") + "]"
-	if update {
-		ioutil.WriteFile(goldenFile, []byte(actual), 0644)
-	}
-
-	expected, err := ioutil.ReadFile(goldenFile)
-	if err != nil {
-		t.Fatal("Could not open golden file" + err.Error())
-	}
-
-	if firstCriticalEvent != nil {
-		assert.FailNow(t, "Caught Critical event: ", firstCriticalEvent.Comment)
-	}
-
-	assert.Equal(t, string(expected), string(actual))
 }
 
 type tuplesAndEvents = struct {
