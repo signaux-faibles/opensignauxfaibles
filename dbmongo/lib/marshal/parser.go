7a9caec6f049ecbcdfd3a76b6f245ee0aca1b160
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-10-26 16:02:13 +0100

    pour éviter de mélanger différents types d'entrées de journal, retrait des entrées "ouverture"

--- dbmongo/lib/marshal/parser.go
+++ dbmongo/lib/marshal/parser.go
@@ -44,7 +44,6 @@ func ParseFilesFromBatch(cache Cache, batch *base.AdminBatch, parser Parser) (ch
 				map[string]string{"path": path, "batchKey": batch.ID.Key},
 				TrackerReports)
 
-			event.Info(path + ": ouverture")
 			parser.FileParser(viper.GetString("APP_DATA")+path, &cache, batch, &tracker, outputChannel)
 			event.Info(tracker.Report("abstract"))
 		}
