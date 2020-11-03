9aeb0e155b188a44041c84119a138f10be254a5a
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-11-03 15:13:34 +0100

    fix: rendu de l'erreur de siret manquant à une période donnée, pour un compte urssaf

--- dbmongo/lib/urssaf/compte.go
+++ dbmongo/lib/urssaf/compte.go
@@ -68,7 +68,7 @@ func parseCompteLines(periodes []time.Time, mapping *marshal.Comptes, parsedLine
 			compte.NumeroCompte = account
 			compte.Periode = p
 			compte.Siret, err = marshal.GetSiretFromComptesMapping(account, &p, *mapping)
-			parsedLine.AddError(base.NewCriticError(err, "erreur"))
+			parsedLine.AddError(base.NewRegularError(err))
 			parsedLine.AddTuple(compte)
 		}
 		parsedLineChan <- parsedLine
