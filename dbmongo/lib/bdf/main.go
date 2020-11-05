7ae36240d3e7fa3f7111dd74494c76186fc2be88
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-11-05 16:48:26 +0100

    conversion de bdf

--- dbmongo/lib/bdf/main.go
+++ dbmongo/lib/bdf/main.go
@@ -47,33 +47,39 @@ func (bdf BDF) Scope() string {
 var Parser = marshal.Parser{FileType: "bdf", FileParser: ParseFile}
 
 // ParseFile permet de lancer le parsing du fichier demandé.
-func ParseFile(filePath string, cache *marshal.Cache, batch *base.AdminBatch) marshal.OpenFileResult {
-	closeFct, reader, err := openFile(filePath)
-	return marshal.OpenFileResult{
-		Error: err,
-		ParseLines: func(parsedLineChan chan marshal.ParsedLineResult) {
-			parseLines(reader, parsedLineChan)
-		},
-		Close: closeFct,
-	}
+func ParseFile(filePath string, cache *marshal.Cache, batch *base.AdminBatch) (marshal.FileReader, error) {
+	file, reader, err := openFile(filePath)
+	return bdfReader{
+		file:   file,
+		reader: reader,
+	}, err
 }
 
-func openFile(filePath string) (func() error, *csv.Reader, error) {
+type bdfReader struct {
+	file   *os.File
+	reader *csv.Reader
+}
+
+func (parser bdfReader) Close() error {
+	return parser.file.Close()
+}
+
+func openFile(filePath string) (*os.File, *csv.Reader, error) {
 	file, err := os.Open(filePath)
 	if err != nil {
-		return file.Close, nil, err
+		return file, nil, err
 	}
 	reader := csv.NewReader(bufio.NewReader(file))
 	reader.Comma = ';'
 	reader.LazyQuotes = true
 	_, err = reader.Read() // Sauter l'en-tête
-	return file.Close, reader, err
+	return file, reader, err
 }
 
-func parseLines(reader *csv.Reader, parsedLineChan chan marshal.ParsedLineResult) {
+func (parser bdfReader) ParseLines(parsedLineChan chan marshal.ParsedLineResult) {
 	for {
 		parsedLine := marshal.ParsedLineResult{}
-		row, err := reader.Read()
+		row, err := parser.reader.Read()
 		if err == io.EOF {
 			close(parsedLineChan)
 			break
