ce6bac2653ae7e9527146c6e340df46ebc699232
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2020-11-13 11:09:56 +0100

    Green: ajout test ouverture/cloture redressement

--- dbmongo/js/reduce.algo2/dealWithProcols_tests.ts
+++ dbmongo/js/reduce.algo2/dealWithProcols_tests.ts
@@ -2,19 +2,19 @@ import test from "ava"
 import { dealWithProcols, InputEvent } from "./dealWithProcols"
 import { ParHash } from "../RawDataTypes"
 
-test("Une entrée en liquidation est prise en compte dans la période courante et les suivantes", (t) => {
+test("Une ouverture de liquidation est prise en compte dans la période courante et les suivantes", (t) => {
   const output_indexed = {
     ["2018-01-01"]: {},
     ["2018-02-01"]: {},
     ["2018-03-01"]: {},
   }
 
-  const date_proc_collective = new Date("2018-02-12")
+  const date_ouverture = new Date("2018-02-12")
   const data_source = {
     ["123"]: {
       action_procol: "liquidation",
       stade_procol: "ouverture",
-      date_effet: date_proc_collective,
+      date_effet: date_ouverture,
     },
   } as ParHash<InputEvent>
 
@@ -22,13 +22,54 @@ test("Une entrée en liquidation est prise en compte dans la période courante e
   const expected = {
     ["2018-01-01"]: {},
     ["2018-02-01"]: {
-      date_proc_collective,
+      date_proc_collective: date_ouverture,
       etat_proc_collective: "liquidation",
       tag_failure: true,
     },
     ["2018-03-01"]: {
-      date_proc_collective,
+      date_proc_collective: date_ouverture,
       etat_proc_collective: "liquidation",
+      tag_failure: true,
+    },
+  }
+
+  t.deepEqual(output_indexed, expected)
+})
+
+test("Une ouverture puis cloture d'un redressement sont pris en compte, tag_failure reste à TRUE", (t) => {
+  const output_indexed = {
+    ["2018-01-01"]: {},
+    ["2018-02-01"]: {},
+    ["2018-03-01"]: {},
+  }
+
+  const date_ouverture = new Date("2018-02-12")
+  const date_cloture = new Date("2018-03-05")
+
+  const data_source = {
+    ["123"]: {
+      action_procol: "redressement",
+      stade_procol: "ouverture",
+      date_effet: date_ouverture,
+    },
+    ["456"]: {
+      action_procol: "redressement",
+      stade_procol: "fin_procedure",
+      date_effet: date_cloture,
+    },
+  } as ParHash<InputEvent>
+
+  dealWithProcols(data_source, output_indexed)
+  const expected = {
+    ["2018-01-01"]: {},
+    ["2018-02-01"]: {
+      date_proc_collective: date_ouverture,
+      etat_proc_collective: "plan_redressement",
+      tag_failure: true,
+    },
+    ["2018-03-01"]: {
+      date_proc_collective: date_cloture,
+      etat_proc_collective: "in_bonis",
       tag_failure: true,
     },
   }
