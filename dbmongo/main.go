10575bf200731759bce63f85a8e9c63ed4b14d77
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-11-20 11:15:34 +0100

    suppression /data/copyScores et /debug

--- dbmongo/main.go
+++ dbmongo/main.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"time"
-
-	"github.com/globalsign/mgo/bson"
-
 	"github.com/signaux-faibles/opensignauxfaibles/dbmongo/lib/engine"
 
 	"github.com/signaux-faibles/opensignauxfaibles/dbmongo/lib/naf"
@@ -66,60 +62,11 @@ func main() {
 
 		api.GET("/data/purgeNotCompacted", purgeNotCompactedHandler)
 
-		api.POST("/data/copyScores", copyScores)
-
 		api.GET("/data/etablissements", exportEtablissementsHandler)
 		api.GET("/data/entreprises", exportEntreprisesHandler)
 		api.POST("/data/validate", validateHandler)
-
-		// api.GET("/debug", debug)
 	}
 
 	bind := viper.GetString("APP_BIND")
 	r.Run(bind)
-}
-
-func copyScores(c *gin.Context) {
-	var params struct {
-		From string `json:"from"`
-		To   string `json:"to"`
-		Algo string `json:"algo"`
-	}
-	c.Bind(&params)
-
-	type Score struct {
-		ID        bson.ObjectId `bson:"_id"`
-		Siret     string        `bson:"siret"`
-		Periode   string        `bson:"periode"`
-		Score     float64       `bson:"score"`
-		ScoreDiff float64       `bson:"score_diff"`
-		Algo      string        `bson:"algo"`
-		Batch     string        `bson:"batch"`
-		Timestamp time.Time     `bson:"timestamp"`
-		Alert     string        `bson:"alert"`
-	}
-
-	to, _ := engine.Db.DB.C("Scores").Find(bson.M{
-		"batch": params.To,
-		"algo":  params.Algo,
-	}).Count()
-
-	if to > 0 {
-		c.JSON(300, "il existe déjà des objets dans la destination")
-		return
-	}
-
-	from := engine.Db.DB.C("Scores").Find(bson.M{
-		"batch": params.From,
-		"algo":  params.Algo,
-	}).Iter()
-
-	var f Score
-	var dest []interface{}
-	for from.Next(&f) {
-		f.ID = bson.NewObjectId()
-		f.Batch = params.To
-		dest = append(dest, f)
-	}
-	engine.Db.DB.C("Scores").Insert(dest...)
 }
