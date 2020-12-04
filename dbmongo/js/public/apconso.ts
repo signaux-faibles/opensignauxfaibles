cf7ecf9a241b577fdd13d53662f081f5c3ba84e6
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-12-04 15:30:13 +0100

    fix Uncaught exception in public/ava_tests.ts
      public/apconso.ts(5,3): error TS2322: Type '(EntréeApConso | undefined)[]' is not assignable to type 'EntréeApConso[]'.
        Type 'EntréeApConso | undefined' is not assignable to type 'EntréeApConso'.
          Type 'undefined' is not assignable to type 'EntréeApConso'.
      public/apconso.ts(7,24): error TS2532: Object is possibly 'undefined'.
      public/apconso.ts(7,37): error TS2532: Object is possibly 'undefined'.

--- dbmongo/js/public/apconso.ts
+++ dbmongo/js/public/apconso.ts
@@ -1,8 +1,7 @@
-import { f } from "./functions"
 import { EntréeApConso, ParHash } from "../RawDataTypes"
 
 export function apconso(apconso?: ParHash<EntréeApConso>): EntréeApConso[] {
-  return f
-    .iterable(apconso)
-    .sort((p1, p2) => (p1.periode < p2.periode ? 1 : -1))
+  return Object.values(apconso ?? {}).sort((p1, p2) =>
+    p1.periode < p2.periode ? 1 : -1
+  )
 }
