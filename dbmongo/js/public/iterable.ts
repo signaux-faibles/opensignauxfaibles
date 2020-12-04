023e8e8ea2eb00c1421e955656f40723555e4545
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-12-04 15:23:37 +0100

    fix Uncaught exception in public/ava_tests.ts
      public/iterable.ts(2,3): error TS2322: Type '(T | undefined)[]' is not assignable to type 'T[]'.
        Type 'T | undefined' is not assignable to type 'T'.
          'T' could be instantiated with an arbitrary type which could be unrelated to 'T | undefined'.
            Type 'undefined' is not assignable to type 'T'.
              'T' could be instantiated with an arbitrary type which could be unrelated to 'undefined'.

--- dbmongo/js/public/iterable.ts
+++ dbmongo/js/public/iterable.ts
@@ -1,3 +1,5 @@
-export function iterable<T>(dict?: Record<string | number, T>): T[] {
+export function iterable<T>(
+  dict?: Record<string | number, T>
+): (T | undefined)[] {
   return typeof dict === "object" ? Object.keys(dict).map((h) => dict[h]) : []
 }
