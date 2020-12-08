344d324f48b64235dfbe9da57c6f949a7ce41565
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-12-08 14:21:38 +0100

    add test: "reports a fatal error in case of unexpected csv header"

--- dbmongo/lib/sirene_ul/main_test.go
+++ dbmongo/lib/sirene_ul/main_test.go
@@ -35,6 +35,18 @@ func TestSireneUlHeader(t *testing.T) {
 		reportData, _ := parseReport(output.Events[0])
 		assert.Equal(t, false, reportData["isFatal"], "should not report a fatal error")
 	})
+
+	t.Run("reports a fatal error in case of unexpected csv header", func(t *testing.T) {
+		csvData := strings.Join([]string{
+			"sirenXYZ,statutDiffusionUniteLegale,unitePurgeeUniteLegale,dateCreationUniteLegale,sigleUniteLegale,sexeUniteLegale,prenom1UniteLegale,prenom2UniteLegale,prenom3UniteLegale,prenom4UniteLegale,prenomUsuelUniteLegale,pseudonymeUniteLegale,identifiantAssociationUniteLegale,trancheEffectifsUniteLegale,anneeEffectifsUniteLegale,dateDernierTraitementUniteLegale,nombrePeriodesUniteLegale,categorieEntreprise,anneeCategorieEntreprise,dateDebut,etatAdministratifUniteLegale,nomUniteLegale,nomUsageUniteLegale,denominationUniteLegale,denominationUsuelle1UniteLegale,denominationUsuelle2UniteLegale,denominationUsuelle3UniteLegale,categorieJuridiqueUniteLegale,activitePrincipaleUniteLegale,nomenclatureActivitePrincipaleUniteLegale,nicSiegeUniteLegale,economieSocialeSolidaireUniteLegale,caractereEmployeurUniteLegale",
+		}, "\n")
+		csvFile := createTempFileWithContent(t, []byte(csvData))
+		output := marshal.RunParser(Parser, marshal.NewCache(), csvFile.Name())
+		assert.Equal(t, []marshal.Tuple(nil), output.Tuples, "should return no tuples")
+		assert.Equal(t, 1, len(output.Events), "should return a parsing report")
+		reportData, _ := parseReport(output.Events[0])
+		assert.Equal(t, true, reportData["isFatal"], "should report a fatal error")
+	})
 }
 
 func parseReport(reportEvent marshal.Event) (map[string]interface{}, error) {
