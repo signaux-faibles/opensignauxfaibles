291a0b043c9776ebc71ed566606377fa9f11b8cd
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-12-10 14:24:09 +0100

    mise à jour du test d'api import, aussi basé sur ValidateDataEntries

--- tests/test-api-import.sh
+++ tests/test-api-import.sh
@@ -95,7 +95,7 @@ printjson(db.ImportedData.find().sort({"value.key":1}).toArray().map(doc => ({
 
 print("// Reports from db.Journal:");
 // on classe les données par type, de manière à ce que l'ordre soit stable
-printjson(db.Journal.find().sort({ parserCode: 1 }).toArray().map(doc => ({
+printjson(db.Journal.find().sort({ parserCode: 1 }).toArray().map(doc => (doc.event ? {
   event: {
     headRejected: doc.event.headRejected,
     headFatal: doc.event.headFatal,
@@ -105,6 +105,10 @@ printjson(db.Journal.find().sort({ parserCode: 1 }).toArray().map(doc => ({
   },
   reportType: doc.reportType,
   parserCode: doc.parserCode
+} : {
+  reportType: doc.reportType,
+  hasDate: !!doc.date,
+  hasStartDate: !!doc.startDate,
 })));
 
 print("// Results of call to /api/data/validate:");
