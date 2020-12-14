ed5b17f30bcb15e711e2d916af3c4e104fff772c
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-12-14 10:45:09 +0100

    update build (not tested)

--- build.sh
+++ build.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 # build.sh compile les éléments et dispose le nécessaire dans le répertoire dist.
 # ce script ne se charge pas d'installer les éléments nécessaires à la compilation.
-mkdir -p dist/dbmongo/static
+mkdir -p dist/static
 
-# dbmongo: compilation et recopie des éléments nécessaires
-cd dbmongo 
-go build -ldflags="-s -w"
-upx dbmongo
-cp ./dbmongo ../dist/dbmongo/ 
-cp -r js/ ../dist/dbmongo 
-cp config.toml ../dist/dbmongo
+# compilation et recopie des éléments nécessaires
+go build -ldflags="-s -w" -o "sfdata"
+upx sfdata
+cp sfdata ../dist
+cp config.toml ../dist
 
 # création de l'archive
-cd ..
 tar cvJf dist.tar.xz dist/
 
 # cleanup
-
-rm dbmongo/dbmongo
+rm sfdata
 rm -r dist/
