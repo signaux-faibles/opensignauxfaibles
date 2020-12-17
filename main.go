cc53e861cbca8b0cb3214a2cc98dc44844478889
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-12-17 13:56:31 +0100

    wip: ajout d'un champ de description concise pour chaque commande

--- main.go
+++ main.go
@@ -33,17 +33,17 @@ func main() {
 	engine.FlushEventQueue()
 }
 
-var cmds = map[string]commandDefinition{
+var cmds = map[string]*commandDefinition{
 	// TODO: convert api.GET("/data/purgeNotCompacted", purgeNotCompactedHandler)
-	"purge": func(args []string) error {
+	"purge": {"TODO - summary", func(args []string) error {
 		var params purgeBatchParams // TODO: also populate other parameters
 		flag.StringVar(&params.FromBatchKey, "since-batch", "", "Batch identifier")
 		flag.BoolVar(&params.IUnderstandWhatImDoing, "i-understand-what-im-doing", false, "Confirm data deletion")
 		flag.CommandLine.Parse(args)
 		connectDb()
 		return purgeBatchHandler(params) // [x] écrit dans Journal
-	},
-	"check": func(args []string) error {
+	}},
+	"check": {"TODO - summary", func(args []string) error {
 		var parsers string
 		params := checkBatchParams{}
 		flag.StringVar(&params.BatchKey, "batch", "", "Batch identifier")
@@ -52,70 +52,73 @@ var cmds = map[string]commandDefinition{
 		params.Parsers = strings.Split(parsers, ",")
 		connectDb()
 		return checkBatchHandler(params) // [x] écrit dans Journal
-	},
-	"pruneEntities": func(args []string) error {
+	}},
+	"pruneEntities": {"TODO - summary", func(args []string) error {
 		params := pruneEntitiesParams{}
 		flag.StringVar(&params.BatchKey, "batch", "", "Batch identifier")
 		flag.BoolVar(&params.Delete, "delete", false, "Delete entities")
 		flag.CommandLine.Parse(args)
 		connectDb()
 		return pruneEntitiesHandler(params) // [x] écrit dans Journal
-	},
-	"import": func(args []string) error {
+	}},
+	"import": {"TODO - summary", func(args []string) error {
 		params := importBatchParams{} // TODO: also populate other parameters
 		flag.StringVar(&params.BatchKey, "batch", "", "Batch identifier")
 		flag.BoolVar(&params.NoFilter, "no-filter", false, "Tolerate the absence of filter/perimeter file")
 		flag.CommandLine.Parse(args)
 		connectDb()
 		return importBatchHandler(params) // [x] écrit dans Journal
-	},
-	"validate": func(args []string) error {
+	}},
+	"validate": {"TODO - summary", func(args []string) error {
 		params := validateParams{}
 		flag.StringVar(&params.Collection, "collection", "", "Name of the collection to validate")
 		flag.CommandLine.Parse(args)
 		connectDb()
 		return validateHandler(params) // [x] écrit dans Journal
-	},
-	"compact": func(args []string) error {
+	}},
+	"compact": {"TODO - summary", func(args []string) error {
 		params := compactParams{}
 		flag.StringVar(&params.FromBatchKey, "since-batch", "", "Batch identifier")
 		flag.CommandLine.Parse(args)
 		connectDb()
 		return compactHandler(params) // [x] écrit dans Journal
-	},
-	"reduce": func(args []string) error {
+	}},
+	"reduce": {"TODO - summary", func(args []string) error {
 		params := reduceParams{} // TODO: also populate other parameters
 		flag.StringVar(&params.BatchKey, "until-batch", "", "Batch identifier")
 		flag.StringVar(&params.Key, "key", "", "SIRET or SIREN to focus on")
 		flag.CommandLine.Parse(args)
 		connectDb()
 		return reduceHandler(params) // [x] écrit dans Journal
-	},
-	"public": func(args []string) error {
+	}},
+	"public": {"TODO - summary", func(args []string) error {
 		params := publicParams{}
 		flag.StringVar(&params.BatchKey, "until-batch", "", "Batch identifier")
 		flag.StringVar(&params.Key, "key", "", "SIRET or SIREN to focus on")
 		flag.CommandLine.Parse(args)
 		connectDb()
 		return publicHandler(params) // [x] écrit dans Journal
-	},
-	"etablissements": func(args []string) error {
+	}},
+	"etablissements": {"TODO - summary", func(args []string) error {
 		params := exportParams{}
 		flag.StringVar(&params.Key, "key", "", "SIRET or SIREN to export")
 		flag.CommandLine.Parse(args)
 		connectDb()
 		return exportEtablissementsHandler(params) // TODO: écrire rapport dans Journal ?
-	},
-	"entreprises": func(args []string) error {
+	}},
+	"entreprises": {"TODO - summary", func(args []string) error {
 		params := exportParams{}
 		flag.StringVar(&params.Key, "key", "", "SIRET or SIREN to export")
 		flag.CommandLine.Parse(args)
 		connectDb()
 		return exportEntreprisesHandler(params) // TODO: écrire rapport dans Journal ?
-	},
+	}},
 }
 
-type commandDefinition func(args []string) error
+type commandDefinition struct {
+	summary string
+	run     func(args []string) error
+}
 
 func runCommand(args []string) error {
 	if len(args) < 1 {
@@ -124,9 +127,9 @@ func runCommand(args []string) error {
 	}
 
 	command := os.Args[1]
-	commandFct := cmds[command]
-	if commandFct != nil {
-		return commandFct(os.Args[2:])
+	commandDef := cmds[command]
+	if commandDef != nil {
+		return commandDef.run(os.Args[2:])
 	}
 
 	printSupportedCommands()
@@ -135,7 +138,7 @@ func runCommand(args []string) error {
 
 func printSupportedCommands() {
 	fmt.Println("Supported commands:")
-	for cmd := range cmds {
-		fmt.Printf(" - %s\n", cmd)
+	for cmd, cmdDef := range cmds {
+		fmt.Printf(" - %s (%s)\n", cmd, cmdDef.summary)
 	}
 }
