b0ba8929e18086cc5138cccbce76a7cef7466e23
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-12-18 10:50:09 +0100

    définition de plusieurs commandes

--- flag/main.go
+++ flag/main.go
@@ -1,3 +1,5 @@
+// Test with `$ go build -o "go" . && ./go --help`
+
 package main
 
 import (
@@ -7,56 +9,60 @@ import (
 	flag "github.com/cosiner/flag"
 )
 
-type Tar struct {
-	GZ          bool     `names:"-z, --gz" usage:"gzip format"`
-	BZ          bool     `names:"-j, --bz" usage:"bzip2 format"`
-	XZ          bool     `names:"-J, --xz" usage:"xz format"`
-	Create      bool     `names:"-c" usage:"create tar file"`
-	Extract     bool     `names:"-x" usage:"extract tar file"`
-	File        string   `names:"-f" usage:"output file for create or input file for extract"`
-	Directory   string   `names:"-C" usage:"extract directory"`
-	SourceFiles []string `args:"true"`
+type GoCmd struct {
+	Build struct {
+		Enable  bool
+		Already bool   `names:"-a" important:"1" desc:"force rebuilding of packages that are already up-to-date."`
+		Race    bool   `important:"1" desc:"enable data race detection.\nSupported only on linux/amd64, freebsd/amd64, darwin/amd64 and windows/amd64."`
+		Output  string `names:"-o" arglist:"output" important:"1" desc:"only allowed when compiling a single package"`
+
+		LdFlags  string   `names:"-ldflags" arglist:"'flag list'" desc:"arguments to pass on each go tool link invocation."`
+		Packages []string `args:"true"`
+	} `usage:"compile packages and dependencies"`
+	Clean struct {
+		Enable bool
+	} `usage:"remove object files"`
 }
 
-func (t *Tar) Metadata() map[string]flag.Flag {
-	const (
-		usage   = "tar is a tool for manipulate tape archives."
-		version = `
-			version: v1.0.0
-			commit: 10adf10dc10
-			date:   2017-01-01 10:00:01
-		`
-		desc = `
-		tar creates and manipulates streaming archive files.  This implementation can extract
-		from tar, pax, cpio, zip, jar, ar, and ISO 9660 cdrom images and can create tar, pax,
-		cpio, ar, and shar archives.
-		`
-	)
+func (*GoCmd) Metadata() map[string]flag.Flag {
 	return map[string]flag.Flag{
 		"": {
-			Usage:   usage,
-			Version: version,
-			Desc:    desc,
+			Usage:   "Go is a tool for managing Go source code.",
+			Arglist: "command [argument]",
 		},
-		"--gz": {
-			Desc: "use gzip format",
+		"build": {
+			Arglist: "[-o output] [-i] [build flags] [packages]",
+			Desc: `
+		Build compiles the packages named by the import paths,
+		along with their dependencies, but it does not install the results.
+		...
+		The build flags are shared by the build, clean, get, install, list, run,
+		and test commands:
+			`,
 		},
 	}
 }
 
 func main() {
-	var tar Tar
+	var g GoCmd
 
-	flag.NewFlagSet(flag.Flag{}).ParseStruct(&tar, os.Args...)
-	// flag.NewFlagSet(flag.Flag{}).ParseStruct(&tar, "tar", "-zcf", "a.tgz", "a.go", "b.go")
-	fmt.Println(tar.GZ)
-	fmt.Println(tar.Create)
-	fmt.Println(tar.File)
-	fmt.Println(tar.SourceFiles)
+	set := flag.NewFlagSet(flag.Flag{})
+	set.ParseStruct(&g, os.Args...)
 
-	// Output:
-	// true
-	// true
-	// a.tgz
-	// [a.go b.go]
+	if g.Build.Enable {
+		if len(g.Build.Packages) == 0 {
+			fmt.Fprintln(os.Stderr, "Error: you should at least specify one package")
+			fmt.Println("")
+			build, _ := set.FindSubset("build")
+			build.Help(false) // display usage information for the "go build" command only
+		} else {
+			fmt.Println("Going to build with the following parameters:")
+			fmt.Println(g.Build)
+		}
+	} else if g.Clean.Enable {
+		fmt.Println("Going to clean with the following parameters:")
+		fmt.Println(g.Clean)
+	} else {
+		set.Help(false) // display usage information, with list of supported commands
+	}
 }
