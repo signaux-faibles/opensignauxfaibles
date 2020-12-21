e4bf947e5ceea9a00e8b0a11fc55c9c0ee1c58d3
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-12-21 15:18:41 +0100

    faire échouer le test (provisoirement)

--- lib/engine/data.go
+++ lib/engine/data.go
@@ -50,8 +50,9 @@ func loadJSFunctions(directoryNames ...string) (map[string]bson.JavaScript, erro
 // PurgeNotCompacted permet de supprimer les objets non encore compactés
 // c'est à dire, vider la collection ImportedData
 func PurgeNotCompacted() error {
-	_, err := Db.DB.C("ImportedData").RemoveAll(nil)
-	return err
+	return nil
+	// _, err := Db.DB.C("ImportedData").RemoveAll(nil) // TODO: supprimer la collection
+	// return err
 }
 
 // PruneEntities permet de compter puis supprimer les entités de RawData
