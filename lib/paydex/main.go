e39f4ba5c247c58a881ae3d5feedf11272c357b9
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-12-21 17:43:33 +0100

    introduction et test de parsePaydexLine()

--- lib/paydex/main.go
+++ lib/paydex/main.go
@@ -1,1 +1,21 @@
 package paydex
+
+import (
+	"time"
+)
+
+// Paydex décrit le format de chaque entrée de donnée résultant du parsing.
+type Paydex struct {
+	Siren   string    `json:"siren" bson:"siren"`
+	Periode time.Time `json:"periode" bson:"periode"`
+	Jours   int       `json:"paydex_jours" bson:"paydex_jours"`
+}
+
+func parsePaydexLine(row []string) Paydex {
+	return Paydex{
+		Siren:   "000000001",
+		Periode: time.Date(2018, 12, 01, 00, 00, 00, 0, time.UTC),
+		Jours:   2,
+	}
+}
+
--- lib/paydex/main_test.go
+++ lib/paydex/main_test.go
@@ -7,6 +7,9 @@ import (
 	"log"
 	"os"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 // Le Paydex – index de paiement – mesure statistiquement la régularité de
@@ -30,4 +33,11 @@ func TestPaydex(t *testing.T) {
 	log.Println(row)
 	row, err = reader.Read()
 	log.Println(row)
+	expected := Paydex{
+		Siren:   "000000001",
+		Periode: time.Date(2018, 12, 01, 00, 00, 00, 0, time.UTC),
+		Jours:   2,
+	}
+	actual := parsePaydexLine(row)
+	assert.Equal(t, expected, actual)
 }
