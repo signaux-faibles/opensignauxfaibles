cb684825cde17372798ae93f6b46440828f2b08b
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-12-21 18:16:28 +0100

    test avec golden file

--- lib/paydex/main.go
+++ lib/paydex/main.go
@@ -1,9 +1,16 @@
 package paydex
 
 import (
+	"bufio"
+	"encoding/csv"
+	"io"
 	"log"
+	"os"
 	"strconv"
 	"time"
+
+	"github.com/signaux-faibles/opensignauxfaibles/lib/base"
+	"github.com/signaux-faibles/opensignauxfaibles/lib/marshal"
 )
 
 // Paydex décrit le format de chaque entrée de donnée résultant du parsing.
@@ -11,6 +18,73 @@ type Paydex struct {
 	Siren   string    `json:"siren" bson:"siren"`
 	Periode time.Time `json:"periode" bson:"periode"`
 	Jours   int       `json:"paydex_jours" bson:"paydex_jours"`
+}
+
+// Key _id de l'objet
+func (paydex Paydex) Key() string {
+	return paydex.Siren
+}
+
+// Scope de l'objet
+func (paydex Paydex) Scope() string {
+	return "entreprise"
+}
+
+// Type de l'objet
+func (paydex Paydex) Type() string {
+	return "paydex"
+}
+
+// ParserPaydex fournit une instance utilisable par ParseFilesFromBatch.
+var ParserPaydex = &paydexParser{}
+
+type paydexParser struct {
+	file   *os.File
+	reader *csv.Reader
+}
+
+func (parser *paydexParser) GetFileType() string {
+	return "paydex"
+}
+
+func (parser *paydexParser) Init(cache *marshal.Cache, batch *base.AdminBatch) error {
+	return nil
+}
+
+func (parser *paydexParser) Close() error {
+	return parser.file.Close()
+}
+
+func (parser *paydexParser) Open(filePath string) (err error) {
+	parser.file, parser.reader, err = openPaydexFile(filePath)
+	return err
+}
+
+func openPaydexFile(filePath string) (*os.File, *csv.Reader, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return file, nil, err
+	}
+	reader := csv.NewReader(bufio.NewReader(file))
+	reader.Comma = ';'
+	return file, reader, err
+}
+
+func (parser *paydexParser) ParseLines(parsedLineChan chan marshal.ParsedLineResult) {
+	parser.reader.Read() // parse header
+	for {
+		parsedLine := marshal.ParsedLineResult{}
+		row, err := parser.reader.Read()
+		if err == io.EOF {
+			close(parsedLineChan)
+			break
+		} else if err != nil {
+			parsedLine.AddRegularError(err)
+		} else {
+			parsedLine.AddTuple(parsePaydexLine(row))
+		}
+		parsedLineChan <- parsedLine
+	}
 }
 
 func parsePaydexLine(row []string) Paydex {
@@ -31,8 +105,5 @@ func parsePaydexLine(row []string) Paydex {
 
 func beginningOfMonth(date time.Time) time.Time {
 	return date.AddDate(0, 0, -date.Day()+1)
-}
-
-	}
 }
 
--- lib/paydex/main_test.go
+++ lib/paydex/main_test.go
@@ -1,14 +1,12 @@
 package paydex
 
 import (
-	"bufio"
-	"encoding/csv"
 	"flag"
-	"log"
-	"os"
+	"path/filepath"
 	"testing"
 	"time"
 
+	"github.com/signaux-faibles/opensignauxfaibles/lib/marshal"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -21,20 +19,22 @@ import (
 var update = flag.Bool("update", false, "Update the expected test values in golden file")
 
 func TestPaydex(t *testing.T) {
-	t.Run("can read an actual file", func(t *testing.T) {
-		filePath := "../../../paydex/E_202011095813_Retro-Paydex_20201207.csv" // => SIREN;NB_JOURS;NB_JOURS_LIB;DATE_VALEUR
-		// filePath := "../../../paydex/E_202011095813_Identite_20201207.csv" // => R�f�rence Client;Siren;Siret;Raison sociale 1;Raison sociale 2;Enseigne;Sigle;Compl�ment d'adresse;Adresse;Distribution sp�ciale;Code postal et bureau distributeur;Pays;Code postal;Ville;Qualit� Etablissement;Code type d'�tablissement;Libell� type d'�tablissement;Etat d'activit� �tablissement;Etat d'activit� entreprise;Etat de proc�dure collective;Diffusible;
-		file, err := os.Open(filePath)
-		if err != nil {
-			t.Error(err)
-		}
-		reader := csv.NewReader(bufio.NewReader(file))
-		reader.Comma = ';'
-		row, err := reader.Read()
-		log.Println(row)
-		row, err = reader.Read()
-		log.Println(row)
-	})
+	/*
+		t.Run("can read an actual file", func(t *testing.T) {
+			filePath := "../../../paydex/E_202011095813_Retro-Paydex_20201207.csv" // => SIREN;NB_JOURS;NB_JOURS_LIB;DATE_VALEUR
+			// filePath := "../../../paydex/E_202011095813_Identite_20201207.csv" // => R�f�rence Client;Siren;Siret;Raison sociale 1;Raison sociale 2;Enseigne;Sigle;Compl�ment d'adresse;Adresse;Distribution sp�ciale;Code postal et bureau distributeur;Pays;Code postal;Ville;Qualit� Etablissement;Code type d'�tablissement;Libell� type d'�tablissement;Etat d'activit� �tablissement;Etat d'activit� entreprise;Etat de proc�dure collective;Diffusible;
+			file, err := os.Open(filePath)
+			if err != nil {
+				t.Error(err)
+			}
+			reader := csv.NewReader(bufio.NewReader(file))
+			reader.Comma = ';'
+			row, err := reader.Read()
+			log.Println(row)
+			row, err = reader.Read()
+			log.Println(row)
+		})
+	*/
 
 	t.Run("can parse a line", func(t *testing.T) {
 		row := []string{"000000001", "2", "2 jours", "15/12/2018"}
@@ -46,4 +46,11 @@ func TestPaydex(t *testing.T) {
 		actual := parsePaydexLine(row)
 		assert.Equal(t, expected, actual)
 	})
+
+	t.Run("generate the right tuples and events from test file", func(t *testing.T) {
+		var golden = filepath.Join("testData", "expectedPaydex.json")
+		var testData = filepath.Join("testData", "paydexTestData.csv")
+		marshal.TestParserOutput(t, ParserPaydex, marshal.NewCache(), testData, golden, *update)
+	})
+
 }
