0bccd0c856b614abe9614f5215675750338379fc
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-12-23 15:35:48 +0100

    ajout d'un test pour éviter regressions de Sirene

--- lib/sirene/main_test.go
+++ lib/sirene/main_test.go
@@ -3,9 +3,12 @@ package sirene
 import (
 	"flag"
 	"path/filepath"
+	"regexp"
+	"strings"
 	"testing"
 
 	"github.com/signaux-faibles/opensignauxfaibles/lib/marshal"
+	"github.com/stretchr/testify/assert"
 )
 
 var update = flag.Bool("update", false, "Update the expected test values in golden file")
@@ -15,4 +18,15 @@ var testData = filepath.Join("testData", "sireneTestData.csv")
 
 func TestSirene(t *testing.T) {
 	marshal.TestParserOutput(t, Parser, marshal.NewCache(), testData, golden, *update)
+
+	t.Run("should fail if a required column is missing", func(t *testing.T) {
+		csvData := strings.Join([]string{"siren"}, "\n") // many columns are missing
+		csvFile := marshal.CreateTempFileWithContent(t, []byte(csvData))
+		output := marshal.RunParser(Parser, marshal.NewCache(), csvFile.Name())
+		assert.Equal(t, []marshal.Tuple(nil), output.Tuples, "should return no tuples")
+		assert.Equal(t, 1, len(output.Events), "should return a parsing report")
+		reportData, _ := output.Events[0].ParseReport()
+		assert.Equal(t, true, reportData["isFatal"], "should report a fatal error")
+		assert.Regexp(t, regexp.MustCompile("Colonne [^ ]+ non trouvée"), reportData["headFatal"])
+	})
 }
