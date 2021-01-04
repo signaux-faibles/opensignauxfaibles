2bcaedab21f6e14796e6a4ba382b245143683167
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-01-04 14:06:58 +0100

    🧹 Move the invocation into the parens that contain the function. https://app.codacy.com/gh/signaux-faibles/opensignauxfaibles/file/51764969568/issues/source?bid=21654748&fileBranchId=21654748#l40

--- js/reduce.algo2/map_tests.ts
+++ js/reduce.algo2/map_tests.ts
@@ -9,7 +9,7 @@ import { objects as testData } from "../test/data/objects"
 import { naf as nafValues } from "../test/data/naf"
 import { runMongoMap, indexMapResultsByKey } from "../test/helpers/mongodb"
 import { setGlobals } from "../test/helpers/setGlobals"
-import { Scope, EntréeDelai } from "../RawDataTypes"
+import { Scope } from "../RawDataTypes"
 
 // initialisation des paramètres globaux de reduce.algo2
 function initGlobalParams(dateDebut: Date, dateFin: Date) {
@@ -34,55 +34,48 @@ test("map() retourne les même données que d'habitude", (t) => {
 
 test("delai_deviation_remboursement est calculé à partir d'un débit et d'une demande de délai de règlement de cotisations sociales", (t: ExecutionContext) => {
   const DAY_IN_MS = 24 * 60 * 60 * 1000
+  const siret = "12345678901234"
   const dateDebut = new Date("2018-01-01")
   const dateFin = new Date("2018-02-01")
   initGlobalParams(dateDebut, dateFin)
-  const input = (function makeInput(
-    dateDebut: Date,
-    dateFin: Date,
-    delaiOverrides?: Partial<EntréeDelai>
-  ) {
-    const siret = "12345678901234"
-    const duréeDelai = 60 // en jours
-    return {
-      _id: siret,
-      value: {
-        key: siret,
-        scope: "etablissement" as Scope,
-        batch: {
-          "1905": {
-            cotisation: {},
-            debit: {
-              hashDette: {
-                periode: {
-                  start: dateDebut,
-                  end: dateFin,
-                },
-                numero_ecart_negatif: 1,
-                numero_historique: 2,
-                numero_compte: "",
-                date_traitement: dateDebut,
-                debit_suivant: "",
-                part_ouvriere: 60,
-                part_patronale: 0,
+  const duréeDelai = 60 // en jours
+  const input = {
+    _id: siret,
+    value: {
+      key: siret,
+      scope: "etablissement" as Scope,
+      batch: {
+        "1905": {
+          cotisation: {},
+          debit: {
+            hashDette: {
+              periode: {
+                start: dateDebut,
+                end: dateFin,
               },
+              numero_ecart_negatif: 1,
+              numero_historique: 2,
+              numero_compte: "",
+              date_traitement: dateDebut,
+              debit_suivant: "",
+              part_ouvriere: 60,
+              part_patronale: 0,
             },
-            delai: {
-              hashDelai: {
-                date_creation: dateDebut,
-                date_echeance: new Date(
-                  dateDebut.getTime() + duréeDelai * DAY_IN_MS
-                ),
-                duree_delai: duréeDelai,
-                montant_echeancier: 100,
-                ...delaiOverrides,
-              },
+          },
+          delai: {
+            hashDelai: {
+              date_creation: dateDebut,
+              date_echeance: new Date(
+                dateDebut.getTime() + duréeDelai * DAY_IN_MS
+              ),
+              duree_delai: duréeDelai,
+              montant_echeancier: 100,
             },
           },
         },
       },
-    }
-  })(dateDebut, dateFin)
+    },
+  }
 
   const [res] = runMongoMap<EntréeMap, CléSortieMap, SortieMap>(map, [input])
   const finalCompanyData = Object.values(res?.value ?? {})[0]
