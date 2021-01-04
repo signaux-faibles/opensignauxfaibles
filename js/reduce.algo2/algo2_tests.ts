a7a623f7bb50e91fb4894f4c2ad1693256038c82
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-01-04 17:59:47 +0100

    test d'intégration avec snapshot: `algo2.map() retourne les propriétés d'entreprise attendues par l'algo d'apprentissage`

--- js/reduce.algo2/algo2_tests.ts
+++ js/reduce.algo2/algo2_tests.ts
@@ -1,6 +1,10 @@
-// Converted to TS from _test.js
+// Ces tests visent à couvrir toutes les fonctions invoquées par map(),
+// reduce() et finalize(), en leur fournissant un jeu de données minimal
+// mais incluant tous les types d'entrée inclus dans RawData.
+//
+// => Penser à ajouter les nouveaux types dans rawEtabData et rawEntrData.
 
-import test, { ExecutionContext } from "ava"
+import { ExecutionContext, serial as test } from "ava"
 import { map, EntréeMap, CléSortieMap, SortieMap } from "./map"
 import { reduce } from "./reduce"
 import { finalize, Clé } from "./finalize"
@@ -10,7 +14,7 @@ import { naf as nafValues } from "../test/data/naf"
 import { reducer, invertedReducer } from "../test/helpers/reducers"
 import { runMongoMap, indexMapResultsByKey } from "../test/helpers/mongodb"
 import { setGlobals } from "../test/helpers/setGlobals"
-import { Scope, EntréeDelai } from "../RawDataTypes"
+import { EntréeBdf, EntréeDelai, EntrepriseBatchProps } from "../RawDataTypes"
 
 const DAY_IN_MS = 24 * 60 * 60 * 1000
 
@@ -30,14 +34,14 @@ const makeInput = (
   dateDebut: Date,
   dateFin: Date,
   delaiOverrides?: Partial<EntréeDelai>
-) => {
+): EntréeMap => {
   const siret = "12345678901234"
   const duréeDelai = 60 // en jours
   return {
     _id: siret,
     value: {
       key: siret,
-      scope: "etablissement" as Scope,
+      scope: "etablissement",
       batch: {
         "1905": {
           cotisation: {},
@@ -112,3 +116,48 @@ test("delai_deviation_remboursement est calculé à partir d'un débit et d'une 
   t.is(typeof finalCompanyData?.delai_deviation_remboursement, "number")
   t.is(finalCompanyData?.delai_deviation_remboursement, -0.4)
 })
+
+test(`algo2.map() retourne les propriétés d'entreprise attendues par l'algo d'apprentissage`, (t: ExecutionContext) => {
+  const siren = "012345678"
+  const batchKey = "1910"
+  const dateDébut = new Date("2015-12-01T00:00:00.000+0000")
+  const dateFin = new Date("2016-01-01T00:00:00.000+0000")
+  const serie_periode = [dateDébut, dateFin]
+  setGlobals({
+    actual_batch: batchKey,
+    serie_periode,
+    includes: { all: true },
+  })
+  // Cet objet sera a compléter, au fur et à mesure qu'on ajoutera des props dans EntrepriseBatchProps
+  const rawEntrData: EntrepriseBatchProps = {
+    reporder: {},
+    paydex: {
+      decembre: { date_valeur: new Date("2015-12-15T00:00Z"), nb_jours: 1 },
+      janvier: { date_valeur: new Date("2016-01-15T00:00Z"), nb_jours: 2 },
+    },
+  }
+  // Note: l'entrée bdf est nécéssaire pour map() émette les données
+  const entréeBdf = {
+    arrete_bilan_bdf: new Date(
+      dateDébut.getTime() - 7 * 31 * 24 * 60 * 60 * 1000 // 7 mois avant date_debut
+    ),
+  } as EntréeBdf
+  const mapResults = runMongoMap<EntréeMap, CléSortieMap, SortieMap>(map, [
+    {
+      _id: siren,
+      value: {
+        scope: "entreprise",
+        key: siren,
+        batch: {
+          [batchKey]: {
+            ...rawEntrData,
+            bdf: { entréeBdf },
+          },
+        },
+      },
+    },
+  ])
+  t.snapshot(mapResults)
+})
+
+// TODO: écrire un test équivalent pour les établissements
