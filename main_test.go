c2419feae2c27fc920d2556e6d9ed9f7fd772a23
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-01-05 17:15:29 +0100

    lancement de mongodb sur le port 27016 (comme les tests de bout en bout)

--- main_test.go
+++ main_test.go
@@ -13,7 +13,7 @@ import (
 const (
 	mongoImage     = "mongo:4.2@sha256:1c2243a5e21884ffa532ca9d20c221b170d7b40774c235619f98e2f6eaec520a"
 	mongoContainer = "dockertestmongodb"
-	mongoPort      = 27017 // TODO: switch to 27016
+	mongoPort      = 27016
 	mongoDatabase  = "signauxfaibles"
 )
 
@@ -37,7 +37,8 @@ func TestMain(t *testing.T) {
 
 func startMongoContainer(t *testing.T) {
 	t.Log("Starting MongoDB in Docker container...")
-	err := exec.Command("docker", "run", "--rm", "-d", "-p", "27017:27017", "--name", mongoContainer, mongoImage).Run()
+	portMapping := fmt.Sprintf("%v:27017", mongoPort)
+	err := exec.Command("docker", "run", "--rm", "-d", "-p", portMapping, "--name", mongoContainer, mongoImage).Run()
 	if err != nil {
 		t.Fatalf("docker run: %v", err)
 	}
