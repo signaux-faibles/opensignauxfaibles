a9224660d952eff8095e773e92c5d0d036893409
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-01-11 15:00:53 +0100

    correction des types de add()

--- js/reduce.algo2/add.ts
+++ js/reduce.algo2/add.ts
@@ -1,6 +1,9 @@
 import { ParPériode } from "../RawDataTypes"
 
-export function add<T>(obj: ParPériode<Partial<T>>, output: ParPériode<T>) {
+export function add<T>(
+  obj: ParPériode<T>,
+  output: ParPériode<Partial<T>>
+): void {
   "use strict"
   Object.keys(output).forEach(function (periode) {
     if (periode in obj) {
