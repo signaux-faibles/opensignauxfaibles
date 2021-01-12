918adef75205853e3f7b29c85a60f1feed4c7ad6
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-01-12 19:25:03 +0100

    usage de ValidateAndIndexColumnsFromColTags() dans urssaf/delai

--- lib/urssaf/delai.go
+++ lib/urssaf/delai.go
@@ -18,17 +18,17 @@ import (
 // Delai tuple fichier ursaff
 type Delai struct {
 	key               string    `hash:"-"`
-	NumeroCompte      string    `json:"numero_compte" bson:"numero_compte"`
-	NumeroContentieux string    `json:"numero_contentieux" bson:"numero_contentieux"`
-	DateCreation      time.Time `json:"date_creation" bson:"date_creation"`
-	DateEcheance      time.Time `json:"date_echeance" bson:"date_echeance"`
-	DureeDelai        int       `json:"duree_delai" bson:"duree_delai"`
-	Denomination      string    `json:"denomination" bson:"denomination"`
-	Indic6m           string    `json:"indic_6m" bson:"indic_6m"`
-	AnneeCreation     int       `json:"annee_creation" bson:"annee_creation"`
-	MontantEcheancier float64   `json:"montant_echeancier" bson:"montant_echeancier"`
-	Stade             string    `json:"stade" bson:"stade"`
-	Action            string    `json:"action" bson:"action"`
+	NumeroCompte      string    `col:"Numero_compte_externe"       json:"numero_compte"      bson:"numero_compte"`
+	NumeroContentieux string    `col:"Numero_structure"            json:"numero_contentieux" bson:"numero_contentieux"`
+	DateCreation      time.Time `col:"Date_creation"               json:"date_creation"      bson:"date_creation"`
+	DateEcheance      time.Time `col:"Date_echeance"               json:"date_echeance"      bson:"date_echeance"`
+	DureeDelai        int       `col:"Duree_delai"                 json:"duree_delai"        bson:"duree_delai"`
+	Denomination      string    `col:"Denomination_premiere_ligne" json:"denomination"       bson:"denomination"`
+	Indic6m           string    `col:"Indic_6M"                    json:"indic_6m"           bson:"indic_6m"`
+	AnneeCreation     int       `col:"Annee_creation"              json:"annee_creation"     bson:"annee_creation"`
+	MontantEcheancier float64   `col:"Montant_global_echeancier"   json:"montant_echeancier" bson:"montant_echeancier"`
+	Stade             string    `col:"Code_externe_stade"          json:"stade"              bson:"stade"`
+	Action            string    `col:"Code_externe_action"         json:"action"             bson:"action"`
 }
 
 // Key _id de l'objet
@@ -53,6 +53,7 @@ type delaiParser struct {
 	file    *os.File
 	reader  *csv.Reader
 	comptes marshal.Comptes
+	idx     marshal.ColMapping
 }
 
 func (parser *delaiParser) GetFileType() string {
@@ -75,26 +76,15 @@ func (parser *delaiParser) Open(filePath string) (err error) {
 	}
 	parser.reader = csv.NewReader(bufio.NewReader(parser.file))
 	parser.reader.Comma = ';'
-	_, err = parser.reader.Read() // Sauter l'en-tête
+	header, err := parser.reader.Read()
+	if err == nil {
+		parser.idx, err = marshal.ValidateAndIndexColumnsFromColTags(header, Delai{})
+	}
 	return err
 }
 
-var idxDelai = colMapping{
-	"NumeroCompte":      2,
-	"NumeroContentieux": 3,
-	"DateCreation":      4,
-	"DateEcheance":      5,
-	"DureeDelai":        6,
-	"Denomination":      7,
-	"Indic6m":           8,
-	"AnneeCreation":     9,
-	"MontantEcheancier": 10,
-	"Stade":             11,
-	"Action":            12,
-}
-
 func (parser *delaiParser) ParseLines(parsedLineChan chan marshal.ParsedLineResult) {
-	idx := idxDelai
+	idx := parser.idx
 	for {
 		parsedLine := marshal.ParsedLineResult{}
 		row, err := parser.reader.Read()
@@ -104,10 +94,10 @@ func (parser *delaiParser) ParseLines(parsedLineChan chan marshal.ParsedLineResu
 		} else if err != nil {
 			parsedLine.AddRegularError(err)
 		} else {
-			date, err := time.Parse("02/01/2006", row[idx["DateCreation"]])
+			date, err := time.Parse("02/01/2006", row[idx["Date_creation"]])
 			if err != nil {
 				parsedLine.AddRegularError(err)
-			} else if siret, err := marshal.GetSiretFromComptesMapping(row[idx["NumeroCompte"]], &date, parser.comptes); err == nil {
+			} else if siret, err := marshal.GetSiretFromComptesMapping(row[idx["Numero_compte_externe"]], &date, parser.comptes); err == nil {
 				parseDelaiLine(row, idx, siret, &parsedLine)
 				if len(parsedLine.Errors) > 0 {
 					parsedLine.Tuples = []marshal.Tuple{}
@@ -120,24 +110,24 @@ func (parser *delaiParser) ParseLines(parsedLineChan chan marshal.ParsedLineResu
 	}
 }
 
-func parseDelaiLine(row []string, idx colMapping, siret string, parsedLine *marshal.ParsedLineResult) {
+func parseDelaiLine(row []string, idx marshal.ColMapping, siret string, parsedLine *marshal.ParsedLineResult) {
 	var err error
 	delai := Delai{}
 	delai.key = siret
-	delai.NumeroCompte = row[idx["NumeroCompte"]]
-	delai.NumeroContentieux = row[idx["NumeroContentieux"]]
-	delai.DateCreation, err = time.Parse("02/01/2006", row[idx["DateCreation"]])
+	delai.NumeroCompte = row[idx["Numero_compte_externe"]]
+	delai.NumeroContentieux = row[idx["Numero_structure"]]
+	delai.DateCreation, err = time.Parse("02/01/2006", row[idx["Date_creation"]])
 	parsedLine.AddRegularError(err)
-	delai.DateEcheance, err = time.Parse("02/01/2006", row[idx["DateEcheance"]])
+	delai.DateEcheance, err = time.Parse("02/01/2006", row[idx["Date_echeance"]])
 	parsedLine.AddRegularError(err)
-	delai.DureeDelai, err = strconv.Atoi(row[idx["DureeDelai"]])
-	delai.Denomination = row[idx["Denomination"]]
-	delai.Indic6m = row[idx["Indic6m"]]
-	delai.AnneeCreation, err = strconv.Atoi(row[idx["AnneeCreation"]])
+	delai.DureeDelai, err = strconv.Atoi(row[idx["Duree_delai"]])
+	delai.Denomination = row[idx["Denomination_premiere_ligne"]]
+	delai.Indic6m = row[idx["Indic_6M"]]
+	delai.AnneeCreation, err = strconv.Atoi(row[idx["Annee_creation"]])
 	parsedLine.AddRegularError(err)
-	delai.MontantEcheancier, err = strconv.ParseFloat(strings.Replace(row[idx["MontantEcheancier"]], ",", ".", -1), 64)
+	delai.MontantEcheancier, err = strconv.ParseFloat(strings.Replace(row[idx["Montant_global_echeancier"]], ",", ".", -1), 64)
 	parsedLine.AddRegularError(err)
-	delai.Stade = row[idx["Stade"]]
-	delai.Action = row[idx["Action"]]
+	delai.Stade = row[idx["Code_externe_stade"]]
+	delai.Action = row[idx["Code_externe_action"]]
 	parsedLine.AddTuple(delai)
 }
