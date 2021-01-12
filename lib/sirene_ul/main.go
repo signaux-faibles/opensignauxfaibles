cb3eb5352e0bcf0cd5fec287582df942fdef9d11
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-01-12 18:25:58 +0100

    usage de ValidateAndIndexColumnsFromColTags() dans sirene_ul

--- lib/sirene_ul/main.go
+++ lib/sirene_ul/main.go
@@ -3,10 +3,8 @@ package sireneul
 import (
 	//"bufio"
 	"encoding/csv"
-	"errors"
 	"io"
 	"os"
-	"strings"
 	"time"
 
 	"github.com/signaux-faibles/opensignauxfaibles/lib/base"
@@ -15,63 +13,18 @@ import (
 
 // SireneUL informations sur les entreprises
 type SireneUL struct {
-	Siren               string     `json:"siren,omitempty"         bson:"siren,omitempty"`
-	Nic                 string     `json:"nic,omitempty"           bson:"nic,omitempty"`
-	RaisonSociale       string     `json:"raison_sociale"          bson:"raison_sociale"`
-	Prenom1UniteLegale  string     `json:"prenom1_unite_legale,omitempty"      bson:"prenom1_unite_legale,omitempty"`
-	Prenom2UniteLegale  string     `json:"prenom2_unite_legale,omitempty"      bson:"prenom2_unite_legale,omitempty"`
-	Prenom3UniteLegale  string     `json:"prenom3_unite_legale,omitempty"      bson:"prenom3_unite_legale,omitempty"`
-	Prenom4UniteLegale  string     `json:"prenom4_unite_legale,omitempty"      bson:"prenom4_unite_legale,omitempty"`
-	NomUniteLegale      string     `json:"nom_unite_legale,omitempty"          bson:"nom_unite_legale,omitempty"`
-	NomUsageUniteLegale string     `json:"nom_usage_unite_legale,omitempty"     bson:"nom_usage_unite_legale,omitempty"`
-	CodeStatutJuridique string     `json:"statut_juridique"        bson:"statut_juridique"`
-	Creation            *time.Time `json:"date_creation,omitempty" bson:"date_creation,omitempty"`
+	Siren               string     `col:"siren"                         json:"siren,omitempty"                  bson:"siren,omitempty"`
+	Nic                 string     `                                    json:"nic,omitempty"                    bson:"nic,omitempty"`
+	RaisonSociale       string     `col:"denominationUniteLegale"       json:"raison_sociale"                   bson:"raison_sociale"`
+	Prenom1UniteLegale  string     `col:"prenom1UniteLegale"            json:"prenom1_unite_legale,omitempty"   bson:"prenom1_unite_legale,omitempty"`
+	Prenom2UniteLegale  string     `col:"prenom2UniteLegale"            json:"prenom2_unite_legale,omitempty"   bson:"prenom2_unite_legale,omitempty"`
+	Prenom3UniteLegale  string     `col:"prenom3UniteLegale"            json:"prenom3_unite_legale,omitempty"   bson:"prenom3_unite_legale,omitempty"`
+	Prenom4UniteLegale  string     `col:"prenom4UniteLegale"            json:"prenom4_unite_legale,omitempty"   bson:"prenom4_unite_legale,omitempty"`
+	NomUniteLegale      string     `col:"nomUniteLegale"                json:"nom_unite_legale,omitempty"       bson:"nom_unite_legale,omitempty"`
+	NomUsageUniteLegale string     `col:"nomUsageUniteLegale"           json:"nom_usage_unite_legale,omitempty" bson:"nom_usage_unite_legale,omitempty"`
+	CodeStatutJuridique string     `col:"categorieJuridiqueUniteLegale" json:"statut_juridique"                 bson:"statut_juridique"`
+	Creation            *time.Time `col:"dateCreationUniteLegale"       json:"date_creation,omitempty"          bson:"date_creation,omitempty"`
 }
-
-sireneul.Siren = row[0]
-sireneul.RaisonSociale = row[23]
-sireneul.Prenom1UniteLegale = row[6]
-sireneul.Prenom2UniteLegale = row[7]
-sireneul.Prenom3UniteLegale = row[8]
-sireneul.Prenom4UniteLegale = row[9]
-sireneul.NomUniteLegale = row[21]
-sireneul.NomUsageUniteLegale = row[22]
-sireneul.CodeStatutJuridique = row[27]
-creation, err := time.Parse("2006-01-02", row[3]) // note: cette date n'est pas toujours présente, et on ne souhaite pas être rapporter d'erreur en cas d'absence
-
-// 0: siren
-// 1: statutDiffusionUniteLegale
-// 2: unitePurgeeUniteLegale
-// 3: dateCreationUniteLegale
-// 4: sigleUniteLegale
-// 5: sexeUniteLegale
-// 6: prenom1UniteLegale
-// 7: prenom2UniteLegale
-// 8: prenom3UniteLegale
-// 9: prenom4UniteLegale
-// 10: prenomUsuelUniteLegale
-// 11: pseudonymeUniteLegale
-// 12: identifiantAssociationUniteLegale
-// 13: trancheEffectifsUniteLegale
-// 14: anneeEffectifsUniteLegale
-// 15: dateDernierTraitementUniteLegale
-// 16: nombrePeriodesUniteLegale
-// 17: categorieEntreprise
-// 18: anneeCategorieEntreprise
-// 19: dateDebut
-// 20: etatAdministratifUniteLegale
-// 21: nomUniteLegale
-// 22: nomUsageUniteLegale
-// 23: denominationUniteLegale
-// 24: denominationUsuelle1UniteLegale
-// 25: denominationUsuelle2UniteLegale
-// 26: denominationUsuelle3UniteLegale
-// 27: categorieJuridiqueUniteLegale
-// 28: activitePrincipaleUniteLegale
-// 29: nomenclatureActivitePrincipaleUniteLegale
-// 30: nicSiegeUniteLegale
-// 31: economieSocialeSolidaireUniteLegale
-// 32: caractereEmployeurUniteLegale
 
 // Key id de l'objet
 func (sirene_ul SireneUL) Key() string {
@@ -118,13 +71,12 @@ func (parser *sireneUlParser) Open(filePath string) (err error) {
 	parser.reader.LazyQuotes = true
 
 	// parse header
-	row, err := parser.reader.Read()
+	header, err := parser.reader.Read()
 	if err != nil {
 		return err // may be io.EOF
-	} else if strings.Join(row, ",") != expectedHeader {
-		return errors.New("sirene_ul header does not match the parser's expectations")
 	}
 
+	_, err = marshal.ValidateAndIndexColumnsFromColTags(header, SireneUL{})
 	return err
 }
 
@@ -145,6 +97,7 @@ func (parser *sireneUlParser) ParseLines(parsedLineChan chan marshal.ParsedLineR
 }
 
 func parseSireneUlLine(row []string, parsedLine *marshal.ParsedLineResult) {
+	// TODO: utiliser ColMapping
 	sireneul := SireneUL{}
 	sireneul.Siren = row[0]
 	sireneul.RaisonSociale = row[23]
--- lib/sirene_ul/main_test.go
+++ lib/sirene_ul/main_test.go
@@ -30,6 +30,6 @@ func TestSireneUlHeader(t *testing.T) {
 		csvRows := []string{"sirenXYZ,statutDiffusionUniteLegale,unitePurgeeUniteLegale,dateCreationUniteLegale,sigleUniteLegale,sexeUniteLegale,prenom1UniteLegale,prenom2UniteLegale,prenom3UniteLegale,prenom4UniteLegale,prenomUsuelUniteLegale,pseudonymeUniteLegale,identifiantAssociationUniteLegale,trancheEffectifsUniteLegale,anneeEffectifsUniteLegale,dateDernierTraitementUniteLegale,nombrePeriodesUniteLegale,categorieEntreprise,anneeCategorieEntreprise,dateDebut,etatAdministratifUniteLegale,nomUniteLegale,nomUsageUniteLegale,denominationUniteLegale,denominationUsuelle1UniteLegale,denominationUsuelle2UniteLegale,denominationUsuelle3UniteLegale,categorieJuridiqueUniteLegale,activitePrincipaleUniteLegale,nomenclatureActivitePrincipaleUniteLegale,nicSiegeUniteLegale,economieSocialeSolidaireUniteLegale,caractereEmployeurUniteLegale"}
 		output := marshal.RunParserInline(t, Parser, csvRows)
 		assert.Equal(t, []marshal.Tuple(nil), output.Tuples, "should return no tuples")
-		assert.Equal(t, "Fatal: sirene_ul header does not match the parser's expectations", marshal.GetFatalError(output))
+		assert.Equal(t, "Fatal: Colonne siren non trouvée. Abandon.", marshal.GetFatalError(output))
 	})
 }
