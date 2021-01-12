ecd03645c5a98fcbe576727da3caa97ff0d59f1d
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-01-12 10:06:50 +0100

    test: apconso doit détecter s'il manque des colonnes

--- lib/apconso/main_test.go
+++ lib/apconso/main_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/signaux-faibles/opensignauxfaibles/lib/marshal"
+	"github.com/stretchr/testify/assert"
 )
 
 var update = flag.Bool("update", false, "Update the expected test values in golden file")
@@ -14,4 +15,10 @@ func TestApconso(t *testing.T) {
 	var golden = filepath.Join("testData", "expectedApconso.json")
 	var testData = filepath.Join("testData", "apconsoTestData.csv")
 	marshal.TestParserOutput(t, Parser, marshal.NewCache(), testData, golden, *update)
+
+	t.Run("doit détecter s'il manque des colonnes", func(t *testing.T) {
+		output := marshal.RunParserInline(t, Parser, []string{"ID_DA,ETAB_SIRET,MOIS,HEURE,MONTANTS,EFFECTIFS"}) // typo: HEURE au lieu de HEURES
+		assert.Equal(t, []marshal.Tuple(nil), output.Tuples, "le parseur doit retourner aucun tuple")
+		assert.Contains(t, marshal.GetFatalError(output), "entête non conforme")
+	})
 }
