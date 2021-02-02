6c97f243ce491efa891be70dc87d207f18487bd0
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-02-02 16:16:12 +0100

    ajout de tests et TODO

--- validation/validation_test.go
+++ validation/validation_test.go
@@ -37,11 +37,35 @@ func TestDiffMaps(t *testing.T) {
 	})
 }
 
+func TestReflectPropsFromStruct(t *testing.T) {
+	t.Run("doit extraire le nom JSON et type d'un champ de struct Go", func(t *testing.T) {
+		type MyType struct {
+			MyField string `json:"myField"`
+		}
+		assert.Equal(t, map[string]propertySchema{"myField": {"string"}}, reflectPropsFromStruct(MyType{}))
+	})
+	t.Run("doit interpréter les types float64 et int comme number", func(t *testing.T) {
+		type MyType struct {
+			MyField1 int     `json:"f1"`
+			MyField2 float64 `json:"f2"`
+		}
+		assert.Equal(t, map[string]propertySchema{"f1": {"number"}, "f2": {"number"}}, reflectPropsFromStruct(MyType{}))
+	})
+	t.Run("doit reconnaitre le type des pointeurs", func(t *testing.T) {
+		type MyType struct {
+			MyField1 *int `json:"f1"`
+		}
+		assert.Equal(t, map[string]propertySchema{"f1": {"number"}}, reflectPropsFromStruct(MyType{}))
+	})
+}
+
 func TestTypeAlignment(t *testing.T) {
 
 	t.Run("chaque type décrit en JSON Schema doit correspondre à la structure retournée par le parseur correspondant", func(t *testing.T) {
 		typesToCompare := map[string]interface{}{
 			"delai": urssaf.Delai{},
+			// TODO: lister les fichiers JSON Schema du répertoire,
+			// pour s'assurer qu'ils sont couvert ici par un test d'alignement de type.
 		}
 		for jsonTypeName, structInstance := range typesToCompare {
 			t.Run(jsonTypeName, func(t *testing.T) {
