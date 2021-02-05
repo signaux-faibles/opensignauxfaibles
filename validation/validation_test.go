53ee1502bd88e196ec6821931eb65b2a98410d7b
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-02-05 17:36:32 +0100

    ajout schema Ellisphere => "property not found in JSON Schema: *,omitempty"

--- validation/validation_test.go
+++ validation/validation_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/signaux-faibles/opensignauxfaibles/lib/apconso"
 	"github.com/signaux-faibles/opensignauxfaibles/lib/apdemande"
 	"github.com/signaux-faibles/opensignauxfaibles/lib/bdf"
+	"github.com/signaux-faibles/opensignauxfaibles/lib/ellisphere"
 	"github.com/signaux-faibles/opensignauxfaibles/lib/paydex"
 	"github.com/signaux-faibles/opensignauxfaibles/lib/urssaf"
 	"github.com/stretchr/testify/assert"
@@ -80,6 +81,7 @@ func TestTypeAlignment(t *testing.T) {
 		"delai.schema.json":        {urssaf.Delai{}, []error{}},
 		"effectif.schema.json":     {urssaf.Effectif{}, []error{}},
 		"effectif_ent.schema.json": {urssaf.EffectifEnt{}, []error{}},
+		"ellisphere.schema.json":   {ellisphere.Ellisphere{}, []error{}},
 		"paydex.schema.json":       {paydex.Paydex{}, []error{}},
 		"procol.schema.json":       {urssaf.Procol{}, []error{}},
 		"bdf.schema.json": {bdf.BDF{}, []error{ // bdf.schema.json n'est pas encore complet => la vérification va retourner les erreurs suivantes:
