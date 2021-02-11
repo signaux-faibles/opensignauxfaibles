6c71d67a3caa66c83528de220b3769c71658ee56
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-02-11 11:09:41 +0100

    🔵 validate-json-schema.js

--- js/validate-json-schema.js
+++ js/validate-json-schema.js
@@ -1,6 +1,9 @@
-var Ajv = require("ajv").default
-var ajv = new Ajv({ strict: false })
-require("ajv-bsontype")(ajv)
+const Ajv = require("ajv").default // eslint-disable-line @typescript-eslint/no-var-requires
+const useMongoDbPlugin = require("ajv-bsontype") // eslint-disable-line @typescript-eslint/no-var-requires
+
+// Initialize JSON Schema validator with MongoDB extensions (e.g. "bsonType" property)
+const ajv = new Ajv({ strict: false })
+useMongoDbPlugin(ajv)
 
 const readStdin = () =>
   new Promise((resolve) => {
@@ -11,20 +14,22 @@ const readStdin = () =>
       .on("end", () => resolve(chunks.join("")))
   })
 
-;(async () => {
-  ;(await readStdin())
-    .split("\n")
-    .filter((line) => line.trim() != "")
-    .forEach((line) => {
-      console.log(line)
-      const { dataType, dataObject } = JSON.parse(line)
-      const schema = require(`../validation/${dataType}.schema.json`) // eslint-disable-line @typescript-eslint/no-var-requires
-      const valid = ajv.validate(schema, dataObject)
-      ajv.errors.forEach(({ message, params }) =>
-        console.log("- Validation error:", `"${message}"`, params)
-      )
-    })
-})().catch((err) => {
-  console.error(err)
-  process.exit(1)
-})
+readStdin()
+  .then((stdin) =>
+    stdin
+      .split("\n")
+      .filter((line) => line.trim() !== "")
+      .forEach((line) => {
+        console.log(line)
+        const { dataType, dataObject } = JSON.parse(line)
+        const schema = require(`../validation/${dataType}.schema.json`) // eslint-disable-line @typescript-eslint/no-var-requires
+        ajv.validate(schema, dataObject)
+        ajv.errors.forEach(({ message, params }) =>
+          console.log("- Validation error:", `"${message}"`, params)
+        )
+      })
+  )
+  .catch((err) => {
+    console.error(err)
+    process.exit(1)
+  })
