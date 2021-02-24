fcaa2479034bad0da55688c05a9694bfe407e2a1
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-02-24 14:46:59 +0100

    ajout d'un commentaire descriptif pour parPériode()

--- js/test/helpers/parPériode.ts
+++ js/test/helpers/parPériode.ts
@@ -1,3 +1,8 @@
+/**
+ * parPériode convertit un objet indexé par des dates exprimées sous la
+ * forme `YYYY-MM-DD` en objet indexé par le timestamp de ces Dates,
+ * par souci de compatibilité avec le type ParPériode.
+ */
 export const parPériode = <T extends Record<number, unknown>>(
   indexed: Record<string, T[keyof T]>
 ): T => {
