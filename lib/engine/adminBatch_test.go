590b232c0913b853adfe9c39718c40add77cb08c
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-03-31 16:03:36 +0200

    test: "CheckBatch devrait réussir à parser un fichier gzip"

--- lib/engine/adminBatch_test.go
+++ lib/engine/adminBatch_test.go
@@ -1,10 +1,15 @@
 package engine
 
 import (
+	"os"
+	"os/exec"
+	"path/filepath"
 	"testing"
 
 	"github.com/signaux-faibles/opensignauxfaibles/lib/base"
 	"github.com/signaux-faibles/opensignauxfaibles/lib/marshal"
+	"github.com/signaux-faibles/opensignauxfaibles/lib/urssaf"
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_IsBatchID(t *testing.T) {
@@ -73,4 +78,33 @@ func Test_ImportBatchWithUnreadableFilter(t *testing.T) {
 	if err == nil {
 		t.Error("ImportBatch devrait échouer en tentant d'ouvrir un fichier filtre illisible")
 	}
+}
+
+func Test_CheckBatch(t *testing.T) {
+
+	t.Run("CheckBatch devrait réussir à parser un fichier gzip", func(t *testing.T) {
+		// TODO: renommer en "CheckBatch devrait réussir à parser un fichier compressé spécifié avec le préfixe 'gzip:'"
+
+		// Compression du fichier de données
+		procolFilePath := filepath.Join("..", "urssaf", "testData", "procolTestData.csv")
+		compressedFilePath := filepath.Join("..", "urssaf", "testData", "procolTestData.csv.gz")
+		cmd := exec.Command("gzip", "--suffix", ".gz", "--keep", procolFilePath) // créée une version gzippée du fichier, TODO: utiliser une autre extension que .gz
+		err := cmd.Run()
+		assert.NoError(t, err)
+		cmd.Wait()
+		t.Cleanup(func() { os.Remove(compressedFilePath) })
+
+		// Exécution de CheckBatch sur un AdminBatch mentionnant un fichier compressé
+		batch := base.AdminBatch{
+			Files: base.BatchFiles{
+				"procol": {"../../lib/urssaf/testData/procolTestData.csv.gz"}, // TODO: utiliser extension alternative et prefixe "gzip:"
+			},
+		}
+		InitVoidEventQueue() // permettre à CheckBatch d'envoyer des messages au canal d'événements, sans stocker dans la db
+		reports, err := CheckBatch(batch, []marshal.Parser{urssaf.ParserProcol})
+		if assert.NoError(t, err) {
+			expectedReports := []string{"../../lib/urssaf/testData/procolTestData.csv.gz: intégration terminée, 3 lignes traitées, 0 erreurs fatales, 0 lignes rejetées, 0 lignes filtrées, 3 lignes valides"}
+			assert.Equal(t, expectedReports, reports)
+		}
+	})
 }
