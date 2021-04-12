316c133a00b4e653451be3e5f304fe4ee727ca63
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-04-12 12:05:20 +0200

    liste des globals extraits depuis chaque traitement

--- js/generate-javascript.sh
+++ js/generate-javascript.sh
@@ -40,3 +40,8 @@ function checkJS {
 checkJS "compact/*.js" "f,emit,$(getGlobals 'compact/*.ts')"
 checkJS "public/*.js" "f,emit,$(getGlobals 'public/*.ts')"
 checkJS "reduce.algo2/*.js" "f,print,emit,bsonsize,$(getGlobals 'reduce.algo2/*.ts')"
+
+# TODO: effacer ces lignes -- utilisées pour référence pendant le développement de PR #345
+# getGlobals 'compact/*.ts' # => batches,completeTypes,fromBatchKey,serie_periode
+# getGlobals 'public/*.ts' # => actual_batch,date_fin,serie_periode
+# getGlobals 'reduce.algo2/*.ts' # => actual_batch,date_fin,includes,naf,offset_effectif,serie_periode
