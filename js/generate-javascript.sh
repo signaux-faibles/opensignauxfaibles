6de564c96584a7170aab74689f89fcbf9ed658a4
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-04-12 14:04:12 +0200

    découverte: déclaration de variable globale trouvée dans common/*.ts

--- js/generate-javascript.sh
+++ js/generate-javascript.sh
@@ -45,3 +45,4 @@ checkJS "reduce.algo2/*.js" "f,print,emit,bsonsize,$(getGlobals 'reduce.algo2/*.
 # getGlobals 'compact/*.ts' # => batches,completeTypes,fromBatchKey,serie_periode
 # getGlobals 'public/*.ts' # => actual_batch,date_fin,serie_periode
 # getGlobals 'reduce.algo2/*.ts' # => actual_batch,date_fin,includes,naf,offset_effectif,serie_periode
+# getGlobals 'common/*.ts' => 
