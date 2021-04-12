026872081d1b455307f7cd31b8e363149e99aff1
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-04-12 11:50:19 +0200

    documentation des étapes de génération et vérification des traitements map-reduce (JavaScript)

--- js/generate-javascript.sh
+++ js/generate-javascript.sh
@@ -8,11 +8,12 @@ set -e # will stop the script if any command fails with a non-zero exit code
 # Run typescript transpiler, to generate .js files from .ts files.
 $(npm bin)/tsc --p "tsconfig-transpilation.json"
 
-# Clean-up JS functions, for mongodb compatibility.
+# Exclude JavaScript keywords that are not supported by MongoDB.
 perl -pi'' -e 's/^const .*$//g;' -e 's/^export //;' -e 's/^import .*$//g' ./**/*.js
 # Note: We use perl because sed adds an empty line at the end of every js file,
 # which was adding changes to git's staging, while debugging failing tests.
 
+# Extract a comma-separated list of global variables that are expected by TypeScript files passed as arguments.
 function getGlobals {
   grep -F --no-filename 'declare const' $@ \
     | cut -d' ' -f3 \
@@ -22,6 +23,7 @@ function getGlobals {
     | paste -sd "," -
 }
 
+# Fails if any JavaScript file references a symbol that is not included in the list of expected globals.
 function checkJS {
   FILES="$1"
   GLOBALS="$2"
@@ -33,8 +35,8 @@ function checkJS {
     "${FILES}"
 }
 
-# Check that JS files only call functions through the f global variable.
-
+# Check that all functions and constants referenced in JavaScript files will be
+# made available by MongoDB or the map-reduce command sent by sfdata.
 checkJS "compact/*.js" "f,emit,$(getGlobals 'compact/*.ts')"
 checkJS "public/*.js" "f,emit,$(getGlobals 'public/*.ts')"
 checkJS "reduce.algo2/*.js" "f,print,emit,bsonsize,$(getGlobals 'reduce.algo2/*.ts')"
