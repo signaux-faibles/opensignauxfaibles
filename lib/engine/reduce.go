5a573a4e9f85f3c18331250d0a95ef7c9a8777b5
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-04-12 14:32:31 +0200

    le paramètre "date_debut" n'est plus nécessaire pour reduce.algo2

--- lib/engine/reduce.go
+++ lib/engine/reduce.go
@@ -307,7 +307,6 @@ func reduceDefineScope(batch base.AdminBatch, types []string) (bson.M, error) {
 	}
 
 	scope := bson.M{
-		"date_debut":      batch.Params.DateDebut,
 		"date_fin":        batch.Params.DateFin,
 		"serie_periode":   misc.GenereSeriePeriode(batch.Params.DateDebut, batch.Params.DateFin),
 		"offset_effectif": (batch.Params.DateFinEffectif.Year()-batch.Params.DateFin.Year())*12 + int(batch.Params.DateFinEffectif.Month()-batch.Params.DateFin.Month()),
