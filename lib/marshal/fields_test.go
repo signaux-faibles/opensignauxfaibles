9df6eb2f2e56f3b109b15a6ab04521082ae7c649
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-04-14 16:00:09 +0200

    refacto des tests de fields

--- lib/marshal/fields_test.go
+++ lib/marshal/fields_test.go
@@ -10,37 +10,43 @@ import (
 
 func TestIndexColumnsFromCsvHeader(t *testing.T) {
 
-	t.Run("retourne un index fonctionnel des champs détectés, depuis un fichier CSV valide", func(t *testing.T) {
-		type TupleStruct struct{}
-		reader := csv.NewReader(strings.NewReader("year\n2020"))
-		idx, err := IndexColumnsFromCsvHeader(reader, TupleStruct{})
-		if assert.NoError(t, err) {
+	reader := makeCsvReader("year", "2020")
+	idx, err := IndexColumnsFromCsvHeader(reader, struct{}{})
+
+	if assert.NoError(t, err) {
+		t.Run("permet de vérifier la présence de colonnes depuis un fichier CSV valide", func(t *testing.T) {
 			hasYear, err := idx.HasFields([]string{"year"})
 			assert.Equal(t, true, hasYear)
 			assert.Equal(t, nil, err)
 			hasBothFields, err := idx.HasFields([]string{"year", "dummy"})
 			assert.Equal(t, false, hasBothFields)
 			assert.EqualError(t, err, "Colonne dummy non trouvée. Abandon.")
-		}
-	})
+		})
+
+		t.Run("permet d'indexer chaque ligne depuis un fichier CSV valide", func(t *testing.T) {
+			indexedRow := idx.IndexRow([]string{"2020"})
+			assert.Equal(t, "2020", indexedRow.GetVal("year"))
+		})
+	}
+
 }
 
 func TestIndexedRow(t *testing.T) {
 
+	idx := CreateColMapping(map[string]int{"year": 0}) // simule un fichier csv avec une colonne "year" à l'indice 0
+
 	t.Run("GetOptionalVal retourne une valeur si la colonne est trouvée", func(t *testing.T) {
-		type TupleStruct struct{}
-		headerRow := "year"
 		dataRow := "2020"
-		reader := csv.NewReader(strings.NewReader(headerRow + "\n" + dataRow))
-		idx, err := IndexColumnsFromCsvHeader(reader, TupleStruct{})
-		if assert.NoError(t, err) {
-			row := idx.IndexRow([]string{dataRow})
-			yearVal, yearFound := row.GetOptionalVal("year")
-			assert.Equal(t, "2020", yearVal)
-			assert.Equal(t, true, yearFound)
-			undefVal, undefFound := row.GetOptionalVal("colonne_inexistante")
-			assert.Equal(t, "", undefVal)
-			assert.Equal(t, false, undefFound)
-		}
+		row := idx.IndexRow([]string{dataRow})
+		yearVal, yearFound := row.GetOptionalVal("year")
+		assert.Equal(t, "2020", yearVal)
+		assert.Equal(t, true, yearFound)
+		undefVal, undefFound := row.GetOptionalVal("colonne_inexistante")
+		assert.Equal(t, "", undefVal)
+		assert.Equal(t, false, undefFound)
 	})
+}
+
+func makeCsvReader(headerRow string, dataRow string) *csv.Reader {
+	return csv.NewReader(strings.NewReader(headerRow + "\n" + dataRow))
 }
