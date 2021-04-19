cc4c277794ea5a2db5f9743cc230a5f76ad8a589
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-04-19 12:45:38 +0200

    test: `ccsf retourne la date de début de la procédure CCSF pour chaque période`

---
+++ js/reduce.algo2/ccsf_tests.ts
@@ -0,0 +1,28 @@
+// $ cd js && $(npm bin)/ava ./reduce.algo2/ccsf_tests.ts
+
+import test from "ava"
+import { ccsf } from "./ccsf"
+import { ParHash } from "../RawDataTypes"
+import { EntréeCcsf } from "../GeneratedTypes"
+
+test(`ccsf retourne la date de début de la procédure CCSF pour chaque période`, (t) => {
+  const vCcsf: ParHash<EntréeCcsf> = {
+    hash1: { date_traitement: makeUTCDate(2021, 2, 15) } as EntréeCcsf,
+    hash2: { date_traitement: makeUTCDate(2021, 0, 15) } as EntréeCcsf,
+    hash3: { date_traitement: makeUTCDate(2021, 1, 15) } as EntréeCcsf,
+  }
+  const output: { periode: Date; date_ccsf?: Date }[] = [
+    { periode: makeUTCDate(2021, 0) },
+    { periode: makeUTCDate(2021, 1) },
+    { periode: makeUTCDate(2021, 2) },
+  ]
+  ccsf(vCcsf, output)
+  t.deepEqual(output, [
+    { periode: makeUTCDate(2021, 0) },
+    { periode: makeUTCDate(2021, 1), date_ccsf: makeUTCDate(2021, 0, 15) },
+    { periode: makeUTCDate(2021, 2), date_ccsf: makeUTCDate(2021, 1, 15) },
+  ])
+})
+
+const makeUTCDate = (year: number, month: number, day?: number): Date =>
+  new Date(Date.UTC(year, month, day || 1))
