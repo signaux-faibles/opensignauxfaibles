396671ce2c716b494453bce942aaa76d981cd720
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-04-19 10:23:56 +0200

    feat(validation): Validation à chaque insertion dans ImportedData, par MongoDB (#342)
    Fix #87. (Suite et fin de "Mise en place de tests de validation de données au moment de l'import")
    
    ## Objectif
    
    Dans #87, nous avons ajouté la commande `sfdata validate` pour vérifier la validité des données importées dans la collection `ImportedData`, après avoir exécuté `sfdata import`.
    
    Maintenant que nous avons constaté le bon fonctionnement de cette validation et la validité des données qui ont été importées en production, cette PR propose de configurer MongoDB pour que tout document inséré dans `ImportedData` soit rejeté immédiatement en cas d'invalidité.
    
    De ce fait, la commande `sfdata validate` ne sera désormais utile que pour valider les documents de `RawData`. (c.a.d. données compactées)
    
    ## Notes importantes
    
    - Cette procédure de validation est couverte par un test automatisé, mais n'a pas été testée sur des données de production.
    - L'impact de cette procédure de validation sur le temps d'exécution de `sfdata import` n'a pas été mesuré.
    
    ## References
    
    * [Schema Validation — MongoDB Manual](https://docs.mongodb.com/manual/core/schema-validation/)
    * [$jsonSchema — MongoDB Manual](https://docs.mongodb.com/manual/reference/operator/query/jsonSchema/)

--- lib/engine/db.go
+++ lib/engine/db.go
@@ -1,6 +1,7 @@
 package engine
 
 import (
+	"fmt"
 	"log"
 	"sync"
 	"time"
@@ -56,11 +57,9 @@ func InitDB() DB {
 		Key: []string{"_id.type", "_id.key"},
 	})
 
-	// Création d'index sur la collection ImportedData, pour le découpage du map-reduce de Compact
-	db.C("ImportedData").EnsureIndex(mgo.Index{
-		Name: "value.key_1",         // trouvé sur la db de prod
-		Key:  []string{"value.key"}, // numéro SIRET ou SIREN
-	})
+	if err = CreateImportedDataCollection(db, "ImportedData"); err != nil {
+		log.Fatal("échec d'initialisation de ImportedData: " + err.Error())
+	}
 
 	firstBatchID := viper.GetString("FIRST_BATCH")
 	if !base.IsBatchID(firstBatchID) {
@@ -99,6 +98,45 @@ func InitDB() DB {
 	}
 }
 
+// CreateImportedDataCollection créée une collection "ImportedData" avec index et
+// validation de documents.
+func CreateImportedDataCollection(db *mgo.Database, colName string) error {
+	// Création d'index sur la collection ImportedData, pour le découpage du map-reduce de Compact
+	db.C(colName).EnsureIndex(mgo.Index{
+		Name: "value.key_1",         // trouvé sur la db de prod
+		Key:  []string{"value.key"}, // numéro SIRET ou SIREN
+	})
+
+	// Injection du schéma de validation de données JSON dans ImportedData
+	jsonSchemas, err := LoadJSONSchemaFiles()
+	if err != nil {
+		return err
+	}
+	schemaPerHashedDataType := MakeValidationSchemaPerHashedDataType(jsonSchemas)
+	jsonSchema := MakeValidationSchemaForImportedData(schemaPerHashedDataType)
+	return setupDocValidation(db, colName, jsonSchema)
+}
+
+// setupDocValidation configure la validation de documents pour une collection existante.
+func setupDocValidation(db *mgo.Database, colName string, jsonSchema bson.M) error {
+	var validRes struct {
+		Ok            bool          `bson:"ok" json:"ok"`
+		Errmsg        string        `bson:"errmsg" json:"errmsg"`
+		Code          int           `bson:"code" json:"code"`
+		CodeName      string        `bson:"codeName" json:"codeName"`
+		OperationTime time.Duration `bson:"operationTime" json:"operationTime"`
+	}
+	db.Run(bson.D{
+		{Name: "collMod", Value: colName},
+		{Name: "validator", Value: bson.M{"$jsonSchema": jsonSchema}},
+		{Name: "validationLevel", Value: "strict"},
+		{Name: "validationAction", Value: "error"}}, &validRes)
+	if !validRes.Ok {
+		return fmt.Errorf("error %v while trying to setup doc validation on %v: %v", validRes.Code, colName, validRes.Errmsg)
+	}
+	return nil
+}
+
 var importing sync.WaitGroup
 
 // InsertIntoImportedData retourne un canal dont les objets seront ajoutés à
@@ -112,13 +150,14 @@ func InsertIntoImportedData(db *mgo.Database) chan *Value {
 		buffer := make(map[string]*Value)
 		objects := make([]interface{}, 0)
 		i := 0
-
 		insertObjectsIntoImportedData := func() {
 			for _, v := range buffer {
 				objects = append(objects, *v)
 			}
 			if len(objects) > 0 {
-				db.C("ImportedData").Insert(objects...)
+				if err := db.C("ImportedData").Insert(objects...); err != nil {
+					log.Println("Erreur lors de l'insertion de certains documents dans ImportedData: " + err.Error()) // ex: document invalide, cf CreateImportedDataCollection()
+				}
 			}
 			buffer = make(map[string]*Value)
 			objects = make([]interface{}, 0)
