589e2aa78d05af68e709c98065dcf0f3bdd24395
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-04-19 14:00:31 +0200

    simplification et optimisation de public/reduce.ts

--- js/public/reduce.ts
+++ js/public/reduce.ts
@@ -3,8 +3,5 @@ import { SortieMap } from "./map"
 export type V = Partial<SortieMap>
 
 export function reduce(_key: unknown, values: V[]): V {
-  return values.reduce((m, v) => {
-    Object.assign(m, v)
-    return m
-  }, {} as V)
+  return Object.assign({}, ...values)
 }
