725a97cff7c73b1d1cf62db963faebe55e959988
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-04-28 14:22:25 +0200

    reproduction bug: "Type de fichier non reconnu: filter"

--- tests/test-import.sh
+++ tests/test-import.sh
@@ -35,6 +35,7 @@ tests/helpers/mongodb-container.sh run > /dev/null << CONTENTS
         "type" : "batch"
     },
     "files": {
+      "filter":       [],
       "paydex":       [ "/../lib/paydex/testData/paydexTestData.csv" ],
       "apconso":      [ "/../lib/apconso/testData/apconsoTestData.csv" ],
       "apdemande":    [ "/../lib/apdemande/testData/apdemandeTestData.csv" ],
