7e82cedd4cdfdfdbc1241c2629cfee77f77e8b01
Jérôme Desseaux <jerome.desseaux@gmail.com>
2022-04-04 18:08:07 +0200

    Ajout de la logique pour retrouver un batch à partir d'une date figée. Nécessite encore de récupérer une date en paramètre.

--- js/redressement2203/map.ts
+++ js/redressement2203/map.ts
@@ -1,23 +1,9 @@
-// import { f } from "./functions"
-// import {
-//   EntréeApConso,
-//   EntréeApDemande,
-//   EntréeCompte,
-//   EntréeDelai,
-//   EntréeDéfaillances,
-//   EntréeDiane,
-//   EntréeEllisphere,
-//   EntréePaydex,
-//   EntréeSirene,
-//   EntréeSireneEntreprise,
-// } from "../GeneratedTypes"
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {
   CompanyDataValues,
   BatchKey,
   SortieRedressementUrssaf2203,
 } from "../RawDataTypes"
-// import { SortieDebit } from "./debits"
-// import { Bdf } from "./bdf"
 
 export type SortieMap = SortieRedressementUrssaf2203
 
@@ -31,10 +17,31 @@ export type OutValue = Partial<SortieMap>
 declare function emit(key: string, value: OutValue): void
 
 export function map(this: Input): void {
+  const testDate = new Date("2021-09-01")
+  const batches = this.value.batch
+  const beforeBatches = []
+  const afterBatches = []
+  for (const [_, value] of Object.entries(batches)) {
+    if (value.debit) {
+      const debit = value.debit
+      for (const [_, value2] of Object.entries(debit)) {
+        value2.periode.start > testDate
+          ? afterBatches.push(value2)
+          : beforeBatches.push(value2)
+      }
+    }
+  }
+  const latestBatchBeforeDate = beforeBatches.reduce((a, b) =>
+    a.periode.start > b.periode.start ? a : b
+  )
+  const latestBatchAfterDate = afterBatches.reduce((a, b) =>
+    a.periode.start > b.periode.start ? a : b
+  )
+
   emit(this.value.key, {
-    partPatronaleAncienne: 0,
-    partOuvriereAncienne: 0,
-    partPatronaleRecente: 0,
-    partOuvriereRecente: 0,
+    partPatronaleAncienne: latestBatchBeforeDate.part_patronale,
+    partOuvriereAncienne: latestBatchBeforeDate.part_ouvriere,
+    partPatronaleRecente: latestBatchAfterDate.part_patronale,
+    partOuvriereRecente: latestBatchAfterDate.part_ouvriere,
   })
 }
