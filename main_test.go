9ff10b4cbd7819b297728f3e83cbd32d7d20eecd
Christophe Ninucci <christophe-gitkraken@zbouboutchi.net>
2023-01-23 15:11:25 +0100

    - retrait de l'import inutile

--- main_test.go
+++ main_test.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/globalsign/mgo"
 	"github.com/globalsign/mgo/bson"
-	"github.com/signaux-faibles/opensignauxfaibles/lib/engine"
+// 	"github.com/signaux-faibles/opensignauxfaibles/lib/engine"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 )
