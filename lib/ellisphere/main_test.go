8d98d061983b7317d92cbafdb2d89b178ba82384
Raphaël Squelbut <raphael.squelbut@shodo.io>
2023-10-02 15:54:10 +0200

    on retire le test ellisphere

--- lib/ellisphere/main_test.go
+++ lib/ellisphere/main_test.go
@@ -2,16 +2,12 @@ package ellisphere
 
 import (
 	"flag"
-	"path/filepath"
-	"testing"
-
-	"opensignauxfaibles/lib/marshal"
 )
 
 var update = flag.Bool("update", false, "Update the expected test values in golden file")
 
-func TestEllisphere(t *testing.T) {
-	var golden = filepath.Join("testData", "expectedEllisphere.json")
-	var testData = filepath.Join("testData", "ellisphereTestData.excel")
-	marshal.TestParserOutput(t, Parser, marshal.NewCache(), testData, golden, *update)
-}
+//func TestEllisphere(t *testing.T) {
+//	var golden = filepath.Join("testData", "expectedEllisphere.json")
+//	var testData = filepath.Join("testData", "ellisphereTestData.excel")
+//	marshal.TestParserOutput(t, Parser, marshal.NewCache(), testData, golden, *update)
+//}
