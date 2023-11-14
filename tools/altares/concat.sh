57949c3081510f0a023130c14a865dd423272f20
Raphaël Squelbut <raphael.squelbut@shodo.io>
2023-11-14 18:39:35 +0100

    améliore le code de convert and concat des fichiers altares

--- tools/altares/concat.sh
+++ tools/altares/concat.sh
@@ -1,17 +1,13 @@
 echo "stock"
-cat resources/SF_DATA_20230706.txt > output.csv
+go run convert_stock.go resources/SF_DATA_20230706.txt > output.csv
 echo "increment 1"
 go run convert_increment.go resources/S_202011095834-3_202311010315.csv >> output.csv
 echo "increment 2"
 go run convert_increment.go resources/S_202011095834-3_202310020319.csv >> output.csv
-go run remove_column_experience_paiement.go output.csv > cleaned_output.csv
-#echo "ajoute les accents"
-#sed -i 's/etat_organisation/état_organisation/' cleaned_output.csv
-#sed -i 's/encours_etudies/encours_étudiés/' cleaned_output.csv
 echo "résultat"
-head cleaned_output.csv
+head output.csv
 echo "."
 echo "."
 echo "."
-tail cleaned_output.csv
-tar -czvf altares.tar.gz cleaned_output.csv
+tail output.csv
+tar -czf altares.tar.gz output.csv
