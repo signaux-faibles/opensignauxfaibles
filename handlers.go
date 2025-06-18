336ddc86d404db102919529927323a9ec12881f0
Pierre Camilleri <pierre.camilleri@multi.coop>
2025-06-18 17:04:21 +0200

    Remove handlers

--- handlers.go
+++ handlers.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"os"
 	"sort"
-	"time"
 
 	"github.com/cosiner/flag"
 	"github.com/globalsign/mgo/bson"
@@ -118,81 +117,6 @@ func (params checkBatchHandler) Run() error {
 	sort.Strings(reports) // to make sure that parsed files are always listed in the same order
 	printJSON(bson.M{"reports": reports})
 	return nil
-}
-
-type purgeNotCompactedHandler struct {
-	Enable                 bool // set to true by cosiner/flag if the user is running this command
-	IUnderstandWhatImDoing bool `names:"--i-understand-what-im-doing" desc:"Nécessaire pour confirmer la suppression de données"`
-}
-
-func (params purgeNotCompactedHandler) Documentation() flag.Flag {
-	return flag.Flag{
-		Usage: "Vide la collection ImportedData",
-		Desc: `
-		Vide la collection "ImportedData".
-		Répond "ok" dans la sortie standard, si le traitement s'est bien déroulé.
-		`,
-	}
-}
-
-func (params purgeNotCompactedHandler) IsEnabled() bool {
-	return params.Enable
-}
-
-func (params purgeNotCompactedHandler) Validate() error {
-	if !params.IUnderstandWhatImDoing {
-		return errors.New("--i-understand-what-im-doing doit être employé pour confirmer la suppression")
-	}
-	return nil
-}
-
-func (params purgeNotCompactedHandler) Run() error {
-	startDate := time.Now()
-	err := engine.PurgeNotCompacted()
-	if err != nil {
-		return err
-	}
-	engine.LogOperationEvent("PurgeNotCompacted", startDate)
-	printJSON("ok")
-	return nil
-}
-
-type pruneEntitiesHandler struct {
-	Enable   bool   // set to true by cosiner/flag if the user is running this command
-	BatchKey string `names:"--batch" arglist:"batch_key" desc:"Identifiant du batch à nettoyer (ex: 1802, pour Février 2018)"`
-	Delete   bool   `names:"--delete" desc:"Nécessaire pour confirmer la suppression de données"`
-}
-
-func (params pruneEntitiesHandler) Documentation() flag.Flag {
-	return flag.Flag{
-		Usage: "Compte/supprime les entités hors périmètre",
-		Desc: `
-		Compte ou supprime dans la collection "RawData" les entités (établissements et entreprises)
-		non listées dans le filtre de périmètre du batch spécifié.
-		Répond avec un propriété JSON "count" qui vaut le nombre d'entités hors périmètre comptées ou supprimées.
-	`,
-	}
-}
-
-func (params pruneEntitiesHandler) IsEnabled() bool {
-	return params.Enable
-}
-
-func (params pruneEntitiesHandler) Validate() error {
-	if params.BatchKey == "" {
-		return errors.New("paramètre `batch` obligatoire")
-	}
-	return nil
-}
-
-// Count – then delete – companies from RawData that should have been
-// excluded by the SIREN Filter.
-func (params pruneEntitiesHandler) Run() error {
-	count, err := engine.PruneEntities(params.BatchKey, params.Delete)
-	if err == nil {
-		printJSON(bson.M{"count": count})
-	}
-	return err
 }
 
 func printJSON(object interface{}) {
