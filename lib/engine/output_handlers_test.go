48ba462ccd85014d7a173dddb2382747f12fa824
Pierre Camilleri <pierre.camilleri@multi.coop>
2025-07-01 11:18:21 +0200

    fixup! fixup! 🔵 no more Value/Data type needed

--- lib/engine/output_handlers_test.go
+++ lib/engine/output_handlers_test.go
@@ -12,6 +12,27 @@ import (
 
 var fakeCsv faker.Faker
 
+type TestTuple struct{}
+
+func (TestTuple) Key() string {
+	return "000000000"
+}
+
+func (TestTuple) Scope() string {
+	return "entreprise"
+}
+
+func (TestTuple) Type() string {
+	return "test"
+}
+
+func (TestTuple) Headers() []string {
+	return []string{"test_header"}
+}
+func (TestTuple) Values() []string {
+	return []string{"test_value"}
+}
+
 func init() {
 	fakeCsv = faker.New()
 }
@@ -19,9 +40,9 @@ func init() {
 func Test_writeLinesToCSV(t *testing.T) {
 	exportPath := filepath.Join(os.TempDir(), fakeCsv.Lorem().Word())
 	viper.Set("export.path", exportPath)
-	batchKey := BatchKey("2310")
+	batchKey := "2310"
 
-	tuple := Test{}
+	tuple := TestTuple{}
 	writeLineToCSV(batchKey, tuple)
 	assert.FileExists(t, filepath.Join(exportPath, batchKey, tuple.Type()+".csv"))
 }
