fb6604a2aba6218014646096088c0b6673013133
Pierre Camilleri <pierre.camilleri@multi.coop>
2025-07-09 11:07:06 +0200

    🔵 improving output.go

--- lib/engine/output.go
+++ lib/engine/output.go
@@ -1,6 +1,7 @@
 package engine
 
 import (
+	"errors"
 	"opensignauxfaibles/lib/marshal"
 	"sync"
 )
@@ -12,25 +13,25 @@ type OutputStreamer interface {
 	Stream(ch chan marshal.Tuple) error
 }
 
-func NewCombinedStreamer(out1, out2 OutputStreamer) *CombinedStreamer {
-	return &CombinedStreamer{out1, out2}
-
-}
-
 // A CombinedStreamer streams to two different sinks
 type CombinedStreamer struct {
 	out1 OutputStreamer
 	out2 OutputStreamer
 }
 
-// Stream dispatches data to the two OutputStreamer
-// The returned error is the first error encountered, if any.
+func NewCombinedStreamer(out1, out2 OutputStreamer) *CombinedStreamer {
+	return &CombinedStreamer{out1, out2}
+}
+
+// Stream dispatches data to two sinks, with two `OutputStreamer`s that run concurrently.
 func (combined *CombinedStreamer) Stream(ch chan marshal.Tuple) error {
 
-	outCh1 := make(chan marshal.Tuple, 10)
-	outCh2 := make(chan marshal.Tuple, 10)
+	outCh1 := make(chan marshal.Tuple, 1000)
+	outCh2 := make(chan marshal.Tuple, 1000)
 
-	errCh := make(chan error, 3)
+	// All errors (max 2) are buffered as they are processed only after both
+	// streams have ended
+	errCh := make(chan error, 2)
 
 	go func() {
 		defer close(outCh1)
@@ -67,10 +68,20 @@ func (combined *CombinedStreamer) Stream(ch chan marshal.Tuple) error {
 	// When all go routines have ended we can safely close errCh
 	close(errCh)
 
-	err, ok := <-errCh
-	if ok {
-		return err
+	errs := []error{}
+	for err := range errCh {
+		errs = append(errs, err)
 	}
 
-	return nil
+	var err error
+	switch len(errs) {
+	case 2:
+		err = errors.Join(errs[0], errs[1])
+	case 1:
+		err = errs[0]
+	case 0:
+		err = nil
+	}
+
+	return err
 }
