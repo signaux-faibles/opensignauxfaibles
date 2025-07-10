44e00aa2c8024b6230204a621606f8fa1e2ac4a1
Pierre Camilleri <pierre.camilleri@multi.coop>
2025-07-10 11:27:03 +0200

    Update apdemande header

--- lib/apdemande/main.go
+++ lib/apdemande/main.go
@@ -14,20 +14,20 @@ import (
 
 // APDemande Demande d'activité partielle
 type APDemande struct {
-	ID                 string       `input:"ID_DA"              json:"id_demande"           csv:"ID_DA"`
-	Siret              string       `input:"ETAB_SIRET"         json:"-"                    csv:"ETAB_SIRET"`
-	EffectifEntreprise *int         `input:"EFF_ENT"            json:"effectif_entreprise"  csv:"EFF_ENT"`
-	Effectif           *int         `input:"EFF_ETAB"           json:"effectif"             csv:"EFF_ETAB"`
-	DateStatut         time.Time    `input:"DATE_STATUT"        json:"date_statut"          csv:"DATE_STATUT"`
+	ID                 string       `input:"ID_DA"              json:"id_demande"           csv:"id_demande"`
+	Siret              string       `input:"ETAB_SIRET"         json:"-"                    csv:"siret"`
+	EffectifEntreprise *int         `input:"EFF_ENT"            json:"effectif_entreprise"  csv:"effectif_entreprise"`
+	Effectif           *int         `input:"EFF_ETAB"           json:"effectif"             csv:"effectif"`
+	DateStatut         time.Time    `input:"DATE_STATUT"        json:"date_statut"          csv:"date_statut"`
 	Periode            misc.Periode `                           json:"periode"`
-	HTA                *float64     `input:"HTA"                json:"hta"                  csv:"HTA"`
-	MTA                *float64     `                           json:"mta"                  csv:"MTA"`
-	EffectifAutorise   *int         `input:"EFF_AUTO"           json:"effectif_autorise"    csv:"EFF_AUTO"`
-	MotifRecoursSE     *int         `input:"MOTIF_RECOURS_SE"   json:"motif_recours_se"     csv:"MOTIF_RECOURS_SE"`
-	HeureConsommee     *float64     `input:"S_HEURE_CONSOM_TOT" json:"heure_consommee"      csv:"S_HEURE_CONSOM_TOT"`
-	MontantConsomme    *float64     `                           json:"montant_consommee"    csv:"MONTANT_CONSOMME"`
-	EffectifConsomme   *int         `input:"S_HEURE_CONSOM_TOT" json:"effectif_consomme"    csv:"S_HEURE_CONSOM_TOT"`
-	Perimetre          *int         `input:"PERIMETRE_AP"       json:"perimetre"            csv:"PERIMETRE_AP"`
+	HTA                *float64     `input:"HTA"                json:"hta"                  csv:"heures_autorisées"`
+	MTA                *float64     `                           json:"mta"                  csv:"montants_autorisés"`
+	EffectifAutorise   *int         `input:"EFF_AUTO"           json:"effectif_autorise"    csv:"effectif_autorisé"`
+	MotifRecoursSE     *int         `input:"MOTIF_RECOURS_SE"   json:"motif_recours_se"     csv:"motif_recours_se"`
+	HeureConsommee     *float64     `input:"S_HEURE_CONSOM_TOT" json:"heure_consommee"      csv:"heure_consommee"`
+	MontantConsomme    *float64     `                           json:"montant_consommee"    csv:"montant_consomme"`
+	EffectifConsomme   *int         `input:"S_HEURE_CONSOM_TOT" json:"effectif_consomme"    csv:"effectif_consomme"`
+	Perimetre          *int         `input:"PERIMETRE_AP"       json:"perimetre"            csv:"perimetre"`
 }
 
 // Key id de l'objet
