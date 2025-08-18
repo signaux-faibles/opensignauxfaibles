785c75ba320c8dc6a12877983f835eada77fd07b
Pierre Camilleri <pierre.camilleri@multi.coop>
2025-08-18 18:11:06 +0200

    feat: prepare sirene_ul pgsql import

---
+++ lib/engine/migrations/004_create_sirene_ul.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS stg_sirene_ul (
+    siren                   VARCHAR(9),
+    nic                     VARCHAR(5),
+    raison_sociale          TEXT,
+    prenom1_unite_legale    VARCHAR(100),
+    prenom2_unite_legale    VARCHAR(100),
+    prenom3_unite_legale    VARCHAR(100),
+    prenom4_unite_legale    VARCHAR(100),
+    nom_unite_legale        VARCHAR(100),
+    nom_usage_unite_legale  VARCHAR(100),
+    statut_juridique        VARCHAR(10),
+    creation                DATE
+);
+
+CREATE INDEX IF NOT EXISTS idx_stg_sirene_ul_siren ON stg_sirene_ul(siren);
+
+---- create above / drop below ----
+
+DROP TABLE stg_sirene_ul;
