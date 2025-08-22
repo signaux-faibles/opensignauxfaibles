7cc7723e946851cc772794d9eee791e02831abb9
Pierre Camilleri <pierre.camilleri@multi.coop>
2025-08-22 11:33:44 +0200

    Back to view...

--- lib/engine/migrations/003_create_apconso.sql
+++ lib/engine/migrations/003_create_apconso.sql
@@ -10,6 +10,8 @@ CREATE TABLE  IF NOT EXISTS stg_apconso  (
 
 CREATE INDEX IF NOT EXISTS idx_stg_apconso_id_demande ON stg_apconso(id_demande);
 CREATE INDEX IF NOT EXISTS idx_stg_apconso_siret ON stg_apconso(siret);
+CREATE INDEX IF NOT EXISTS idx_stg_apconso_siren ON stg_apconso(LEFT(siret, 9));
+CREATE INDEX IF NOT EXISTS idx_stg_apconso_period ON stg_apconso(periode);
 
 ---- create above / drop below ----
 
