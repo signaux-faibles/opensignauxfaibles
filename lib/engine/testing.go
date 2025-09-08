f1e117ed0ad01a95edbeea71f6b7d3b1247f4f37
Pierre Camilleri <pierre.camilleri@multi.coop>
2025-09-08 14:44:08 +0200

    🟢 test that parser errors if sink fails
    Passes right away

--- lib/engine/testing.go
+++ lib/engine/testing.go
@@ -1,11 +1,28 @@
 package engine
 
-import "opensignauxfaibles/lib/marshal"
+import (
+	"context"
+	"fmt"
+	"opensignauxfaibles/lib/marshal"
+)
 
-// DiscardTuple supprime les évènements
+// DiscardTuple ignore les données
 func DiscardTuple(tuples chan marshal.Tuple) {
 	go func() {
 		for range tuples {
 		}
 	}()
+}
+
+// FailDataSink is a sink that always fails
+type FailDataSink struct{}
+
+func (s *FailDataSink) ProcessOutput(ctx context.Context, ch chan marshal.Tuple) error {
+	return fmt.Errorf("this sink always fails")
+}
+
+type FailSinkFactory struct{}
+
+func (FailSinkFactory) CreateSink(parserType string) (DataSink, error) {
+	return &FailDataSink{}, nil
 }
