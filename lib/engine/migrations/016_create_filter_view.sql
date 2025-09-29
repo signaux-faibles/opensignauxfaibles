26458b0747da621183f75b0251dcecf0a5b36c92
Pierre Camilleri <pierre.camilleri@multi.coop>
2025-09-29 12:26:08 +0200

    Query does not fail with missing "excluded_categories"

--- lib/engine/migrations/016_create_filter_view.sql
+++ lib/engine/migrations/016_create_filter_view.sql
@@ -13,7 +13,7 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS filter AS
   LEFT JOIN clean_sirene_ul s ON s.siren = LEFT(e.siret, 9)
   WHERE e.effectif >= const.min_effectif
     AND e.periode >= CURRENT_DATE - const.n_months * INTERVAL '1 month'
-    AND NOT (s.statut_juridique = ANY(const.excluded_categories));
+    AND NOT (s.statut_juridique = ANY(const.excluded_categories)) OR s.statut_juridique IS NULL;
 
 CREATE UNIQUE INDEX filter_siren_index
     ON filter(siren);
