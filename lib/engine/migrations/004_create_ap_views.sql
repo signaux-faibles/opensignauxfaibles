2bc19a99b4578362458d2709bd8f2a0822e50366
Pierre Camilleri <pierre.camilleri@multi.coop>
2025-10-01 14:50:25 +0200

    🔴 changing clean_ap to a materialized view

--- lib/engine/migrations/004_create_ap_views.sql
+++ lib/engine/migrations/004_create_ap_views.sql
@@ -1,10 +1,10 @@
--- Vue intermédiaire des demandes
--- * elle décompose (=répète pour chaque période) les intervalles concernés par
--- la demdande.
--- * elle calcule en "ETP équivalent" le nombre d'heures demandés.
---
--- On matérialise la vue pour lui faire bénéficier de nouveaux indexes
-CREATE MATERIALIZED VIEW IF NOT EXISTS stg_apdemande_by_period AS
+CREATE MATERIALIZED VIEW IF NOT EXISTS clean_ap AS
+
+  WITH stg_apdemande_by_period AS (
+    -- Vue intermédiaire des demandes
+    -- * elle décompose (=répète pour chaque période) les intervalles concernés par
+    -- la demdande.
+    -- * elle calcule en "ETP équivalent" le nombre d'heures demandés.
     SELECT
         d.siret,
         DATE_TRUNC('month', period_series.period)::date as periode,
@@ -25,39 +25,35 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS stg_apdemande_by_period AS
     ) period_series
     WHERE d.siret IS NOT NULL
       AND d.periode_debut IS NOT NULL
-      AND d.periode_fin IS NOT NULL;
+      AND d.periode_fin IS NOT NULL
+    ),
 
-CREATE INDEX IF NOT EXISTS idx_stg_apdemande_by_period_period ON stg_apdemande_by_period(periode);
-CREATE INDEX IF NOT EXISTS idx_stg_apdemande_by_period_siret ON stg_apdemande_by_period(siret);
-CREATE INDEX IF NOT EXISTS idx_stg_apdemande_by_period_siren ON stg_apdemande_by_period(LEFT(siret, 9));
-
-
--- Vue intermédiaire des consommations.
--- * calcule les ETP consommés,
--- * formatée dans un format identique que les demandes pour permettre un UNION ALL
---
--- Contrairement à la demande, cette vue bénéficie déjà des bons indexes, et
--- ne nécessite pas d'être matérialisée
-CREATE OR  REPLACE VIEW stg_apconso_by_period AS
-SELECT
-    c.siret,
-    periode::date,
-    0 as ETP_autorise,
-    CASE
-        WHEN c.heures IS NOT NULL THEN c.heures / 151.67
-        ELSE 0
-    END as ETP_consomme,
-    NULL as motif_recours  -- pas de motif pour la consommation, uniquement pour les demandes
-FROM stg_apconso c
-WHERE c.siret   IS NOT NULL
-  AND c.periode IS NOT NULL;
+    stg_apconso_by_period  AS (
+      -- Vue intermédiaire des consommations.
+      -- * calcule les ETP consommés,
+      -- * formatée dans un format identique que les demandes pour permettre un UNION ALL
+      --
+      -- Contrairement à la demande, cette vue bénéficie déjà des bons indexes, et
+      -- ne nécessite pas d'être matérialisée
+        SELECT
+            c.siret,
+            periode::date,
+            0 as ETP_autorise,
+            CASE
+                WHEN c.heures IS NOT NULL THEN c.heures / 151.67
+                ELSE 0
+            END as ETP_consomme,
+            NULL as motif_recours  -- pas de motif pour la consommation, uniquement pour les demandes
+        FROM stg_apconso c
+        WHERE c.siret   IS NOT NULL
+          AND c.periode IS NOT NULL
+       )
 
 
--- Finalement, on agrége par siret x période les consommations et les demandes
-CREATE OR REPLACE VIEW clean_ap AS
+-- on agrége par siret x période les consommations et les demandes
 SELECT
     siret,
-    LEFT(siret, 9) as siren,
+    LEFT(siret, 9)::VARCHAR(9) as siren,
     periode,
     SUM(ETP_autorise) as ETP_autorise,
     SUM(ETP_consomme) as ETP_consomme,
@@ -67,7 +63,9 @@ SELECT
 FROM (SELECT * FROM stg_apdemande_by_period UNION ALL SELECT * FROM stg_apconso_by_period) tmp
 GROUP BY siret, periode;
 
+CREATE INDEX IF NOT EXISTS idx_clean_ap_period ON clean_ap(periode);
+CREATE INDEX IF NOT EXISTS idx_clean_ap_siret ON clean_ap(siret);
+CREATE INDEX IF NOT EXISTS idx_clean_ap_siren ON clean_ap(siren);
+
 ---- create above / drop below ----
-DROP MATERIALIZED VIEW IF EXISTS stg_apdemande_by_period CASCADE;
-DROP VIEW IF EXISTS stg_apconso_by_period CASCADE;
-DROP VIEW IF EXISTS clean_ap CASCADE;
+DROP MATERIALIZED VIEW IF EXISTS clean_ap CASCADE;
