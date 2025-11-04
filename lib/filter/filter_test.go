f8624b465db27f08c147443027609d8810141b95
Pierre Camilleri <pierre.camilleri@multi.coop>
2025-11-04 11:21:15 +0100

    fixup! 🔴 test empty filter

--- lib/filter/filter_test.go
+++ lib/filter/filter_test.go
@@ -9,7 +9,7 @@ import (
 func TestFilterReader(t *testing.T) {
 
 	t.Run("Using a nil reader is possible and results in no filtering", func(t *testing.T) {
-		var r Reader
+		var r *Reader
 		filter, err := r.Read()
 		assert.NoError(t, err, "Reading a nil filter.Reader should succeed")
 		assert.Nil(t, filter.All(), "Reading a nil filter.Reader should return a NoFilter")
