35940db7ff462b20515deb0730c4c300429f264b
Pierre Camilleri <pierre.camilleri@multi.coop>
2025-11-19 12:28:12 +0100

    🔵 doc

--- lib/parsing/sirene_histo/tuple.go
+++ lib/parsing/sirene_histo/tuple.go
@@ -5,14 +5,13 @@ import (
 	"time"
 )
 
-// SireneHisto
-// History of changes to "établissements" SIRENE
-// We keep only changes related to
+// SireneHisto is one event in the history of changes to "établissements" SIRENE
+// We keep only changes related to being active or closed
 type SireneHisto struct {
-	Siret     string     `input:"siret"                                    json:"siret"                    sql:"siret"`
-	DateDebut *time.Time `input:"dateDebut"                                json:"date_debut,omitempty"     sql:"date_debut"`
-	DateFin   *time.Time `input:"dateFin"                                  json:"date_fin,omitempty"       sql:"date_fin"`
-	EstActif  bool       `input:"etatAdministratifEtablissement"           json:"est_actif,omitempty"      sql:"est_actif"`
+	Siret     string     `input:"siret"                          json:"siret"                    sql:"siret"`
+	DateDebut *time.Time `input:"dateDebut"                      json:"date_debut,omitempty"     sql:"date_debut"`
+	DateFin   *time.Time `input:"dateFin"                        json:"date_fin,omitempty"       sql:"date_fin"`
+	EstActif  bool       `input:"etatAdministratifEtablissement" json:"est_actif,omitempty"      sql:"est_actif"`
 	// Is the change related to a the "établissement" closing (EstActif true -> false) or (re)opening (EstActif false -> true) ?
 	ChangementStatutActif bool ` input:"changementEtatAdministratifEtablissement" json:"changement_statut_actif"  sql:"changement_statut_actif"`
 }
