5ad16b124909d0bd5096786cbcf7cf16b7d5e039
Pierre Camilleri <pierre.camilleri@multi.coop>
2025-11-26 14:24:40 +0100

    fix: rollback should restore previous view

--- lib/db/migrations/022_change_effectif_views.sql
+++ lib/db/migrations/022_change_effectif_views.sql
@@ -21,5 +21,14 @@ CREATE OR REPLACE VIEW clean_effectif_ent AS
 
 ---- create above / drop below ----
 
-DROP VIEW IF EXISTS clean_effectif;
-DROP VIEW IF EXISTS clean_effectif_ent;
+-- Rollback : restaure l'ancienne vue
+-- (migration 016_create_effectif_views.sql)
+CREATE OR REPLACE VIEW clean_effectif AS
+  SELECT *
+  FROM stg_effectif
+  WHERE LEFT(siret, 9) IN (SELECT siren FROM clean_filter);
+
+CREATE OR REPLACE VIEW clean_effectif_ent AS
+  SELECT *
+  FROM stg_effectif_ent
+  WHERE siren IN (SELECT siren FROM clean_filter);
