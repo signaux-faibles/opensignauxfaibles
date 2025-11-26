660ee7647395860d2f01bc2884d508bfea53b20d
Pierre Camilleri <pierre.camilleri@multi.coop>
2025-11-26 13:52:30 +0100

    🔵 comment on migrations being superseeded

--- lib/db/migrations/016_create_effectif_views.sql
+++ lib/db/migrations/016_create_effectif_views.sql
@@ -1,7 +1,5 @@
--- Ces vues permettent de ne pas modifier le code des utilisateurs de données
--- le jour ou des transformations sont nécessaires dans ces données.
--- Par ailleurs, les utilisateurs savent qu'ils doivent uniquement requêter
--- les tables nommées 'clean_...'
+
+-- Migrations remplacées par 022_change_effectif_views.sql
 CREATE OR REPLACE VIEW clean_effectif AS
   SELECT *
   FROM stg_effectif
