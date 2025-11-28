b83b6a37bef3369cc321f38bc9e15cb28db6c485
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2025-11-28 17:05:21 +0100

    feat: procol aggrégé par sirene (#538)
    * feat: procol aggrégé par sirene
    
    * 🟢 adapt the procol_helper function accordingly

--- lib/db/migrations/018_create_procol_views.sql
+++ lib/db/migrations/018_create_procol_views.sql
@@ -1,7 +1,14 @@
 CREATE OR REPLACE VIEW clean_procol AS
-SELECT *
+SELECT
+  LEFT(siret, 9) as siren,
+  date_effet,
+  action_procol,
+  stade_procol
 FROM stg_procol
-WHERE LEFT(siret, 9) IN (SELECT siren FROM clean_filter);
+WHERE LEFT(siret, 9) IN (SELECT siren FROM clean_filter)
+  -- On ignore la clôture administrative de la procédure
+  AND stade_procol != 'solde_procedure'
+GROUP BY LEFT(siret, 9), date_effet, action_procol, stade_procol;
 
 ---- create above / drop below ----
 
