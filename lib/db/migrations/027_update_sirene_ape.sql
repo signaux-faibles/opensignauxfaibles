23c06fb9dc2afa6839e9124abffb03fd5c97ba04
Pierre Camilleri <pierre.camilleri@multi.coop>
2025-12-08 21:53:30 +0100

    Add naf_section to sirene views

--- lib/db/migrations/027_update_sirene_ape.sql
+++ lib/db/migrations/027_update_sirene_ape.sql
@@ -10,45 +10,49 @@ ALTER TABLE stg_sirene_ul RENAME COLUMN activite_principale TO ape;
 -- Update clean_sirene view to use ape instead of code_activite and nomenclature_activite
 CREATE OR REPLACE VIEW clean_sirene AS
   SELECT
-    siren,
-    siret,
-    siege,
+    s.siren,
+    s.siret,
+    s.siege,
     -- adresse
     TRIM(
       CONCAT_WS(' ',
-        COALESCE(numero_voie, ''),
-        COALESCE(indrep, ''),
-        COALESCE(type_voie, ''),
-        COALESCE(voie, ''),
-        CASE WHEN complement_adresse IS NOT NULL THEN CONCAT('(', complement_adresse, ')') END
+        COALESCE(s.numero_voie, ''),
+        COALESCE(s.indrep, ''),
+        COALESCE(s.type_voie, ''),
+        COALESCE(s.voie, ''),
+        CASE WHEN s.complement_adresse IS NOT NULL THEN CONCAT('(', s.complement_adresse, ')') END
         ) || E'\n' ||
       CONCAT_WS(' ',
-        COALESCE(code_postal, ''),
-        COALESCE(commune, commune_etranger, ''),
-        CASE WHEN pays_etranger IS NOT NULL THEN CONCAT(E'\n', pays_etranger) END
+        COALESCE(s.code_postal, ''),
+        COALESCE(s.commune, s.commune_etranger, ''),
+        CASE WHEN s.pays_etranger IS NOT NULL THEN CONCAT(E'\n', s.pays_etranger) END
       )
     ) AS adresse,
-    code_commune,
-    departement,
-    ape,
-    date_creation,
-    longitude,
-    latitude,
-    est_actif
-  FROM stg_sirene
-  WHERE siren IN (SELECT siren FROM clean_filter);
+    s.code_commune,
+    s.departement,
+    s.ape,
+    naf.niv1 AS naf_section,
+    s.date_creation,
+    s.longitude,
+    s.latitude,
+    s.est_actif
+  FROM stg_sirene s
+  LEFT JOIN naf_codes naf ON s.ape = naf.niv5
+  WHERE s.siren IN (SELECT siren FROM clean_filter);
 
 -- Update clean_sirene_ul view to add ape column
 CREATE OR REPLACE VIEW clean_sirene_ul AS
   SELECT
-    siren,
-    raison_sociale,
-    statut_juridique,
-    ape,
-    creation,
-    est_actif
-  FROM stg_sirene_ul
-  WHERE siren IN (SELECT siren FROM clean_filter);
+    s.siren,
+    s.raison_sociale,
+    s.statut_juridique,
+    s.ape,
+    naf.niv1 AS naf_section,
+    s.creation,
+    s.est_actif
+  FROM stg_sirene_ul s
+  LEFT JOIN naf_codes naf ON s.ape = naf.niv5
+  WHERE s.siren IN (SELECT siren FROM clean_filter);
 
 -- Update clean_filter to use ape instead of activite_principale
 DROP MATERIALIZED VIEW IF EXISTS clean_filter;
