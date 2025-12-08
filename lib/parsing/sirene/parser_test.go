526bcef0fc328f6708f657e09f837f262ea798db
Pierre Camilleri <pierre.camilleri@multi.coop>
2025-12-08 21:37:28 +0100

    🟢 test the filtering behavior

--- lib/parsing/sirene/parser_test.go
+++ lib/parsing/sirene/parser_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"opensignauxfaibles/lib/engine"
+	"opensignauxfaibles/lib/parsing"
 )
 
 var update = flag.Bool("update", false, "Update the expected test values in golden file")
@@ -125,4 +126,76 @@ func TestExtractDepartement(t *testing.T) {
 			assert.Equal(t, tt.want, got)
 		})
 	}
+}
+
+var sampleRow = []string{
+	"956520573", "44947", "67323298386574", "t", "2007-04-20", "", "", "", "",
+	"false", "2", "", "", "", "CHE", "dguylittc", "64276", "rjkxla", "", "",
+	"73187", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+	"", "2007-11-19", "A", "", "", "", "", "52.21Z", "NAFRev2", "w", "6.125600",
+	"14.106745", "4.74", "rmyvud", "pycqborak26441jvefqp", "52756_a195_mpo13m",
+	"p", "hkcilbeed", "",
+}
+
+var headers = []string{
+	"siren", "nic", "siret", "statutDiffusionEtablissement", "dateCreationEtablissement", "trancheEffectifsEtablissement", "anneeEffectifsEtablissement", "activitePrincipaleRegistreMetiersEtablissement", "dateDernierTraitementEtablissement", "etablissementSiege", "nombrePeriodesEtablissement", "complementAdresseEtablissement", "numeroVoieEtablissement", "indiceRepetitionEtablissement", "typeVoieEtablissement", "libelleVoieEtablissement", "codePostalEtablissement", "libelleCommuneEtablissement", "libelleCommuneEtrangerEtablissement", "distributionSpecialeEtablissement", "codeCommuneEtablissement", "codeCedexEtablissement", "libelleCedexEtablissement", "codePaysEtrangerEtablissement", "libellePaysEtrangerEtablissement", "complementAdresse2Etablissement", "numeroVoie2Etablissement", "indiceRepetition2Etablissement", "typeVoie2Etablissement", "libelleVoie2Etablissement", "codePostal2Etablissement", "libelleCommune2Etablissement", "libelleCommuneEtranger2Etablissement", "distributionSpeciale2Etablissement", "codeCommune2Etablissement", "codeCedex2Etablissement", "libelleCedex2Etablissement", "codePaysEtranger2Etablissement", "libellePaysEtranger2Etablissement", "dateDebut", "etatAdministratifEtablissement", "enseigne1Etablissement", "enseigne2Etablissement", "enseigne3Etablissement", "denominationUsuelleEtablissement", "activitePrincipaleEtablissement", "nomenclatureActivitePrincipaleEtablissement", "caractereEmployeurEtablissement", "longitude", "latitude", "geo_score", "geo_type", "geo_adresse", "geo_id", "geo_ligne", "geo_l4", "geo_l5",
+}
+
+var colIndex, _ = parsing.HeaderIndexer{Dest: Sirene{}}.Index(headers, true)
+
+func TestNAFFiltering(t *testing.T) {
+	testcases := []struct {
+		name                 string
+		nomenclatureActivite string
+		shouldFilter         bool
+	}{
+		{
+			"NAFRev2 should be included",
+			"NAFRev2",
+			false,
+		},
+		{
+			"NAFRev1 should be excluded",
+			"NAFRev1",
+			true,
+		},
+		{
+			"NAF1993 should be excluded",
+			"NAF1993",
+			true,
+		},
+		{
+			"NAP should be excluded",
+			"NAP",
+			true,
+		},
+		{
+			"missing nomenclature should be excluded",
+			"",
+			true,
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			row := make([]string, len(sampleRow))
+			copy(row, sampleRow)
+
+			index, _ := colIndex.Get("nomenclatureActivitePrincipaleEtablissement")
+			row[index] = tc.nomenclatureActivite
+
+			parser := &sireneRowParser{}
+			res := &engine.ParsedLineResult{}
+
+			parser.ParseRow(row, res, colIndex)
+
+			if tc.shouldFilter {
+				assert.NotNil(t, res.FilterError)
+			} else {
+				assert.Nil(t, res.FilterError)
+			}
+
+		})
+	}
+
 }
