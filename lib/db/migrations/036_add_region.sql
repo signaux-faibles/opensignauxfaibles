a6eac86e7b094e11bf78c5b177b0ca81cf5e5f01
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2026-01-28 11:10:06 +0100

    feat: ajout des régions & des libellés des départements et régions (#566)
    * Add migration : create label_departement_region
    
    * migrate sirene views accordingly
    
    * migration comment
    
    * fix: datatype
    
    * rename for consistency
    
    * add primary key

---
+++ lib/db/migrations/036_add_region.sql
@@ -0,0 +1,125 @@
+CREATE TABLE labels_departement_region (
+  departement VARCHAR(10) PRIMARY KEY,
+  libelle_departement TEXT,
+  region VARCHAR(10),
+  libelle_region TEXT
+);
+
+
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('01','Ain','84','Auvergne-Rhône-Alpes');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('02','Aisne','32','Hauts-de-France');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('03','Allier','84','Auvergne-Rhône-Alpes');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('04','Alpes-de-Haute-Provence','93','Provence-Alpes-Côte d''Azur');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('05','Hautes-Alpes','93','Provence-Alpes-Côte d''Azur');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('06','Alpes-Maritimes','93','Provence-Alpes-Côte d''Azur');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('07','Ardèche','84','Auvergne-Rhône-Alpes');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('08','Ardennes','44','Grand Est');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('09','Ariège','76','Occitanie');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('10','Aube','44','Grand Est');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('11','Aude','76','Occitanie');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('12','Aveyron','76','Occitanie');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('13','Bouches-du-Rhône','93','Provence-Alpes-Côte d''Azur');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('14','Calvados','28','Normandie');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('15','Cantal','84','Auvergne-Rhône-Alpes');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('16','Charente','75','Nouvelle-Aquitaine');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('17','Charente-Maritime','75','Nouvelle-Aquitaine');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('18','Cher','24','Centre-Val de Loire');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('19','Corrèze','75','Nouvelle-Aquitaine');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('21','Côte-d''Or','27','Bourgogne-Franche-Comté');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('22','Côtes-d''Armor','53','Bretagne');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('23','Creuse','75','Nouvelle-Aquitaine');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('24','Dordogne','75','Nouvelle-Aquitaine');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('25','Doubs','27','Bourgogne-Franche-Comté');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('26','Drôme','84','Auvergne-Rhône-Alpes');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('27','Eure','28','Normandie');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('28','Eure-et-Loir','24','Centre-Val de Loire');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('29','Finistère','53','Bretagne');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('2A','Corse-du-Sud','94','Corse');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('2B','Haute-Corse','94','Corse');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('30','Gard','76','Occitanie');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('31','Haute-Garonne','76','Occitanie');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('32','Gers','76','Occitanie');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('33','Gironde','75','Nouvelle-Aquitaine');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('34','Hérault','76','Occitanie');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('35','Ille-et-Vilaine','53','Bretagne');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('36','Indre','24','Centre-Val de Loire');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('37','Indre-et-Loire','24','Centre-Val de Loire');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('38','Isère','84','Auvergne-Rhône-Alpes');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('39','Jura','27','Bourgogne-Franche-Comté');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('40','Landes','75','Nouvelle-Aquitaine');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('41','Loir-et-Cher','24','Centre-Val de Loire');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('42','Loire','84','Auvergne-Rhône-Alpes');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('43','Haute-Loire','84','Auvergne-Rhône-Alpes');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('44','Loire-Atlantique','52','Pays de la Loire');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('45','Loiret','24','Centre-Val de Loire');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('46','Lot','76','Occitanie');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('47','Lot-et-Garonne','75','Nouvelle-Aquitaine');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('48','Lozère','76','Occitanie');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('49','Maine-et-Loire','52','Pays de la Loire');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('50','Manche','28','Normandie');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('51','Marne','44','Grand Est');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('52','Haute-Marne','44','Grand Est');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('53','Mayenne','52','Pays de la Loire');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('54','Meurthe-et-Moselle','44','Grand Est');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('55','Meuse','44','Grand Est');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('56','Morbihan','53','Bretagne');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('57','Moselle','44','Grand Est');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('58','Nièvre','27','Bourgogne-Franche-Comté');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('59','Nord','32','Hauts-de-France');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('60','Oise','32','Hauts-de-France');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('61','Orne','28','Normandie');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('62','Pas-de-Calais','32','Hauts-de-France');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('63','Puy-de-Dôme','84','Auvergne-Rhône-Alpes');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('64','Pyrénées-Atlantiques','75','Nouvelle-Aquitaine');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('65','Hautes-Pyrénées','76','Occitanie');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('66','Pyrénées-Orientales','76','Occitanie');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('67','Bas-Rhin','44','Grand Est');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('68','Haut-Rhin','44','Grand Est');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('69','Rhône','84','Auvergne-Rhône-Alpes');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('70','Haute-Saône','27','Bourgogne-Franche-Comté');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('71','Saône-et-Loire','27','Bourgogne-Franche-Comté');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('72','Sarthe','52','Pays de la Loire');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('73','Savoie','84','Auvergne-Rhône-Alpes');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('74','Haute-Savoie','84','Auvergne-Rhône-Alpes');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('75','Paris','11','Île-de-France');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('76','Seine-Maritime','28','Normandie');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('77','Seine-et-Marne','11','Île-de-France');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('78','Yvelines','11','Île-de-France');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('79','Deux-Sèvres','75','Nouvelle-Aquitaine');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('80','Somme','32','Hauts-de-France');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('81','Tarn','76','Occitanie');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('82','Tarn-et-Garonne','76','Occitanie');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('83','Var','93','Provence-Alpes-Côte d''Azur');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('84','Vaucluse','93','Provence-Alpes-Côte d''Azur');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('85','Vendée','52','Pays de la Loire');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('86','Vienne','75','Nouvelle-Aquitaine');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('87','Haute-Vienne','75','Nouvelle-Aquitaine');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('88','Vosges','44','Grand Est');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('89','Yonne','27','Bourgogne-Franche-Comté');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('90','Territoire de Belfort','27','Bourgogne-Franche-Comté');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('91','Essonne','11','Île-de-France');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('92','Hauts-de-Seine','11','Île-de-France');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('93','Seine-Saint-Denis','11','Île-de-France');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('94','Val-de-Marne','11','Île-de-France');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('95','Val-d''Oise','11','Île-de-France');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('971','Guadeloupe','01','Guadeloupe');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('972','Martinique','02','Martinique');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('973','Guyane','03','Guyane');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('974','La Réunion','04','La Réunion');
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('976','Mayotte','06','Mayotte');
+
+-- Collectivités et territoires d'outre-mer, qu'on assimile abusivement à "département"
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('975', 'Saint-Pierre-et-Miquelon' , '00', 'Collectivité d''outre-mer' );
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('977', 'Saint-Barthélemy' , '00', 'Collectivité d''outre-mer' );
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('978', 'Saint-Martin' , '00', 'Collectivité d''outre-mer' );
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('981', 'Île des Faisans' , '00', 'Collectivité d''outre-mer' );
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('984', 'Terres australes et antarctiques françaises' , '00', 'Collectivité d''outre-mer' );
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('986', 'Wallis-et-Futuna' , '00', 'Collectivité d''outre-mer' );
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('987', 'Polynésie française' , '00', 'Collectivité d''outre-mer' );
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('988', 'Nouvelle-Calédonie' , '00', 'Collectivité d''outre-mer' );
+INSERT INTO labels_departement_region(departement,libelle_departement,region,libelle_region) VALUES('989', 'La Passion-Clipperton' , '00', 'Collectivité d''outre-mer' );
+
+
+---- create above / drop below ----
+
+DROP TABLE IF EXISTS labels_departement_region;
