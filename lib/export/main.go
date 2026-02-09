e20d0fc5cbf2ff52da643f8078c390df40669308
Pierre Camilleri <pierre.camilleri@multi.coop>
2026-02-09 16:23:50 +0100

    Parallel export

--- lib/export/main.go
+++ lib/export/main.go
@@ -12,6 +12,8 @@ import (
 	"path/filepath"
 
 	"opensignauxfaibles/lib/db"
+
+	"golang.org/x/sync/errgroup"
 )
 
 var viewsToExport = []string{
@@ -39,7 +41,7 @@ func NewExporter(path string, conn db.Pool) *Exporter {
 
 // CleanViews exports all clean views
 func (exp *Exporter) CleanViews() error {
-	ctx := context.TODO()
+	g, ctx := errgroup.WithContext(context.Background())
 
 	err := os.MkdirAll(exp.path, 0644)
 	if err != nil {
@@ -51,22 +53,30 @@ func (exp *Exporter) CleanViews() error {
 	}
 
 	for _, view := range viewsToExport {
-		fileAbsPath := filepath.Join(dirAbsPath, view+".csv")
-		// Fail if file already exist
-		_, err := os.Stat(fileAbsPath)
-		if err == nil {
-			slog.Warn(fmt.Sprintf("Le fichier %s existe déjà et n'est pas réexporté", fileAbsPath))
-			continue
-		}
+		// run export in parallel
+		g.Go(func() error {
+			fileAbsPath := filepath.Join(dirAbsPath, view+".csv")
+			// Fail if file already exist
+			_, err := os.Stat(fileAbsPath)
+			if err == nil {
+				slog.Warn(fmt.Sprintf("Le fichier %s existe déjà et n'est pas réexporté", fileAbsPath))
+				return nil
+			}
 
-		f, err := os.Create(fileAbsPath)
-		if err != nil {
-			return err
-		}
+			f, err := os.Create(fileAbsPath)
+			if err != nil {
+				return err
+			}
 
-		if err = toWriter(ctx, f, view, exp.conn); err != nil {
-			return fmt.Errorf("export of %s failed: %w", view, err)
-		}
+			if err = toWriter(ctx, f, view, exp.conn); err != nil {
+				return fmt.Errorf("export of %s failed: %w", view, err)
+			}
+			return nil
+		})
+	}
+
+	if err := g.Wait(); err != nil {
+		fmt.Println("Erreur:", err)
 	}
 
 	return nil
